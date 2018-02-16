827610958ff128ba6103dc7fd9de03c679d896de
Dandelion Mané <dl@dandelion.io>
2018-02-15 22:36:31 -0800

    Initial version of the cred explorer.

---
+++ explorer/src/commitUtils.js
@@ -0,0 +1,93 @@
+// @flow
+
+type CommitData = {
+  // TODO improve variable names
+  fileToCommits: {[filename: string]: string[]};
+  commits: {[commithash: string]: Commit};
+  authors: string[];
+}
+
+type Commit = {
+  author: string;
+  stats: {[filename: string]: FileStats};
+}
+
+type FileStats = {
+  lines: number;
+  added: number;
+  deleted: number;
+}
+
+export function commitWeight(commit: Commit, filepath: string): number {
+  // hack - GitPython encodes renames in the filepath. ignore for now.
+  if (filepath.indexOf('=>') !== -1) {
+    return 0;
+  }
+  return Math.sqrt(commit.stats[filepath].lines);
+}
+
+function allSelectedFiles(filepath: string, data: CommitData): string[] {
+  const fnames = Object.keys(data.fileToCommits);
+  return fnames.filter(x => x.startsWith(filepath));
+}
+
+function *userWeights(files: string[], data: CommitData, weightFn: WeightFn): Iterable<[string, number]> {
+  for (const file of files) {
+    for (const commitHash of data.fileToCommits[file]) {
+      const commit = data.commits[commitHash];
+      let w;
+      if (commit.stats[file] == null) {
+        // hack - likely due to the GitPython file rename issue
+        console.log(`commit ${commitHash} missing file ${file}`);
+        w = 0;
+      } else {
+        w = weightFn(commit, file);
+      }
+      yield [commit.author, w];
+    }
+  }
+}
+
+type WeightFn = (commit: Commit, filepath: string) => number;
+export function userWeightForPath(path: string, data: CommitData, weightFn: WeightFn): {[string]: number} {
+  const userWeightMap = {};
+  const files = allSelectedFiles(path, data);
+  for (const [user, weight] of userWeights(files, data, weightFn)) {
+    if (userWeightMap[user] == null) {
+      userWeightMap[user] = 0;
+    }
+    userWeightMap[user] += weight;
+  }
+  return userWeightMap;
+}
+
+type FileTree = {[string]: FileTree}; // {[string]: FileTree};
+
+export function buildTree(data: CommitData): FileTree {
+  const fileNames = Object.keys(data.fileToCommits).sort();
+  return _buildTree(fileNames);
+}
+
+function _buildTree(fileNames: string[]): FileTree {
+  const sortedFileNames = fileNames.slice().sort();
+  const topLevelBuckets: {[root: string]: string[]} = {};
+  for (const fileName of sortedFileNames) {
+    const topLevel = fileName.split('/')[0];
+    const remainder = fileName
+      .split('/')
+      .slice(1)
+      .join('/');
+    if (topLevelBuckets[topLevel] == null) {
+      topLevelBuckets[topLevel] = [];
+    }
+    if (remainder !== '') {
+      topLevelBuckets[topLevel].push(remainder);
+    }
+  }
+  const result = {};
+  for (const topLevel of Object.keys(topLevelBuckets)) {
+    const remainders = topLevelBuckets[topLevel];
+    result[topLevel] = _buildTree(remainders);
+  }
+  return result;
+}
---
+++ explorer/src/commitUtils.test.js
@@ -0,0 +1,58 @@
+// @flow
+import {userWeightForPath} from './commitUtils';
+
+const exampleData = {
+  fileToCommits: {
+    'foo.txt': ['1'],
+    'bar.txt': ['1', '2']
+  },
+  commits: {
+    '1': {
+      'author': 'dandelionmane',
+      'stats': {
+        'foo.txt': {'lines': 5, 'added': 3, 'deleted': 2},
+        'bar.txt': {'lines': 100, 'added': 100, 'deleted': 0}
+        }
+      },
+    '2': {
+      'author': 'wchargin',
+      'stats': {
+      'bar.txt': {'lines': 100, 'added': 50, 'deleted': 50},
+        }
+      },
+  },
+  authors: ['dandelionmane', 'wchargin']
+}
+
+const emptyData = {fileToCommits: {}, commits: {}, authors: []};
+
+function weightByNumFilesTouched(commit, filepath) {
+  return 1;
+}
+
+describe('userWeightForPath', () => {
+  it('works on empty data', () => {
+    const actual = userWeightForPath('', emptyData, weightByNumFilesTouched);
+    const expected = {};
+    expect(actual).toEqual(expected);
+  });
+
+  it('works in simple case', () => {
+    const actual = userWeightForPath('', exampleData, weightByNumFilesTouched);
+    const expected = {'dandelionmane': 2, 'wchargin': 1};
+    expect(actual).toEqual(expected);
+  });
+
+  it('respects file paths', () => {
+    const actual = userWeightForPath('bar.txt', exampleData, weightByNumFilesTouched);
+    const expected = {'dandelionmane': 1, 'wchargin': 1};
+    expect(actual).toEqual(expected);
+  });
+
+  it('uses custom weight function', () => {
+    const myWeight = (commit, filepath) => commit.stats[filepath].lines;
+    const actual = userWeightForPath('', exampleData, myWeight);
+    const expected = {'dandelionmane': 105, 'wchargin': 100};
+    expect(actual).toEqual(expected);
+  })
+})
