65ae5c05345475ea204ee0d3624d04e614451d28
Dandelion Mané <dl@dandelion.io>
2018-02-26 14:07:17 -0800

    Add types for GitHub plugin.

---
+++ backend/githubPlugin.js
@@ -0,0 +1,45 @@
+// @flow
+
+import type {ID, GraphNode, GraphEdge} from "./graph";
+
+export const GITHUB_PLUGIN_NANE = "sourcecred/github-beta";
+
+export type NodeType = "PULL_REQUEST" | "ISSUE" | "COMMENT" | "USER";
+
+export type PullRequestPayload = {
+  title: string,
+  isClosed: boolean,
+  isMerged: boolean,
+  dateCreated: number,
+  dateMerged: ?number,
+};
+
+export type IssuePayload = {
+  title: string,
+  isClosed: boolean,
+  dateCreated: number,
+};
+
+export type ReviewState = "APPROVED" | "COMMENTED" | "CHANGES_REQUESTED";
+export type CommentPayload = {
+  body: string,
+  date: number,
+  state: ?ReviewState,
+};
+
+export type UserPayload = {
+  githubUserId: number,
+};
+
+export type GithubNodePayload = {
+  type: NodeType,
+  subpayload: PullRequestPayload | IssuePayload | CommentPayload | UserPayload,
+};
+
+export type EdgeType = "AUTHOR" | "REFERENCE";
+
+export type AuthorPayload = {
+  // Issue/PR/Comment -> User
+};
+
+export type ReferencePayload = {};
