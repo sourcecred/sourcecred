3ed8fab4507ade72d686e747810647d4485b2b7f
William Chargin <wchargin@gmail.com>
2018-02-26 21:52:13 -0800

    Add conversion functions for id
    Test Plan:
    Run `yarn test` and note that tests pass.
    
    wchargin-branch: id-conversion

--- src/backend/graph.js
+++ src/backend/graph.js
@@ -24,3 +24,32 @@ export type Graph = {
   nodes: {[stringID: string]: GraphNode<mixed>},
   edges: {[stringID: string]: GraphEdge<mixed>},
 };
+
+export function idToString(id: ID) {
+  if (id.pluginName.includes("$")) {
+    const escaped = JSON.stringify(id.pluginName);
+    throw new Error(`id.pluginName must not include "\$": ${escaped}`);
+  }
+  if (id.repositoryName.includes("$")) {
+    const escaped = JSON.stringify(id.repositoryName);
+    throw new Error(`id.repositoryName must not include "\$": ${escaped}`);
+  }
+  if (id.name.includes("$")) {
+    const escaped = JSON.stringify(id.name);
+    throw new Error(`id.name must not include "\$": ${escaped}`);
+  }
+  return `${id.pluginName}\$${id.repositoryName}\$${id.name}`;
+}
+
+export function stringToID(string: string) {
+  const parts = string.split("$");
+  if (parts.length !== 3) {
+    const escaped = JSON.stringify(string);
+    throw new Error(`Input should have exactly two \$s: ${escaped}`);
+  }
+  return {
+    pluginName: parts[0],
+    repositoryName: parts[1],
+    name: parts[2],
+  };
+}
