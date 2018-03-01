09156bf3f491e51f1e01af6f89d09284e6df1cb6
William Chargin <wchargin@gmail.com>
2018-03-01 01:04:11 -0800

    Promote `Graph` to a class with useful methods (#49)
    Summary:
    We had planned to expose our core types as simple Plain Old JavaScript
    Objects, with accompanying standalone functions to act directly on these
    data structures. We chose this instead of creating `class`es for the
    types because it simplifies serialization interop: it obviates the need
    for serialization and deserialization functions, because the code is
    separated from the data entirely. Reconsidering, we now think that the
    convenience benefits of using classes probably outweigh these
    serialization cons. Furthermore, this design enables us to separate
    ancillary data structures and caches from the raw data, presenting a
    cleaner API for consumers of the data.
    
    This commit introduces a `Graph` class and some related logic. With lots
    of tests! And 100% code coverage! :-)
    
    Paired with @dandelionmane.
    
    Test Plan:
    Run `yarn flow && yarn test` to see the new tests.
    
    wchargin-branch: graph-class

--- src/backend/graph.js
+++ src/backend/graph.js
@@ -18,12 +18,88 @@ export type Edge<T> = {
   payload: T,
 };
 
-export type Graph = {
-  nodes: {[stringAddress: string]: Node<mixed>},
-  edges: {[stringAddress: string]: Edge<mixed>},
-  outEdges: {[nodeAddress: string]: Address[]},
-  inEdges: {[nodeAddress: string]: Address[]},
-};
+export class Graph {
+  _nodes: {[nodeAddress: string]: Node<mixed>};
+  _edges: {[edgeAddress: string]: Edge<mixed>};
+
+  // The keyset of each of the following fields should equal the keyset
+  // of `_nodes`. If `e` is an edge from `u` to `v`, then `e.address`
+  // should appear exactly once in `_outEdges[u.address]` and exactly
+  // once in `_inEdges[v.address]` (and every entry in `_inEdges` and
+  // `_outEdges` should be of this form).
+  _outEdges: {[nodeAddress: string]: Address[]};
+  _inEdges: {[nodeAddress: string]: Address[]};
+
+  constructor() {
+    this._nodes = {};
+    this._edges = {};
+    this._outEdges = {};
+    this._inEdges = {};
+  }
+
+  addNode(node: Node<mixed>) {
+    if (this.getNode(node.address) !== undefined) {
+      throw new Error(
+        `node at address ${JSON.stringify(node.address)} already exists`
+      );
+    }
+    const addressString = addressToString(node.address);
+    this._nodes[addressString] = node;
+    this._outEdges[addressString] = [];
+    this._inEdges[addressString] = [];
+    return this;
+  }
+
+  addEdge(edge: Edge<mixed>) {
+    if (this.getEdge(edge.address) !== undefined) {
+      throw new Error(
+        `edge at address ${JSON.stringify(edge.address)} already exists`
+      );
+    }
+    if (this.getNode(edge.src) === undefined) {
+      throw new Error(`source ${JSON.stringify(edge.src)} does not exist`);
+    }
+    if (this.getNode(edge.dst) === undefined) {
+      throw new Error(`source ${JSON.stringify(edge.dst)} does not exist`);
+    }
+    this._edges[addressToString(edge.address)] = edge;
+    this._outEdges[addressToString(edge.src)].push(edge.address);
+    this._inEdges[addressToString(edge.dst)].push(edge.address);
+    return this;
+  }
+
+  getNode(address: Address): Node<mixed> {
+    return this._nodes[addressToString(address)];
+  }
+
+  getEdge(address: Address): Edge<mixed> {
+    return this._edges[addressToString(address)];
+  }
+
+  /**
+   * Gets the array of all out-edges from the node at the given address.
+   * The order of the resulting array is unspecified.
+   */
+  getOutEdges(nodeAddress: Address): Edge<mixed>[] {
+    const addresses = this._outEdges[addressToString(nodeAddress)];
+    if (addresses === undefined) {
+      throw new Error(`no node for address ${JSON.stringify(nodeAddress)}`);
+    }
+    return addresses.map((e) => this.getEdge(e));
+  }
+
+  /**
+   * Gets the array of all in-edges to the node at the given address.
+   * The order of the resulting array is unspecified.
+   */
+  getInEdges(nodeAddress: Address): Edge<mixed>[] {
+    const addresses = this._inEdges[addressToString(nodeAddress)];
+    if (addresses === undefined) {
+      throw new Error(`no node for address ${JSON.stringify(nodeAddress)}`);
+    }
+    return addresses.map((e) => this.getEdge(e));
+  }
+}
 
 export function addressToString(address: Address) {
   if (address.repositoryName.includes("$")) {
