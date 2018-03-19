99588a22ed07b87879acd3a18225a3b852a3427b
William Chargin <wchargin@gmail.com>
2018-03-19 16:25:27 -0700

    Enable creating artifacts in the artifact list
    Summary:
    Paired with @dandelionmane.
    
    wchargin-branch: create-artifacts

--- src/plugins/artifact/editor/App.js
+++ src/plugins/artifact/editor/App.js
@@ -3,35 +3,49 @@
 import React from "react";
 import {StyleSheet, css} from "aphrodite/no-important";
 
+import type {Node} from "../../../core/graph";
 import {ArtifactList} from "./ArtifactList";
+import type {ArtifactNodePayload} from "../artifactPlugin";
 
 type Props = {};
-type State = {};
+type State = {
+  artifacts: Node<ArtifactNodePayload>[],
+};
+
+function createSampleArtifact(name) {
+  const id = name.toLowerCase().replace(/[^a-z]/g, "-");
+  return {
+    address: {
+      repositoryName: "sourcecred/devnull",
+      pluginName: "sourcecred/artifact-beta",
+      id,
+    },
+    payload: {name},
+  };
+}
 
 export default class App extends React.Component<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      artifacts: [],
+    };
+  }
+
   render() {
-    function createSampleArtifact(name) {
-      const id = name.toLowerCase().replace(/[^a-z]/g, "-");
-      return {
-        address: {
-          repositoryName: "sourcecred/devnull",
-          pluginName: "sourcecred/artifact-beta",
-          id,
-        },
-        payload: {name},
-      };
-    }
-    const artifacts = [
-      createSampleArtifact("Root"),
-      createSampleArtifact("Tooling"),
-      createSampleArtifact("Tests"),
-    ];
     return (
       <div>
         <header className={css(styles.header)}>
           <h1>Artifact editor</h1>
         </header>
-        <ArtifactList artifacts={artifacts} />
+        <ArtifactList
+          artifacts={this.state.artifacts}
+          createArtifact={(name) => {
+            this.setState((state) => ({
+              artifacts: [...state.artifacts, createSampleArtifact(name)],
+            }));
+          }}
+        />
       </div>
     );
   }
