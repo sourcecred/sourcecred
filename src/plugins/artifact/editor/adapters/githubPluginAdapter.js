7f2053b6f1017e49c853a64a54ee1b8030e55793
William Chargin <wchargin@gmail.com>
2018-03-19 19:51:36 -0700

    Begin work on contributions and adapters
    Summary:
    This commit begins to extend the artifact editor to display
    contributions. To display contributions from arbitrary plugins, we need
    to communicate with those plugins somehow. We do so via an adapter
    interface that plugins implement; included in this commit is an
    implementation of this interface for the GitHub plugin (partially: we
    punt on rendering).
    
    This includes a snapshot test. The snapshot format is designed to be
    human-readable and -auditable so that it can serve as documentation.
    
    Test Plan:
    Run the application with `yarn start`. Then, fetch a graph and watch as
    its contributions appear in the view.
    
    wchargin-branch: contributions-and-adapters

---
+++ src/plugins/artifact/editor/adapters/githubPluginAdapter.js
@@ -0,0 +1,110 @@
+// @flow
+
+import React from "react";
+
+import {Graph} from "../../../../core/graph";
+import type {Node} from "../../../../core/graph";
+import type {
+  NodePayload,
+  EdgeID,
+  NodeType,
+  NodeTypes,
+  IssueNodePayload,
+  PullRequestNodePayload,
+  CommentNodePayload,
+  PullRequestReviewCommentNodePayload,
+  PullRequestReviewNodePayload,
+  AuthorNodePayload,
+} from "../../../github/githubPlugin";
+import type {PluginAdapter} from "../pluginAdapter";
+import {GITHUB_PLUGIN_NAME, getNodeType} from "../../../github/githubPlugin";
+
+const adapter: PluginAdapter<NodePayload> = {
+  pluginName: GITHUB_PLUGIN_NAME,
+
+  renderer: class GitHubNodeRenderer extends React.Component<{
+    graph: Graph<any, any>,
+    node: Node<NodePayload>,
+  }> {
+    render() {
+      const type = adapter.extractType(this.props.graph, this.props.node);
+      return <div>type: {type} (details to be implemented)</div>;
+    }
+  },
+
+  extractType(graph: *, node: Node<NodePayload>): NodeType {
+    return getNodeType(node);
+  },
+
+  extractTitle(graph: *, node: Node<NodePayload>): string {
+    // NOTE: If the graph is malformed such that there are containment
+    // cycles, then this function may blow the stack or fail to
+    // terminate. (If necessary, we can fix this by tracking all
+    // previously queried IDs.)
+    function extractParentTitles(node: Node<NodePayload>): string[] {
+      return graph
+        .getInEdges(node.address)
+        .filter((e) => {
+          const edgeID: EdgeID = JSON.parse(e.address.id);
+          return edgeID.type === "CONTAINMENT";
+        })
+        .map((e) => graph.getNode(e.src))
+        .map((container) => {
+          return adapter.extractTitle(graph, container);
+        });
+    }
+    function extractIssueOrPrTitle(
+      node: Node<IssueNodePayload | PullRequestNodePayload>
+    ) {
+      return `#${node.payload.number}: ${node.payload.title}`;
+    }
+    function extractCommentTitle(
+      kind: string,
+      node: Node<CommentNodePayload | PullRequestReviewCommentNodePayload>
+    ) {
+      const parentTitles = extractParentTitles(node);
+      if (parentTitles.length === 0) {
+        // Should never happen.
+        return "comment (orphaned)";
+      } else {
+        // Should just be one parent.
+        return `comment on ${parentTitles.join(" and ")}`;
+      }
+    }
+    function extractPRReviewTitle(node: Node<PullRequestReviewNodePayload>) {
+      const parentTitles = extractParentTitles(node);
+      if (parentTitles.length === 0) {
+        // Should never happen.
+        return "pull request review (orphaned)";
+      } else {
+        // Should just be one parent.
+        return `review of ${parentTitles.join(" and ")}`;
+      }
+    }
+    function extractAuthorTitle(node: Node<AuthorNodePayload>) {
+      return node.payload.login;
+    }
+    type TypedNodeToStringExtractor = <T: $Values<NodeTypes>>(
+      T
+    ) => (node: Node<$ElementType<T, "payload">>) => string;
+    const extractors: $Exact<$ObjMap<NodeTypes, TypedNodeToStringExtractor>> = {
+      ISSUE: extractIssueOrPrTitle,
+      PULL_REQUEST: extractIssueOrPrTitle,
+      COMMENT: (node) => extractCommentTitle("comment", node),
+      PULL_REQUEST_REVIEW_COMMENT: (node) =>
+        extractCommentTitle("review comment", node),
+      PULL_REQUEST_REVIEW: extractPRReviewTitle,
+      USER: extractAuthorTitle,
+      ORGANIZATION: extractAuthorTitle,
+      BOT: extractAuthorTitle,
+    };
+    function fallbackAccessor(node: Node<NodePayload>) {
+      throw new Error(`unknown node type: ${getNodeType(node)}`);
+    }
+    return (extractors[getNodeType(node)] || fallbackAccessor)(
+      (node: Node<any>)
+    );
+  },
+};
+
+export default adapter;
