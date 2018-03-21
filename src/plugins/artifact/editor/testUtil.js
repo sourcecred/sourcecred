cf4ada15d3e554df467beaf034e80a5f4b19065c
William Chargin <wchargin@gmail.com>
2018-03-21 11:50:31 -0700

    Use Enzyme to test ContributionList dynamics
    Summary:
    This is our first dynamic test of a React component! Enzyme looks pretty
    easy to use to me, for both snapshot tests and interaction simulation.
    
    In doing so, we catch a minor bug in the edge case where a contribution
    is not owned by any plugin (`colSpan`, not `colspan`). This edge case
    does not appear in the sample data, but it does appear in the test data,
    even prior to this commit. The previous renderer, `react-test-renderer`,
    appears not to surface this error. Furthermore, this bug did not cause
    any user-visible errors except a `console.error`.
    
    Test Plan:
    Inspect the snapshot file to make sure that it is reasonable. (The
    existing test case has its snapshot regenerated due to formatting
    differences between the two renderers.)
    
    To test that the browser error is fixed, render a contribution list on a
    GitHub graph but with an empty adapter set. One way to do this is to comment out line 7 of
    `standardAdapterSet.js`; alternately, you can use the React Dev Tools to
    select the `ContributionList` node, then run
    ```js
    $r.props.adapters.adapters = {};
    $r.forceUpdate();
    ```
    Note subsequently that there is no console error and that the `<td>`s in
    question span three columns.
    
    wchargin-branch: contributionlist-dynamic-test

--- src/plugins/artifact/editor/testUtil.js
+++ src/plugins/artifact/editor/testUtil.js
@@ -1,6 +1,13 @@
-import {StyleSheetTestUtils} from "aphrodite/no-important";
+// @flow
+
+export function configureEnzyme() {
+  const Enzyme = require("enzyme");
+  const Adapter = require("enzyme-adapter-react-16");
+  Enzyme.configure({adapter: new Adapter()});
+}
 
 export function configureAphrodite() {
+  const {StyleSheetTestUtils} = require("aphrodite/no-important");
   beforeEach(() => {
     StyleSheetTestUtils.suppressStyleInjection();
   });
