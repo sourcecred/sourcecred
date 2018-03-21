61624a5dcfce48f3faf8e94405ba6603554b5ac2
William Chargin <wchargin@gmail.com>
2018-03-20 17:35:26 -0700

    Extract Aphrodite-management test code to testUtil (#99)
    Summary:
    Any tests that render Aphrodite-styled React elements will need to do
    this, so it’s nice to have the code in one place.
    
    wchargin-branch: aphrodite-testutils

--- src/plugins/artifact/editor/App.test.js
+++ src/plugins/artifact/editor/App.test.js
@@ -2,15 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import {StyleSheetTestUtils} from "aphrodite/no-important";
-
-beforeEach(() => {
-  StyleSheetTestUtils.suppressStyleInjection();
-});
-
-afterEach(() => {
-  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
+require("./testUtil").configureAphrodite();
 
 // Check that PropTypes check out.
 it("renders without crashing", () => {
