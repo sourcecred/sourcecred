d949dac1e4741b0bf02778cd0943116511f25ffb
Dandelion Mané <dl@dandelion.io>
2018-04-19 10:23:46 -0400

    add GitHub reference edges

--- src/plugins/github/githubPlugin.test.js
+++ src/plugins/github/githubPlugin.test.js
@@ -8,13 +8,35 @@ import {
 import exampleRepoData from "./demoData/example-repo.json";
 
 describe("GithubParser", () => {
+  function getIssue(n) {
+    const issues = exampleRepoData.data.repository.issues.nodes;
+    const selected = issues.filter((x) => x.number === n);
+    if (selected.length !== 1) {
+      throw new Error(`Failure finding issue #${n}`);
+    }
+    return selected[0];
+  }
+  function getPR(n) {
+    const pulls = exampleRepoData.data.repository.pullRequests.nodes;
+    const selected = pulls.filter((x) => x.number === n);
+    if (selected.length !== 1) {
+      throw new Error(`Failure finding PR #${n}`);
+    }
+    return selected[0];
+  }
+
   describe("whole repo parsing", () => {
     const parser = new GithubParser("sourcecred/example-repo");
     parser.addData(exampleRepoData.data);
+    const danglingReferences = parser.addReferenceEdges();
     const graph = parser.graph;
 
     it("parses the entire example-repo as expected", () => {
       expect(graph).toMatchSnapshot();
+    });
+
+    it("there are no dangling references in the example repo", () => {
+      expect(danglingReferences).toHaveLength(0);
     });
 
     it("no node or edge has undefined properties in its payload", () => {
@@ -70,36 +92,63 @@ describe("GithubParser", () => {
 
   describe("issue parsing", () => {
     it("parses a simple issue (https://github.com/sourcecred/example-repo/issues/1)", () => {
-      const issue1 = exampleRepoData.data.repository.issues.nodes[0];
-      expect(issue1.number).toBe(1);
       const parser = new GithubParser("sourcecred/example-repo");
-      parser.addIssue(issue1);
+      parser.addIssue(getIssue(1));
       expect(parser.graph).toMatchSnapshot();
     });
 
     it("parses an issue with comments (https://github.com/sourcecred/example-repo/issues/6)", () => {
-      const issue6 = exampleRepoData.data.repository.issues.nodes[3];
-      expect(issue6.number).toBe(6);
       const parser = new GithubParser("sourcecred/example-repo");
-      parser.addIssue(issue6);
+      parser.addIssue(getIssue(6));
       expect(parser.graph).toMatchSnapshot();
     });
   });
 
   describe("pull request parsing", () => {
     it("parses a simple pull request (https://github.com/sourcecred/example-repo/pull/3)", () => {
-      const pr3 = exampleRepoData.data.repository.pullRequests.nodes[0];
-      expect(pr3.number).toBe(3);
       const parser = new GithubParser("sourcecred/example-repo");
-      parser.addPullRequest(pr3);
+      parser.addPullRequest(getPR(3));
       expect(parser.graph).toMatchSnapshot();
     });
     it("parses a pr with review comments (https://github.com/sourcecred/example-repo/pull/3)", () => {
-      const pr5 = exampleRepoData.data.repository.pullRequests.nodes[1];
-      expect(pr5.number).toBe(5);
       const parser = new GithubParser("sourcecred/example-repo");
-      parser.addPullRequest(pr5);
+      parser.addPullRequest(getPR(5));
       expect(parser.graph).toMatchSnapshot();
+    });
+  });
+
+  describe("reference detection", () => {
+    it("discovers a simple reference", () => {
+      const parser = new GithubParser("sourcecred/example-repo");
+      parser.addIssue(getIssue(1));
+      parser.addIssue(getIssue(2));
+      parser.addIssue(getIssue(6));
+      const danglingReferences = parser.addReferenceEdges();
+      expect(parser.graph).toMatchSnapshot();
+      expect(danglingReferences).toHaveLength(0);
+    });
+
+    it("discovers references even when parsing issues out of order", () => {
+      // Ensure that we will detect a reference from A to B, even if B hasn't
+      // been discovered at the time that we parse A.
+      const parserA = new GithubParser("sourcecred/example-repo");
+      parserA.addIssue(getIssue(1));
+      parserA.addIssue(getIssue(2));
+      parserA.addIssue(getIssue(6));
+
+      const parserB = new GithubParser("sourcecred/example-repo");
+      parserB.addIssue(getIssue(2));
+      parserB.addIssue(getIssue(1));
+      parserB.addIssue(getIssue(6));
+
+      expect(parserA.graph.equals(parserB.graph)).toBe(true);
+    });
+
+    it("handles dangling references gracefully", () => {
+      const parser = new GithubParser("sourcecred/example-repo");
+      parser.addIssue(getIssue(2));
+      const danglingReferences = parser.addReferenceEdges();
+      expect(danglingReferences).toMatchSnapshot();
     });
   });
 });
