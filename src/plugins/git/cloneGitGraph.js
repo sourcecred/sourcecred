e66ed45cba8c6a65494089ee731d1fea905d831f
Dandelion Mané <dl@dandelion.io>
2018-05-04 11:35:14 -0700

    Add CLI for printing a fresh Git graph (#206)
    `cloneAndPrintGitGraph` clones a git repository, and generates a Git
    object graph for that repository.
    
    This can be run as follows:
    ```
    yarn backend;
    node bin/cloneAndPrintGitGraph sourcecred example-git
    ```
    
    This commit also adds two utility modules:
    * `cloneAndLoadRepository` , which clones a Git repository to a tmpdir,
    parses the `Repository` data out, and then cleans up.
    * `cloneGitGraph`, which calls `cloneAndLoadRepository` and `createGraph`
    
    Test plan: These don't fit well into our CI, because they require
    network access to clone repositories from GitHub. I verified that the
    functions work via the demo script above.

---
+++ src/plugins/git/cloneGitGraph.js
@@ -0,0 +1,24 @@
+// @flow
+
+import cloneAndLoadRepository from "./cloneAndLoadRepository";
+import {createGraph} from "./createGraph";
+import type {NodePayload, EdgePayload} from "./types";
+import type {Graph} from "../../core/graph";
+
+/**
+ * Load Git contribution graph from a fresh clone of a GitHub repo.
+ *
+ * @param {String} repoOwner
+ *   the GitHub username of the owner of the repository to be cloned
+ * @param {String} repoName
+ *   the name of the repository to be cloned
+ * @return {Graph<NodePayload, EdgePayload>}
+ *   the Git contribution graph
+ */
+export default function fetchGitGraph(
+  repoOwner: string,
+  repoName: string
+): Graph<NodePayload, EdgePayload> {
+  const repo = cloneAndLoadRepository(repoOwner, repoName);
+  return createGraph(repo);
+}
