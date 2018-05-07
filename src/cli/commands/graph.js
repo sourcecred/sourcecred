bbe9fadebda039c6295beb8c617453d50e3c45f5
Dandelion Mané <dl@dandelion.io>
2018-05-07 15:39:29 -0700

    CLI commands error on unhandled promise rejection
    Previously, if a CLI command had an unhandled promise rejection, this
    would result in a spurious success and zero exit value.
    
    This commit causes all of our CLI commands to instead fail if they have
    an unhandled promise rejection.
    
    Test plan: Previously, `sourcecred graph src-d go-git` would claim to
    succeed, although it actually fails due to an unrelated bug. After this
    change is applied, it correctly fails to retrieve the GitHub graph (and
    hte combine step is never run).

--- src/cli/commands/graph.js
+++ src/cli/commands/graph.js
@@ -9,6 +9,13 @@ import {pluginNames} from "../common";
 
 const execDependencyGraph = require("../../tools/execDependencyGraph").default;
 
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on("unhandledRejection", (err) => {
+  throw err;
+});
+
 export default class GraphCommand extends Command {
   static description = `\
 create the contribution graph for a repository
