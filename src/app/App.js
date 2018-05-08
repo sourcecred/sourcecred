e1808d1126dd88bfc1389d84455c2ad71804efca
Dandelion Mané <dl@dandelion.io>
2018-05-08 12:09:20 -0700

    Add src/app/App.js
    This commit adds src/app/App.js, which proxies in the frontend from
    src/plugins/artifact/editor/App.js. The observed behavior (run `yarn
    start`; see Artifact Editor) is unchanged.
    
    Test plan: Observe that `yarn start` has the same behavior, and travis
    passes.

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+// @flow
+
+import React from "react";
+import ArtifactEditor from "../plugins/artifact/editor/App";
+
+export default class App extends React.Component<{}> {
+  render() {
+    return <ArtifactEditor />;
+  }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "../plugins/artifact/editor/App";
+import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 const root = document.getElementById("root");
