c459e67074993c5d00e3091de09e0d12f8a6b8e3
Dandelion Mané <dl@dandelion.io>
2018-05-08 11:50:52 -0700

    Turn on flow for index.js
    Test plan: `yarn travis` passes

--- src/plugins/artifact/editor/index.js
+++ src/plugins/artifact/editor/index.js
@@ -1,9 +1,13 @@
-// @no-flow
+// @flow
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = document.getElementById("root");
+if (root == null) {
+  throw new Error("Unable to find root element!");
+}
+ReactDOM.render(<App />, root);
 registerServiceWorker();
