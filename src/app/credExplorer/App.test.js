ac8d0ff66caf740f398c0e18e14d8106b5de773a
Dandelion Mané <dl@dandelion.io>
2018-05-08 17:05:56 -0700

    Setup credExplorer app scaffold (#249)
    Test plan: Run `yarn start`, and observe that the Cred Explorer is now
    included in the nav bar, and can be selected, in which case a short
    message is displayed.

---
+++ src/app/credExplorer/App.test.js
@@ -0,0 +1,13 @@
+// @flow
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+require("../testUtil").configureAphrodite();
+
+// Check that PropTypes check out.
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
