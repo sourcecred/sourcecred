16232159c25e69ffef134631bec072474e6c2986
William Chargin <wchargin@gmail.com>
2018-05-10 12:02:50 -0700

    Add `git/render.js` with `nodeDescription`
    Test Plan:
    Unit tests added, with full coverage of reachable cases.
    
    wchargin-branch: git-render

---
+++ src/plugins/git/render.js
@@ -0,0 +1,30 @@
+// @flow
+
+import type {Address} from "../../core/address";
+import type {Graph} from "../../core/graph";
+import type {NodeType, SubmoduleCommitPayload} from "./types";
+
+/**
+ * Describe a node provided by the Git plugin.
+ */
+export function nodeDescription(graph: Graph<any, any>, address: Address) {
+  const type: NodeType = (address.type: any);
+  switch (type) {
+    case "COMMIT":
+      return `commit ${address.id}`;
+    case "TREE":
+      return `tree ${address.id}`;
+    case "BLOB":
+      return `blob ${address.id}`;
+    case "SUBMODULE_COMMIT": {
+      const payload: SubmoduleCommitPayload = graph.node(address).payload;
+      return `submodule commit ${payload.hash} in ${payload.url}`;
+    }
+    case "TREE_ENTRY":
+      return `entry ${address.id}`;
+    default:
+      // eslint-disable-next-line no-unused-expressions
+      (type: empty);
+      throw new Error(`unknown type: ${type}`);
+  }
+}
