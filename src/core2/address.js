e59fecd8e7a7c2cb6f002de3d0c059df6cbddee9
Dandelion Mané <decentralion@dandelion.io>
2018-05-28 10:21:27 -0700

    Setup `src/core2` as graph refactor staging area
    In #190, @wchargin lays out an ambitious proposal for refactoring away
    the graph's address-payload distinction. This has proven to be a
    complicated refactor to land, so we are going to achieve it by forking
    parts of the project into v2, updating incrementally in v2, and then
    replacing original components with their v2 versions.
    
    Test plan:
    No new code added. `yarn travis` passes.
    
    Paired with @wchargin

---
+++ src/core2/address.js
@@ -0,0 +1,195 @@
+// @flow
+
+import deepEqual from "lodash.isequal";
+import stringify from "json-stable-stringify";
+
+import {toCompat, fromCompat} from "../util/compat";
+import type {Compatible} from "../util/compat";
+
+export type Address = {|
+  +pluginName: string,
+  +id: string,
+  +type: string,
+|};
+
+export interface Addressable {
+  +address: Address;
+}
+
+export type SansAddress<T: Addressable> = $Exact<$Diff<T, {+address: Address}>>;
+
+export type AddressMapJSON<T: Addressable> = Compatible<{
+  [serializedAddress: string]: SansAddress<T>,
+}>;
+
+export const COMPAT_TYPE = "sourcecred/sourcecred/AddressMap";
+export const COMPAT_VERSION = "0.1.0";
+
+/**
+ * A data structure for storing addressable objects, keyed by their
+ * addresses.
+ */
+export class AddressMap<T: Addressable> {
+  /*
+   * Nested structure for fast access.
+   *
+   * It is an representation invariant that there are no empty objects
+   * in this structure (except possibly the top-level object). In
+   * particular, if `_data[somePluginName]` is not `undefined`, then it
+   * is a non-empty object. This is required so that two `AddressMap`s
+   * are logically equal exactly if their `_data` fields are deep-equal.
+   *
+   * This nested structure is significantly more performant than a
+   * simpler version using a flat object keyed by `stringify(address)`.
+   * For basic performance tests, see:
+   *     https://jsperf.com/address-string-302039074
+   */
+  _data: {[pluginName: string]: {[type: string]: {[id: string]: T}}};
+
+  /**
+   * Create an empty `AddressMap`.
+   */
+  constructor() {
+    this._data = {};
+  }
+
+  /**
+   * Test whether this map logically equals another map. Two maps are
+   * logically equal if they contain the same keys and the values at
+   * each key are deep-equal.
+   */
+  equals(that: AddressMap<T>): boolean {
+    return deepEqual(this._data, that._data);
+  }
+
+  toJSON(): AddressMapJSON<T> {
+    const result = {};
+    Object.keys(this._data).forEach((pluginName) => {
+      const dataForPluginName = this._data[pluginName];
+      Object.keys(dataForPluginName).forEach((type) => {
+        const dataForType = dataForPluginName[type];
+        Object.keys(dataForType).forEach((id) => {
+          const address = {pluginName, id, type};
+          const datum = {...dataForType[id]};
+          delete datum.address;
+          result[toString(address)] = datum;
+        });
+      });
+    });
+    return toCompat(
+      {
+        type: COMPAT_TYPE,
+        version: COMPAT_VERSION,
+      },
+      result
+    );
+  }
+
+  static fromJSON(json: AddressMapJSON<T>): AddressMap<T> {
+    const decompat = fromCompat(
+      {
+        type: COMPAT_TYPE,
+        version: COMPAT_VERSION,
+      },
+      json
+    );
+    const result: AddressMap<T> = new AddressMap();
+    Object.keys(decompat).forEach((key) => {
+      result.add({...decompat[key], address: JSON.parse(key)});
+    });
+    return result;
+  }
+
+  /**
+   * Add the given object to the map, replacing any existing value for
+   * the same address.
+   *
+   * Returns `this` for easy chaining.
+   */
+  add(t: T): this {
+    const {address} = t;
+    if (address == null) {
+      throw new Error(`address is ${String(t.address)}`);
+    }
+    let dataForPluginName = this._data[address.pluginName];
+    if (dataForPluginName === undefined) {
+      this._data[address.pluginName] = dataForPluginName = {};
+    }
+    let dataForType = dataForPluginName[address.type];
+    if (dataForType === undefined) {
+      dataForPluginName[address.type] = dataForType = {};
+    }
+    dataForType[address.id] = t;
+    return this;
+  }
+
+  /**
+   * Get the object at the given address, if it exists, or `undefined`
+   * otherwise.
+   */
+  get(address: Address): T {
+    if (address == null) {
+      throw new Error(`address is ${String(address)}`);
+    }
+    const dataForPluginName = this._data[address.pluginName];
+    if (dataForPluginName === undefined) {
+      return (undefined: any);
+    }
+    const dataForType = dataForPluginName[address.type];
+    if (dataForType === undefined) {
+      return (undefined: any);
+    }
+    return dataForType[address.id];
+  }
+
+  /**
+   * Get all objects stored in the map, in some unspecified order.
+   */
+  getAll(): T[] {
+    const result = [];
+    Object.keys(this._data).forEach((pluginName) => {
+      const dataForPluginName = this._data[pluginName];
+      Object.keys(dataForPluginName).forEach((type) => {
+        const dataForType = dataForPluginName[type];
+        Object.keys(dataForType).forEach((id) => {
+          result.push(dataForType[id]);
+        });
+      });
+    });
+    return result;
+  }
+
+  /**
+   * Remove any object with the given address. If none exists, this
+   * method does nothing.
+   */
+  remove(address: Address): this {
+    if (address == null) {
+      throw new Error(`address is ${String(address)}`);
+    }
+    const dataForPluginName = this._data[address.pluginName];
+    if (dataForPluginName === undefined) {
+      return this;
+    }
+    const dataForType = dataForPluginName[address.type];
+    if (dataForType === undefined) {
+      return this;
+    }
+    delete dataForType[address.id];
+    if (Object.keys(dataForType).length === 0) {
+      delete dataForPluginName[address.type];
+      if (Object.keys(dataForPluginName).length === 0) {
+        delete this._data[address.pluginName];
+      }
+    }
+    return this;
+  }
+}
+
+export function toString(x: Address): string {
+  return stringify(x);
+}
+
+export function fromString(s: string): Address {
+  return JSON.parse(s);
+}
