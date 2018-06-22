2dec8868db069976fb3c270b8f18b7afaaaeb7d4
Dandelion Mané <decentralion@dandelion.io>
2018-06-22 16:16:48 -0700

    Copy `github/findReferences` from v1 to v3 (#410)
    The code will be refactored so that references are expressed in terms of
    the GitHub node address code; the implementation is copied first so that
    the review will be cleaner.
    
    Test plan:
    `yarn travis` passes.

---
+++ src/v3/plugins/github/findReferences.js
@@ -0,0 +1,58 @@
+// @flow
+
+function findAllMatches(re: RegExp, s: string): any[] {
+  // modified from: https://stackoverflow.com/a/6323598
+  let m;
+  const matches = [];
+  do {
+    m = re.exec(s);
+    if (m) {
+      matches.push(m);
+    }
+  } while (m);
+  return matches;
+}
+
+export function findReferences(body: string): string[] {
+  // Note to maintainer: If it becomes necessary to encode references in a
+  // richer format, consider implementing the type signature described in
+  // https://github.com/sourcecred/sourcecred/pull/130#pullrequestreview-113849998
+  return [
+    ...findNumericReferences(body),
+    ...findGithubUrlReferences(body),
+    ...findUsernameReferences(body),
+  ];
+}
+
+function findNumericReferences(body: string): string[] {
+  return findAllMatches(/(?:\W|^)(#\d+)(?:\W|$)/g, body).map((x) => x[1]);
+}
+
+function findUsernameReferences(body: string): string[] {
+  return findAllMatches(/(?:\W|^)(@[a-zA-Z0-9-]+)(?:\W|$)/g, body).map(
+    (x) => x[1]
+  );
+}
+
+function findGithubUrlReferences(body: string): string[] {
+  const githubNamePart = /(?:[a-zA-Z0-9_-]+)/.source;
+  const urlRegex = new RegExp(
+    "" +
+      /(?:\W|^)/.source +
+      "(" +
+      /http(?:s)?:\/\/github.com\//.source +
+      githubNamePart +
+      "(?:" +
+      /\//.source +
+      githubNamePart +
+      /\/(?:issues|pull)\//.source +
+      /(?:\d+)/.source +
+      /(?:#(?:issue|issuecomment|pullrequestreview|discussion_r)-?(?:\d+))?/
+        .source +
+      ")?" +
+      ")" +
+      /(?:[^\w/]|$)/.source,
+    "gm"
+  );
+  return findAllMatches(urlRegex, body).map((match) => match[1]);
+}
