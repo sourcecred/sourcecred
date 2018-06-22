a209caeec21e513d7f2cdd9b893cac2f25241c61
Dandelion Mané <decentralion@dandelion.io>
2018-06-22 13:10:19 -0700

    create the GitHub graph (#405)
    This commit:
    - adds `github/createGraph.js`
      - which ingests GitHub GraphQL response
      - and creates a GitHub graph
    - adds `github/graphView.js`,
      - which takes a Graph
      - and validates that all GitHub specific node and edge invariants hold
        - every github node may be parsed by `github/node/fromRaw`
          - with the right node type
        - every github edge may be parsed by `github/edge/fromRaw`
          - with the right edge type
          - with the right src address prefix
          - with the right dst address prefix
        - every child node has exactly one parent
          - of the right type
      - and provides convenient porcelain methods for
        - finding repos in the graph
        - finding issues of a repo
        - finding pulls of a repo
        - finding reviews of a pull
        - finding comments of a Commentable
        - finding authors of Authorables
        - finding parent of a ChildAddress
    - tests `createGraph`
      - via snapshot testing
      - by checking the GraphView invariants hold
    - tests `graphView`
      - by checking individual entities in the example-git repository have
      the proper relationships
      - by checking that for every class of invariant, errors are thrown if
      the invariant is violated
    
    Test plan:
    - Extensive unit and snapshot tests added. `yarn travis` passes.

---
+++ src/v3/plugins/github/createGraph.test.js
@@ -0,0 +1,36 @@
+// @flow
+
+import {createGraph} from "./createGraph";
+import {GraphView} from "./graphView";
+import cloneDeep from "lodash.clonedeep";
+
+function exampleGraph() {
+  const data = cloneDeep(require("./demoData/example-github"));
+  return createGraph(data);
+}
+
+describe("plugins/github/createGraph", () => {
+  it("example graph matches snapshot", () => {
+    expect(exampleGraph()).toMatchSnapshot();
+  });
+
+  it("passes all GraphView invariants", () => {
+    const graph = exampleGraph();
+    const view = new GraphView(graph);
+    // This test is high leverage. It checks:
+    // - that every node starting with a GitHub prefix
+    //   - can be structured using fromRaw
+    //   - has the correct type
+    // - that every edge starting with a GitHub prefix
+    //   - can be structured using fromRaw
+    //   - and has the correct type,
+    //   - and that its src has an expected prefix,
+    //   - and that its dst has an expected prefix,
+    // - that every child node
+    //   - has exactly one parent
+    //   - has a parent with the correct type
+    view.checkInvariants();
+    // as currently written, GV checks invariants on construction.
+    // we call the method explicitly as a defensive step.
+  });
+});
