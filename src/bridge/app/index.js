321bf442cc14b68221e556abd1111d51fab486bb
William Chargin <wchargin@gmail.com>
2018-06-29 12:10:38 -0700

    Add a “version select” to the bridged app
    Summary:
    The bridge now lets you select any version of the app that you want, as
    long as that’s V1, because that’s the only version that exists. We’ll
    add a V3 version shortly.
    
    This implements Step 2 of the plan described in #448.
    
    Test Plan:
    In `yarn start` and `node bin/sourcecred.js start`, note that navigating
    to `/` redirects to `/v1`, and that the cred explorer works.
    
    wchargin-branch: bridge-select

--- src/bridge/app/index.js
+++ src/bridge/app/index.js
@@ -1,6 +1,12 @@
 // @flow
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Redirect,
+  Route,
+} from "react-router-dom";
 import "./index.css";
 import V1App from "../../v1/app/App";
 import registerServiceWorker from "./registerServiceWorker";
@@ -9,5 +15,20 @@ const root = document.getElementById("root");
 if (root == null) {
   throw new Error("Unable to find root element!");
 }
-ReactDOM.render(<V1App />, root);
+ReactDOM.render(
+  <Router>
+    <React.Fragment>
+      <strong>Select version:</strong>
+      <ul>
+        <li>
+          <NavLink to="/v1">V1</NavLink>
+        </li>
+      </ul>
+      <Route exact path="/" render={() => <Redirect to="/v1" />} />
+      <hr />
+      <Route path="/v1" component={V1App} />
+    </React.Fragment>
+  </Router>,
+  root
+);
 registerServiceWorker();
