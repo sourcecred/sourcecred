fe64377194304046e3b82d8992b0b8b3f730223f
Dandelion Mané <decentralion@dandelion.io>
2018-06-29 14:24:28 -0700

    Add the `attribution/pagerank` module (#455)
    This module exposes a method, `pagerank`, which is a convenient entry
    point for taking a `Graph` and returning a `PagerankResult`. This
    obviates the need for `src/v1/app/credExplorer/basicPagerank.js`.
    
    Test plan: Unit tests included.

---
+++ src/v3/core/attribution/pagerank.test.js
@@ -0,0 +1,32 @@
+// @flow
+
+import {pagerank} from "./pagerank";
+import {NodeAddress} from "../graph";
+import {advancedGraph} from "../graphTestUtil";
+
+function snapshotPagerankResult(result) {
+  const partsToProbability = [];
+  const sortedKeys = Array.from(result.keys()).sort();
+  for (const key of sortedKeys) {
+    const probability = result.get(key);
+    const parts = NodeAddress.toParts((key: any));
+    partsToProbability.push({parts, probability});
+  }
+  expect(partsToProbability).toMatchSnapshot();
+}
+
+describe("core/attribution/pagerank", () => {
+  function edgeWeight(_unused_edge) {
+    return {toWeight: 1, froWeight: 0};
+  }
+  it("snapshots as expected on the advanced graph", () => {
+    const pagerankResult = pagerank(advancedGraph().graph1(), edgeWeight);
+    snapshotPagerankResult(pagerankResult);
+  });
+  it("respects explicit arguments", () => {
+    const pagerankResult = pagerank(advancedGraph().graph1(), edgeWeight, {
+      maxIterations: 0,
+    });
+    snapshotPagerankResult(pagerankResult);
+  });
+});
