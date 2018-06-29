67568feaf037e3b7faf9f9ceb2a3b588c0f4e84e
Dandelion Mané <decentralion@dandelion.io>
2018-06-29 13:51:53 -0700

    Add the `attribution/pagerank` module
    This module exposes a method, `pagerank`, which is a convenient entry
    point for taking a `Graph` and returning a `PagerankResult`. This
    obviates the need for `src/v1/app/credExplorer/basicPagerank.js`.
    
    Test plan: Unit tests included.

---
+++ src/v3/core/attribution/pagerank.js
@@ -0,0 +1,49 @@
+// @flow
+
+import {type Edge, Graph} from "../graph";
+import {
+  type PagerankResult,
+  distributionToPagerankResult,
+  graphToOrderedSparseMarkovChain,
+  type EdgeWeight,
+} from "./graphToMarkovChain";
+
+import {findStationaryDistribution} from "./markovChain";
+
+export type PagerankOptions = {|
+  +selfLoopWeight?: number,
+  +verbose?: boolean,
+  +convergenceThreshold?: number,
+  +maxIterations?: number,
+|};
+
+function defaultOptions(): PagerankOptions {
+  return {
+    verbose: false,
+    selfLoopWeight: 1e-3,
+    convergenceThreshold: 1e-7,
+    maxIterations: 255,
+  };
+}
+
+export function pagerank(
+  graph: Graph,
+  edgeWeight: (Edge) => EdgeWeight,
+  options?: PagerankOptions
+): PagerankResult {
+  const fullOptions = {
+    ...defaultOptions(),
+    ...(options || {}),
+  };
+  const osmc = graphToOrderedSparseMarkovChain(
+    graph,
+    edgeWeight,
+    fullOptions.selfLoopWeight
+  );
+  const distribution = findStationaryDistribution(osmc.chain, {
+    verbose: fullOptions.verbose,
+    convergenceThreshold: fullOptions.convergenceThreshold,
+    maxIterations: fullOptions.maxIterations,
+  });
+  return distributionToPagerankResult(osmc.nodeOrder, distribution);
+}
