0ddb3466106e6d7249233b781d088c293c8bb40d
William Chargin <wchargin@gmail.com>
2018-07-20 23:19:40 -0700

    Implement a first-pass static site generation
    Summary:
    Some of the code here is adapted from my site (source available on
    GitHub at wchargin/wchargin.github.io). It has been improved when
    possible and made worse when necessary to fit into our existing build
    system with minimal churn.
    
    As of this commit, there remain the following outstanding tasks:
      - Use a non-hardcoded list of paths in static site generation router.
        This is not trivial. We have the paths nicely available in
        `routes.js`, but this module is written in ES6, and transitively
        depends on many files written in ES6 (i.e., the whole app). Yet
        naïvely it would be required from a Webpack config file, which is
        interpreted as vanilla JavaScript.
      - Add `csso-loader` to minify our CSS. This is easy.
      - Add unit tests for `dedent`. (As is, it comes from my site
        verbatim. I wrote it. dmnd’s `dedent` package on npm is insufficient
        because it dedents arguments as well as the format string, which is
        incorrect at least for our purposes.)
      - Link in canonical static data for the site.
      - Rip out the whole build system and replace it with my build config,
        which is orders of magnitude saner and less bad. (By “the whole
        build system” I mostly mean `webpack.config.{dev,prod}.js`.)
    
    Test Plan:
    
    ```shell
    $ yarn backend
    $ yarn build
    $ node ./bin/sourcecred.js start
    ```
    
    wchargin-branch: static-v0

---
+++ src/app/dedent.js
@@ -0,0 +1,41 @@
+// @flow
+
+/*
+ * A template tag function that performs dedenting on the template, but
+ * not its arguments.
+ *
+ * For instance, given the template
+ *
+ *     |dedent`\
+ *     |    one ${one}
+ *     |        two ${two}
+ *     |    done`,
+ *
+ * where `one === "1"` and `two === "\n    2"`, the template string
+ * would expand to "one 1\n    two\n    2\ndone". Note that four spaces
+ * of indentation were stripped off of each of "one" and "two", but not
+ * from "2".
+ *
+ * Lines that contain only whitespace are not used for measuring.
+ */
+export default function dedent(strings: string[], ...values: string[]) {
+  const lineLengths = strings
+    .join("")
+    .split("\n")
+    .filter((line) => line.trim().length !== 0)
+    .map((line) => line.length - line.trimLeft().length);
+  const trimAmount = Math.min.apply(null, lineLengths);
+
+  const parts = [];
+  for (let i = 0; i < strings.length; i++) {
+    const trimmed = strings[i]
+      .split("\n")
+      .map((line, j) => (i === 0 || j > 0 ? line.substr(trimAmount) : line))
+      .join("\n");
+    parts.push(trimmed);
+    if (i < values.length) {
+      parts.push(values[i]);
+    }
+  }
+  return parts.join("");
+}
---
+++ src/app/server.js
@@ -0,0 +1,50 @@
+// @flow
+
+import {StyleSheetServer} from "aphrodite/no-important";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import {match, RouterContext} from "react-router";
+
+import {createRoutes, resolveTitleFromPath} from "./routes";
+import dedent from "./dedent";
+
+export default function render(
+  locals: {+path: string, +assets: {[string]: string}},
+  callback: (error: ?mixed, result?: string) => void
+): void {
+  const bundlePath = locals.assets["main"];
+  const url = locals.path;
+  const routes = createRoutes();
+  match({routes, location: url}, (error, redirectLocation, renderProps) => {
+    if (error) {
+      callback(error);
+    } else if (renderProps) {
+      const component = <RouterContext {...renderProps} />;
+      const {html, css} = StyleSheetServer.renderStatic(() =>
+        ReactDOMServer.renderToString(component)
+      );
+      const page = dedent`\
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>${resolveTitleFromPath(url)}</title>
+        <style>${require("./index.css")}</style>
+        <style data-aphrodite>${css.content}</style>
+        </head>
+        <body style="overflow-y:scroll">
+        <div id="root">${html}</div>
+        <script src="${bundlePath}"></script>
+        </body>
+        </html>
+      `;
+      callback(null, page);
+    } else {
+      // This shouldn't happen because we should only be visiting
+      // the right routes.
+      throw new Error(`unexpected 404 from ${url}`);
+    }
+  });
+}
