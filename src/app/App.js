b0a55acb53bfa2ff2b7d796ded44f0b87353d36d
William Chargin <wchargin@gmail.com>
2018-07-20 18:50:47 -0700

    Add basic react-router@3 routing
    Summary:
    This commit introduces a `Page` component that we can use to provide
    common styling and navigation to our pages.
    
    wchargin-branch: use-rrv3

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,19 @@
 // @flow
 
 import React from "react";
+import {IndexRoute, Route, Router, browserHistory} from "react-router";
 
+import Page from "./Page";
 import CredExplorer from "./credExplorer/App";
 
 export default class App extends React.Component<{}> {
   render() {
-    return <CredExplorer />;
+    return (
+      <Router history={browserHistory}>
+        <Route path="/" component={Page}>
+          <IndexRoute component={CredExplorer} />
+        </Route>
+      </Router>
+    );
   }
 }
