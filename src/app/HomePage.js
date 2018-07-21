256582d8b3065bd1c79dcc251d16aba8fb5e9fc3
William Chargin <wchargin@gmail.com>
2018-07-20 19:03:38 -0700

    Add a landing page
    Summary:
    This adds a dummy landing page. We’ll want to actually put nice content
    on it. For development convenience, I’m totally fine with having the
    `yarn start` launch `/explorer` instead of just `/`.
    
    Test Plan:
    Run `yarn start` and note that the navigation works.
    
    wchargin-branch: landing-page

---
+++ src/app/HomePage.js
@@ -0,0 +1,9 @@
+// @flow
+
+import React from "react";
+
+export default class HomePage extends React.Component<{||}> {
+  render() {
+    return <h1>Home</h1>;
+  }
+}
--- src/app/Page.js
+++ src/app/Page.js
@@ -1,11 +1,28 @@
 // @flow
 
 import React, {type Node} from "react";
+import {Link} from "react-router";
+
+import {routeData} from "./routes";
+import * as NullUtil from "../util/null";
 
 export default class Page extends React.Component<{|+children: Node|}> {
   render() {
     return (
       <div>
+        <header>
+          <nav>
+            <ul style={{listStyle: "none", paddingLeft: 0, margin: 0}}>
+              {routeData.map(({navTitle, path}) =>
+                NullUtil.map(navTitle, (navTitle) => (
+                  <li key={path} style={{display: "inline", marginRight: 10}}>
+                    <Link to={path}>{navTitle}</Link>
+                  </li>
+                ))
+              )}
+            </ul>
+          </nav>
+        </header>
         <main>{this.props.children}</main>
       </div>
     );
