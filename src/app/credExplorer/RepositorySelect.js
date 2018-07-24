cd1d4247a62bf33022e100de47f47373a44086a2
Dandelion Mané <decentralion@dandelion.io>
2018-07-24 15:44:10 -0700

    Convert Explorer repository selection to dropdown
    Context: The Cred Explorer loads data (currently on a per-repository
    basis) that has previously been prepared by running the `sourcecred
    load` cli command.
    
    Currently, to select a repository to load, the user must manually type
    the repository owner and name. This is a confusing UI, because it
    suggests that any repository may be chosen, when in fact only repos
    already loaded into the data store are available. The user is given no
    feedback as to which repositories are valid options.
    
    As of #516, the backend stores a registry listing available
    repositories. This commit adds a `RepositorySelect` component which
    loads the available from that registry, and makes them available in a
    dropdown, in sorted order.
    
    When the user manually selects one of the repositories, that selection
    is persisted into localStorage and respected on future loads. If the
    user hasn't made such a choice, then the first repository is selected by
    default.
    
    The implementation is highly influenced by testability considerations.
    The default export, `<RepositorySelect onChange={} localStore={} />`, is
    pretty straightforward. The `RepositorySelect` is somewhat cumbersome to
    test because it asynchronously fetches data and then updates its state,
    which affects the render output. So as to avoid testing inside async
    react components wherever possible, I've factored out:
    
    * `loadStatus`, which uses fetch and localStore to get the status of the
    selector.
    * `PureRepositorySelect`, which just renders a `Status`, such as
    loading, failure, or valid
    * `LocalStoreRepositorySelect`, which wraps the `PureRepositorySelect`
    with logic to bind the repository select to localStore on change.
    
    Test plan: Extensive unit tests were added. Also, to ensure that the
    tests were testing the right thing, I manually tested:
    - attempting to load invalid registry
    - attempting to load with no registry
    - attempting to load with empty registry
    - loading without valid localStore
    - changing the setting via dropdown
    - loading from localStore after changing the dropdown
    
    And all behavior was as expected.
    
    Thanks to @wchargin for considerable help testing this PR.

---
+++ src/app/credExplorer/RepositorySelect.js
@@ -0,0 +1,188 @@
+// @flow
+
+import React, {type Node} from "react";
+import sortBy from "lodash.sortby";
+import deepEqual from "lodash.isequal";
+
+import * as NullUtil from "../../util/null";
+import type {LocalStore} from "../localStore";
+
+export const REPO_REGISTRY_API = "/api/v1/data/repositoryRegistry.json";
+export const REPO_KEY = "selectedRepository";
+
+export type Repo = {|+name: string, +owner: string|};
+
+export type Status =
+  | {|+type: "LOADING"|}
+  | {|
+      +type: "VALID",
+      +availableRepos: $ReadOnlyArray<Repo>,
+      +selectedRepo: Repo,
+    |}
+  | {|+type: "NO_REPOS"|}
+  | {|+type: "FAILURE"|};
+
+type Props = {|
+  +onChange: (x: Repo) => void,
+  +localStore: LocalStore,
+|};
+type State = {|status: Status|};
+export default class RepositorySelect extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      status: {type: "LOADING"},
+    };
+  }
+
+  componentDidMount() {
+    loadStatus(this.props.localStore).then((status) => {
+      this.setState({status});
+      if (status.type === "VALID") {
+        this.props.onChange(status.selectedRepo);
+      }
+    });
+  }
+
+  onChange(selectedRepo: Repo) {
+    const status = this.state.status;
+    if (status.type === "VALID") {
+      const newStatus = {...status, selectedRepo};
+      this.setState({status: newStatus});
+    }
+    this.props.onChange(selectedRepo);
+  }
+
+  render() {
+    return (
+      <LocalStoreRepositorySelect
+        onChange={(selectedRepo) => this.onChange(selectedRepo)}
+        status={this.state.status}
+        localStore={this.props.localStore}
+      >
+        {({status, onChange}) => (
+          <PureRepositorySelect onChange={onChange} status={status} />
+        )}
+      </LocalStoreRepositorySelect>
+    );
+  }
+}
+
+function validateRepo(repo: Repo) {
+  const validRe = /^[A-Za-z0-9_-]+$/;
+  if (!repo.owner.match(validRe)) {
+    throw new Error(`Invalid repository owner: ${JSON.stringify(repo.owner)}`);
+  }
+  if (!repo.name.match(validRe)) {
+    throw new Error(`Invalid repository name: ${JSON.stringify(repo.name)}`);
+  }
+}
+
+function repoStringToRepo(x: string): Repo {
+  const pieces = x.split("/");
+  if (pieces.length !== 2) {
+    throw new Error(`Invalid repo string: ${x}`);
+  }
+  const repo = {owner: pieces[0], name: pieces[1]};
+  validateRepo(repo);
+  return repo;
+}
+
+export async function loadStatus(localStore: LocalStore): Promise<Status> {
+  try {
+    const response = await fetch(REPO_REGISTRY_API);
+    if (!response.ok) {
+      return {type: "FAILURE"};
+    }
+    const json = await response.json();
+    let availableRepos = Object.keys(json).map(repoStringToRepo);
+    availableRepos = sortBy(availableRepos, (r) => r.owner, (r) => r.name);
+    if (availableRepos.length === 0) {
+      return {type: "NO_REPOS"};
+    }
+    const localStoreRepo = localStore.get(REPO_KEY, null);
+    const selectedRepo = NullUtil.orElse(
+      availableRepos.find((x) => deepEqual(x, localStoreRepo)),
+      availableRepos[0]
+    );
+    return {type: "VALID", availableRepos, selectedRepo};
+  } catch (e) {
+    return {type: "FAILURE"};
+  }
+}
+
+export class LocalStoreRepositorySelect extends React.Component<{|
+  +status: Status,
+  +onChange: (repo: Repo) => void,
+  +localStore: LocalStore,
+  +children: ({
+    status: Status,
+    onChange: (selectedRepo: Repo) => void,
+  }) => Node,
+|}> {
+  render() {
+    return this.props.children({
+      status: this.props.status,
+      onChange: (repo) => {
+        this.props.onChange(repo);
+        this.props.localStore.set(REPO_KEY, repo);
+      },
+    });
+  }
+}
+
+type PureRepositorySelectProps = {|
+  +onChange: (x: Repo) => void,
+  +status: Status,
+|};
+export class PureRepositorySelect extends React.PureComponent<
+  PureRepositorySelectProps
+> {
+  renderSelect(availableRepos: $ReadOnlyArray<Repo>, selectedRepo: ?Repo) {
+    return (
+      <label>
+        <span>Please choose a repository to inspect:</span>{" "}
+        {selectedRepo != null && (
+          <select
+            value={`${selectedRepo.owner}/${selectedRepo.name}`}
+            onChange={(e) => {
+              const repoString = e.target.value;
+              const repo = repoStringToRepo(repoString);
+              this.props.onChange(repo);
+            }}
+          >
+            {availableRepos.map(({owner, name}) => {
+              const repoString = `${owner}/${name}`;
+              return (
+                <option value={repoString} key={repoString}>
+                  {repoString}
+                </option>
+              );
+            })}
+          </select>
+        )}
+      </label>
+    );
+  }
+
+  renderError(text: string) {
+    return <span style={{fontWeight: "bold", color: "red"}}>{text}</span>;
+  }
+
+  render() {
+    const {status} = this.props;
+    switch (status.type) {
+      case "LOADING":
+        // Just show an empty select while we wait.
+        return this.renderSelect([], null);
+      case "VALID":
+        return this.renderSelect(status.availableRepos, status.selectedRepo);
+      case "NO_REPOS":
+        return this.renderError("Error: No repositories found.");
+      case "FAILURE":
+        return this.renderError("Error: Unable to load repository registry.");
+      default:
+        throw new Error((status.type: empty));
+    }
+  }
+}
