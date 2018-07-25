d0906eed16a36c4b754dbbe8406923cf8d87ca7f
William Chargin <wchargin@gmail.com>
2018-07-24 19:00:39 -0700

    Add a `checkedLocalStore` implementation (#523)
    Summary:
    This provides some extra checking around `LocalStore` calls. In
    particular, it fails fast on the nasty bug where storing a `Map`
    actually stores the empty object (`JSON.stringify(new Map()) === "{}"`).
    Similarly, retrieving a value that was stored as `undefined` will raise
    an error, because `JSON.parse(JSON.stringify(undefined))` raises an
    error.
    
    This should have negligible performance impact—local storage access
    should never be on a critical path. We can choose to elide this in
    production if we want.
    
    Test Plan:
    Unit tests added. Manual testing of the cred explorer yields no errors.
    
    wchargin-branch: checked-local-store

---
+++ src/app/checkedLocalStore.js
@@ -0,0 +1,49 @@
+// @flow
+
+import deepEqual from "lodash.isequal";
+
+import {LocalStore} from "./localStore";
+
+/*
+ * A wrapper for a `LocalStore` that adds contract checking. This
+ * implementation verifies that all provided keys are strings and all
+ * provided values are plain old JSON values.
+ */
+export default class CheckedLocalStore implements LocalStore {
+  _base: LocalStore;
+
+  constructor(base: LocalStore): void {
+    this._base = base;
+  }
+
+  get<T>(key: string, whenUnavailable: T): T {
+    this._validateKey(key);
+    return this._base.get(key, whenUnavailable);
+  }
+
+  set(key: string, data: mixed): void {
+    this._validateKey(key);
+    this._validateValue(data);
+    return this._base.set(key, data);
+  }
+
+  del(key: string): void {
+    this._validateKey(key);
+    return this._base.del(key);
+  }
+
+  _validateKey(key: string) {
+    if (typeof key !== "string") {
+      throw new Error(`bad key (${typeof key}): ${key}`);
+    }
+  }
+
+  _validateValue(data: any) {
+    try {
+      if (deepEqual(data, JSON.parse(JSON.stringify(data)))) {
+        return;
+      }
+    } catch (_) {}
+    throw new Error(`bad value: ${data}`);
+  }
+}
