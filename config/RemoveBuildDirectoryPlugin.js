480bdf1bc74669064bd20f7d026af255950cb951
William Chargin <wchargin@gmail.com>
2018-07-31 15:27:32 -0700

    Refine the build directory cleaning logic (#577)
    Summary:
    We were asking the `clean-webpack-plugin` to remove the `build/`
    directory in all cases. However, Webpack accepts a command-line
    parameter `--output-path`. When such a parameter is passed, we would be
    removing the wrong directory.
    
    The proper behavior is to remove “whatever the actual output path is”.
    Webpack exposes this information, but it appears that the
    `clean-webpack-plugin` does not take advantage of it. Therefore, this
    commit includes a small Webpack plugin to do the right thing.
    
    Test Plan:
    Test that the behavior is correct when no output directory is specified:
    ```
    mkdir -p build && touch build/wat && yarn build && ! [ -e build/wat ]
    ```
    
    Test that the behavior is correct with an explicit `--output-path`:
    ```
    outdir="$(mktemp -d)" && touch "${outdir}/wat" && \
        yarn build --output-path "${outdir}" && \
        ! [ -e "${outdir}/wat" ]
    ```
    
    Test that the plugin refuses to remove the root directory:
    
    ```
    ! yarn build --output-path . && \
        sed -i '/path: /d' config/makeWebpackConfig.js && ! yarn build
    ```
    
    (Feel free to comment out the actual `rimraf.sync` line in the plugin
    when testing this.)
    
    wchargin-branch: clean-actual-build-directory

---
+++ config/RemoveBuildDirectoryPlugin.js
@@ -0,0 +1,39 @@
+// @flow
+
+const fs = require("fs");
+const path = require("path");
+const rimraf = require("rimraf");
+
+// Note: the following type-import just resolves to `any`.
+/*:: import type {Compiler} from "webpack"; */
+
+module.exports = class RemoveBuildDirectoryPlugin {
+  apply(compiler /*: Compiler */) {
+    if (compiler.hooks) {
+      console.warn(
+        "" +
+          "You appear to be running Webpack >= 4. " +
+          "The RemoveBuildDirectoryPlugin should be forward-compatible, " +
+          "but you should update it to use the new APIs. See " +
+          "<https://github.com/webpack/webpack/releases/tag/v4.0.0-beta.0> " +
+          "for details."
+      );
+    }
+    compiler.plugin("compile", () => {
+      const outputPath = compiler.options.output.path;
+      // If a build config has no `output.path` property, and no
+      // `--output-path` is passed on the command line, then Webpack
+      // will default to building into the current directory. Removing
+      // the whole Git repository would be mighty rude, so we protect
+      // against that case.
+      if (fs.existsSync(path.join(outputPath, ".git"))) {
+        throw new Error(
+          "Refusing to remove build directory with a Git repository: " +
+            outputPath
+        );
+      }
+      console.warn("Removing build directory: " + outputPath);
+      rimraf.sync(outputPath);
+    });
+  }
+};
