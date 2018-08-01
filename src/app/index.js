2708ddfd9894b2f2502ebb7cdfe3b292c585d354
claireandcode <cschlenk@wellesley.edu>
2018-07-31 18:03:51 -0700

    updating styling path

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 // @flow
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./style/index.css";
 import App from "./App";
 import {unregister} from "./registerServiceWorker";
 
