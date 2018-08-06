d7cb4c65fa3a904acf60509f94ee96eaed71caf1
William Chargin <wchargin@gmail.com>
2018-08-06 13:05:40 -0700

    Create script to build static site (#592)
    Summary:
    Currently, we create the static site and deploy it all at once in
    `scripts/deploy.sh`. This commit creates a new script that only builds
    the static site. This has the advantage that it is easier/less scary to
    change that script (because it can be tested without worrying about
    deploying to a local test target), and that we can write automated tests
    for it.
    
    Test Plan:
    Run `yarn sharness`; note that it completes very quickly. Then, in a
    shell with your GitHub token exported, run `yarn sharness-full`. Expect
    all tests to pass.
    
    For a sanity check, you can run:
    
    ```shell
    outdir="$(mktemp -d --suffix .sourcecred-site)"
    ./scripts/build_static_site.sh --target "${outdir}" \
        --cname sourcecred.io \
        --repo sourcecred/example-git \
        --repo sourcecred/example-github \
        ;
    (cd "${outdir}" && python -m SimpleHTTPServer)
    ```
    
    and ensure that <http://localhost:8000/> is as expected.
    
    One test case that is not covered is the following: _if_ the actual app
    somehow tries to emit a `CNAME` file at root, _and_ our script’s logic
    to catch this is broken, then we will not catch this failure. I’ve
    tested the logic manually by adding `>"${cname_file}"` after definition
    of that variable, but I don’t see a good way to test it automatically,
    without adding flags like `--but-actually-emit-cname-too` to the build.
    The compound probability of this happening is sufficiently low that this
    doesn’t bother me.
    
    wchargin-branch: build-static-site-script

---
+++ scripts/build_static_site.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+set -eu
+
+usage() {
+    printf 'usage: build_static_site.sh --target TARGET\n'
+    printf '                            [--repo OWNER/NAME [...]]\n'
+    printf '                            [--cname DOMAIN]\n'
+    printf '                            [-h|--help]\n'
+    printf '\n'
+    printf 'Build the static SourceCred website, including example data.\n'
+    printf '\n'
+    printf '%s\n' '--target TARGET'
+    printf '\t%s\n' 'an empty directory into which to build the site'
+    printf '%s\n' '--repo OWNER/NAME'
+    printf '\t%s\n' 'a GitHub repository (e.g., torvalds/linux) for which'
+    printf '\t%s\n' 'to include example data'
+    printf '%s\n' '--cname DOMAIN'
+    printf '\t%s\n' 'configure DNS for a GitHub Pages site to point to'
+    printf '\t%s\n' 'the provided custom domain'
+    printf '%s\n' '-h|--help'
+    printf '\t%s\n' 'show this message'
+}
+
+main() {
+    parse_args "$@"
+
+    toplevel="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
+    cd "${toplevel}"
+
+    sourcecred_data=
+    trap cleanup EXIT
+
+    build
+}
+
+parse_args() {
+    target=
+    cname=
+    repos=( )
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            --target)
+                if [ -n "${target}" ]; then
+                    die '--target specified multiple times'
+                fi
+                shift
+                if [ $# -eq 0 ]; then die 'missing value for --target'; fi
+                if ! target="$(readlink -e "$1")"; then
+                    die "target does not exist: $1"
+                fi
+                ;;
+            --repo)
+                shift
+                if [ $# -eq 0 ]; then die 'missing value for --repo'; fi
+                repos+=( "$1" )
+                ;;
+            --cname)
+                shift
+                if [ $# -eq 0 ]; then die 'missing value for --cname'; fi
+                if [ -n "${cname}" ]; then
+                    die '--cname specified multiple times'
+                fi
+                cname="$1"
+                if [ -z "${cname}" ]; then
+                    die 'empty value for --cname'
+                fi
+                ;;
+            -h|--help)
+                usage
+                exit 0
+                ;;
+            *)
+                printf >&2 'fatal: unknown argument: %s\n' "$1"
+                exit 1
+                ;;
+        esac
+        shift
+    done
+    if [ -z "${target}" ]; then
+        die 'target directory not specified'
+    fi
+    if ! [ -d "${target}" ]; then
+        die "target is not a directory: ${target}"
+    fi
+    if [ "$(command ls -A "${target}" | wc -l)" != 0 ]; then
+        die "target directory is nonempty: ${target}"
+    fi
+}
+
+build() {
+    sourcecred_data="$(mktemp -d --suffix ".sourcecred-data")"
+    export SOURCECRED_DIRECTORY="${sourcecred_data}"
+
+    yarn
+    # shellcheck disable=SC2016
+    printf >&2 'warn: running `yarn backend`, overwriting `bin/` in your repo\n'
+    printf >&2 'warn: if this offends you, please see: %s\n' \
+        'https://github.com/sourcecred/sourcecred/issues/580'
+    yarn backend
+    yarn build --output-path "${target}"
+
+    if [ "${#repos[@]}" -ne 0 ]; then
+        for repo in "${repos[@]}"; do
+            printf >&2 'info: loading repository: %s\n' "${repo}"
+            node ./bin/sourcecred.js load "${repo}"
+        done
+    fi
+
+    # Copy the SourceCred data into the appropriate API route. Using
+    # `mkdir` here will fail in the case where an `api/` folder exists,
+    # which is the correct behavior. (In this case, our site's
+    # architecture conflicts with the required static structure, and we
+    # must fail.)
+    mkdir "${target}/api/"
+    mkdir "${target}/api/v1/"
+    cp -r "${sourcecred_data}" "${target}/api/v1/data"
+
+    if [ -n "${cname:-}" ]; then
+        cname_file="${target}/CNAME"
+        if [ -e "${cname_file}" ]; then
+            die 'CNAME file exists in static site output'
+        fi
+        printf '%s' "${cname}" >"${cname_file}"  # no newline
+    fi
+}
+
+cleanup() {
+    if [ -d "${sourcecred_data}" ]; then rm -rf "${sourcecred_data}"; fi
+}
+
+die() {
+    printf >&2 'fatal: %s\n' "$@"
+    exit 1
+}
+
+main "$@"
