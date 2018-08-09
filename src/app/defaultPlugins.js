cbf701af029802532c161db721751e1f54a9df1b
Dandelion Mané <decentralion@dandelion.io>
2018-08-09 16:12:28 -0700

    Disable the Git plugin
    See #627 for context.
    
    Removing the Git plugin results in an enormous performance improvement.
    In my testing on `metamask/metamask-extension`: before this change, load
    took 23s, and PageRank took >9 mins and then crashed. After this change,
    load+PageRank took 5s combined.
    
    Test plan: All unit tests pass; loading new data from the CLI works; and
    I poked around the UI to make sure there were no spurious references to
    the Git plugin.
    
    Note: This does not break backcompat, there's no need to regenerate any
    already-loaded data.

--- src/app/defaultPlugins.js
+++ src/app/defaultPlugins.js
@@ -1,9 +1,8 @@
 // @flow
 
 import type {StaticPluginAdapter} from "./pluginAdapter";
-import {StaticPluginAdapter as GitAdapter} from "../plugins/git/pluginAdapter";
 import {StaticPluginAdapter as GithubAdapter} from "../plugins/github/pluginAdapter";
 
 export function defaultStaticAdapters(): $ReadOnlyArray<StaticPluginAdapter> {
-  return [new GitAdapter(), new GithubAdapter()];
+  return [new GithubAdapter()];
 }
