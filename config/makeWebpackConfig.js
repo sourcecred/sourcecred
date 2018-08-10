8f2d2cd5cdf9f24805e13bcb653fc4d86f9a4741
William Chargin <wchargin@gmail.com>
2018-08-10 12:49:45 -0700

    Remove service workers entirely (#635)
    Summary:
    This is a follow-up to #514, wherein we disabled new service workers and
    instructed any existing service workers to self-destruct. (See that PR
    for the rationale.) This commit removes them from our codebase entirely,
    enabling us to slim down our build process and our build output.
    
    Test Plan:
    Running `yarn start` still works. Building the static site and exploring
    it works, too.
    
    wchargin-branch: remove-sw

--- config/makeWebpackConfig.js
+++ config/makeWebpackConfig.js
@@ -11,7 +11,6 @@ const path = require("path");
 const webpack = require("webpack");
 const RemoveBuildDirectoryPlugin = require("./RemoveBuildDirectoryPlugin");
 const ManifestPlugin = require("webpack-manifest-plugin");
-const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 const StaticSiteGeneratorPlugin = require("static-site-generator-webpack-plugin");
 const eslintFormatter = require("react-dev-utils/eslintFormatter");
 const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
@@ -229,36 +228,6 @@ function plugins(mode /*: "development" | "production" */) {
     // having to parse `index.html`.
     new ManifestPlugin({
       fileName: "asset-manifest.json",
-    }),
-    // Generate a service worker script that will precache, and keep up to date,
-    // the HTML & assets that are part of the Webpack build.
-    new SWPrecacheWebpackPlugin({
-      // By default, a cache-busting query parameter is appended to requests
-      // used to populate the caches, to ensure the responses are fresh.
-      // If a URL is already hashed by Webpack, then there is no concern
-      // about it being stale, and the cache-busting can be skipped.
-      dontCacheBustUrlsMatching: /\.\w{8}\./,
-      filename: "service-worker.js",
-      logger(message) {
-        if (message.indexOf("Total precache size is") === 0) {
-          // This message occurs for every build and is a bit too noisy.
-          return;
-        }
-        if (message.indexOf("Skipping static resource") === 0) {
-          // This message obscures real errors so we ignore it.
-          // https://github.com/facebookincubator/create-react-app/issues/2612
-          return;
-        }
-        console.log(message);
-      },
-      minify: true,
-      // For unknown URLs, fallback to the index page
-      navigateFallback: publicUrl + "/index.html",
-      // Ignores URLs starting from /__ (useful for Firebase):
-      // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-      navigateFallbackWhitelist: [/^(?!\/__).*/],
-      // Don't precache sourcemaps (they're large) and build asset manifest:
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
     }),
     // Moment.js is an extremely popular library that bundles large locale files
     // by default due to how Webpack interprets its code. This is a practical
