c7b81e9966045936765f7f6d11d03fdce624db6d
William Chargin <wchargin@gmail.com>
2018-08-10 11:46:59 -0700

    Remove service workers entirely
    Summary:
    This is a follow-up to #514, wherein we disabled new service workers and
    instructed any existing service workers to self-destruct. (See that PR
    for the rationale.) This commit removes them from our codebase entirely,
    enabling us to slim down our build process and our build output.
    
    Test Plan:
    Running `yarn start` still works. Building the static site and exploring
    it works, too.
    
    wchargin-branch: remove-sw

--- src/app/index.js
+++ src/app/index.js
@@ -3,11 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {unregister} from "./registerServiceWorker";
 
 const root = document.getElementById("root");
 if (root == null) {
   throw new Error("Unable to find root element!");
 }
 ReactDOM.hydrate(<App />, root);
-unregister();
