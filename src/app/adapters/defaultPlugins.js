0032b4293b9848ef4f1b6db997d8d7ad6ed5d983
Dandelion Mané <decentralion@dandelion.io>
2018-08-10 18:16:46 -0700

    Re-disable the Git plugin
    Thanks to #642, it should now be safe to disable the Git plugin, reaping
    the benefits described in #628, without causing the cred explorer to
    crash (#631).
    
    Test plan:
    - `yarn travis --full` passes
    - The full cred explorer works:
      - Running PageRank does not crash the explorer
      - Expanding a pull request does not crash the explorer
      - (After clearing state) the weight config doesn't show Git weights
      - The filter doesn't show Git nodes

--- src/app/adapters/defaultPlugins.js
+++ src/app/adapters/defaultPlugins.js
@@ -1,9 +1,8 @@
 // @flow
 
 import {StaticAdapterSet} from "./adapterSet";
-import {StaticPluginAdapter as GitAdapter} from "../../plugins/git/pluginAdapter";
 import {StaticPluginAdapter as GithubAdapter} from "../../plugins/github/pluginAdapter";
 
 export function defaultStaticAdapters(): StaticAdapterSet {
-  return new StaticAdapterSet([new GitAdapter(), new GithubAdapter()]);
+  return new StaticAdapterSet([new GithubAdapter()]);
 }
