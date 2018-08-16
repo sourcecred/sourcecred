a460704ea805ebaed97c6e46be3556b193d9b67f
Dandelion Mané <decentralion@dandelion.io>
2018-08-16 11:38:36 -0700

    Add `createMinimalGraph` for a tiny git graph (#689)
    This implements the approach suggested in [1]. Instead of forking the
    git plugin entirely, we'll fork the createGraph method and the
    pluginAdapter so that we have instances that produce a lightweight git
    graph.
    
    createMinimalGraph is a fork of createGraph that only adds commit nodes
    and has_parent edges. New unit tests ensure that only the whitelisted
    nodes and edges appear.
    
    Supersedes #683 and #684.
    
    Test plan: `yarn test`
    
    [1]: https://github.com/sourcecred/sourcecred/issues/627#issuecomment-413623784

---
+++ src/plugins/git/createMinimalGraph.test.js
@@ -0,0 +1,38 @@
+// @flow
+
+import cloneDeep from "lodash.clonedeep";
+
+import {createMinimalGraph} from "./createMinimalGraph";
+import {GraphView} from "./graphView";
+import {_Prefix as NodePrefix} from "./nodes";
+import {_Prefix as EdgePrefix} from "./edges";
+import {NodeAddress, EdgeAddress} from "../../core/graph";
+
+const makeData = () => cloneDeep(require("./example/example-git"));
+
+describe("plugins/git/createMinimalGraph", () => {
+  describe("createMinimalGraph", () => {
+    it("processes a simple repository", () => {
+      expect(createMinimalGraph(makeData())).toMatchSnapshot();
+    });
+
+    it("satisfies the GraphView invariants", () => {
+      const graph = createMinimalGraph(makeData());
+      expect(() => new GraphView(graph)).not.toThrow();
+    });
+
+    it("only has commit nodes and has_parent edges", () => {
+      const graph = createMinimalGraph(makeData());
+      for (const n of graph.nodes()) {
+        if (!NodeAddress.hasPrefix(n, NodePrefix.commit)) {
+          throw new Error("Found non-commit node");
+        }
+      }
+      for (const {address} of graph.edges()) {
+        if (!EdgeAddress.hasPrefix(address, EdgePrefix.hasParent)) {
+          throw new Error("Found non-has-parent edge");
+        }
+      }
+    });
+  });
+});
