e0a518416bd436e80b845693bd0453412f6edcbd
Dandelion Mané <decentralion@dandelion.io>
2018-08-16 00:50:30 -0700

    Re-introduce a simplified git plugin
    This commit re-introduces the git plugin, now that it has been radically
    simplified as described in [1]. The new git plugin only has nodes for
    commits and only has commit has-parent edges. As compared to the version
    that was removed in #628, this plugin is far leaner. It doesn't bloat
    the graph (for `sourcecred/sourcecred`, the git plugin data is just
    164k), and as such doesn't incur much performance penalty.
    
    Re-incorporating the git plugin also brings some tangible benefits. We
    already had git nodes in the graph, as the GitHub plugin attaches them
    to pull requests. Without any git plugin, these nodes are displayed as
    "uknown nodes" with ugly descriptions. Also, including a git plugin,
    even one that is very minimal, communicates to users that git is a
    source of information to SourceCred, and that they can expect more from
    it in the future.
    
    Note that this commit breaks backcompat for existing repositories that
    were locally loaded after #628. As such, it is best to
    `rm -rf $SOURCECRED_DIRECTORY` and start with fresh data. Also, due to a
    known bug in the WeightConfig, you should reset your browser's local
    storage.
    
    Test plan: After removing the SourceCred directory and the stale
    localStorage, the cred explorer nicely displays git commits, and
    connects them via has_parent edges. The NodeType filter allows filtering
    to commits as expected, and the WeightConfig shows node and edge weights
    for the Git plugin's nodes and edges.
    
    [1]: https://github.com/sourcecred/sourcecred/issues/627#issuecomment-413435447

--- src/app/adapters/defaultPlugins.js
+++ src/app/adapters/defaultPlugins.js
@@ -2,7 +2,8 @@
 
 import {StaticAdapterSet} from "./adapterSet";
 import {StaticPluginAdapter as GithubAdapter} from "../../plugins/github/pluginAdapter";
+import {StaticPluginAdapter as GitAdapter} from "../../plugins/git/minimalPluginAdapter";
 
 export function defaultStaticAdapters(): StaticAdapterSet {
-  return new StaticAdapterSet([new GithubAdapter()]);
+  return new StaticAdapterSet([new GithubAdapter(), new GitAdapter()]);
 }
