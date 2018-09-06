0b4fea1c4fa3b4aecc9094b4771ea1cbc7078a64
William Chargin <wchargin@gmail.com>
2018-09-06 13:30:33 -0700

    flow: add typing to Jest transform modules (#790)
    Test Plan:
    `yarn flow` passes, and the [Jest docs][1] suggest that the added type
    annotations are correct.
    
    [1]: https://jestjs.io/docs/en/configuration#transform-object-string-string
    
    wchargin-branch: flow-jest-transforms

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -1,11 +1,11 @@
-// @no-flow
+// @flow
 const path = require("path");
 
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
-  process(src, filename) {
+  process(src /*: string */, filename /*: string */) {
     return `module.exports = ${JSON.stringify(path.basename(filename))};`;
   },
 };
