a1f83a1702a93906924bff018115572e36bc16a8
Dandelion Mané <decentralion@dandelion.io>
2018-09-17 10:41:57 -0700

    Add tensorflow-gardener to the list of bots
    Test plan: I verified that the spelling is correct

--- src/plugins/github/bots.js
+++ src/plugins/github/bots.js
@@ -14,6 +14,7 @@ export function botSet(): Set<string> {
     "metamaskbot",
     "nodejs-github-bot",
     "stickler-ci",
+    "tensorflow-gardener",
     "tensorflow-jenkins",
     "tensorflowbutler",
   ]);
