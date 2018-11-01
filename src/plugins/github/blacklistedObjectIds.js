d19227c268946fc1063e353b80dc01e63555483d
William Chargin <wchargin@gmail.com>
2018-11-01 11:08:17 -0700

    github: use blacklists to unblock twbs/bootstrap (#973)
    Summary:
    This adds object IDs to the GitHub GraphQL blacklist such that the
    `twbs/bootstrap` repository can be loaded.
    
    Ingesting the Mirror-extracted data into the RelationalView yields the
    warnings
    
    ```
    IssueComment[MDEyOklzc3VlQ29tbWVudDEwNTI4Mzk4Ng==].reactions: unexpected null value
    IssueComment[MDEyOklzc3VlQ29tbWVudDI0NTQ3OTM3OA==].reactions: unexpected null value
    IssueComment[MDEyOklzc3VlQ29tbWVudDMwNDE4NzIzMg==].reactions: unexpected null value
    ```
    
    because we have nulled out these `Reaction`s in their enclosing
    connections. This is expected.
    
    Test Plan:
    Run `yarn backend` and `node ./bin/sourcecred.js load twbs/bootstrap`.
    Run `yarn start` and note that the cred attribution renders properly.
    
    (Loading the GitHub data may take an hour or two. The resulting SQLite3
    database is 172MB. Ingesting it into the `RelationalView` still takes
    just a few seconds, and the cred attribution is rendered quickly.)
    
    wchargin-branch: github-use-blacklists

---
+++ src/plugins/github/blacklistedObjectIds.js
@@ -0,0 +1,16 @@
+// @flow
+
+import type {ObjectId} from "../../graphql/schema";
+
+export const BLACKLISTED_IDS: $ReadOnlyArray<ObjectId> = Object.freeze([
+  // These are `Organization` nodes that are sometimes referenced in a
+  // `User` context: in particular, as the author of a reaction.
+  // See: https://gist.github.com/wchargin/a2b8561b81bcc932c84e493d2485ea8a
+  "MDEyOk9yZ2FuaXphdGlvbjE3OTUyOTI1",
+  "MDEyOk9yZ2FuaXphdGlvbjI5MTkzOTQ=",
+  "MDEyOk9yZ2FuaXphdGlvbjEyNDE3MDI0",
+  // These are the offending reactions.
+  "MDg6UmVhY3Rpb24yMTY3ODkyNQ==",
+  "MDg6UmVhY3Rpb240NDMwMzQ1",
+  "MDg6UmVhY3Rpb24xMDI4MzQxOA==",
+]);
