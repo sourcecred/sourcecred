16edea64132d2720b8f9cacb6507c9112969377e
Dandelion Mané <decentralion@dandelion.io>
2019-06-03 21:07:27 +0300

    Remove the `graphView`s (#1171)
    A long time ago, we made graph views for git and github. These are
    interfaces over the graph which allow retrieving nodes' relations, e.g.
    finding the parent address of a commit just using the graph.
    
    These are fairly complex, and have seen almost no use at all. The one
    thing they are used for is implementing invariant checking. The
    invariant checking is nice in principle, but since we only apply it to
    the example data, its of very limited value in practice.
    
    Since I'm planning a significant Graph refactor (#1136), I'd rather delete this
    code than continue to maintain it, since I think it's complexity/value
    ratio is unfavorable.
    
    Test plan: `yarn test`

--- src/plugins/github/createGraph.test.js
+++ src/plugins/github/createGraph.test.js
@@ -1,29 +1,9 @@
 // @flow
 
-import {GraphView} from "./graphView";
 import {exampleGraph} from "./example/example";
 
 describe("plugins/github/createGraph", () => {
   it("example graph matches snapshot", () => {
     expect(exampleGraph()).toMatchSnapshot();
-  });
-
-  it("passes all GraphView invariants", () => {
-    const view = new GraphView(exampleGraph());
-    // This test is high leverage. It checks:
-    // - that every node starting with a GitHub prefix
-    //   - can be structured using fromRaw
-    //   - has the correct type
-    // - that every edge starting with a GitHub prefix
-    //   - can be structured using fromRaw
-    //   - and has the correct type,
-    //   - and that its src has an expected prefix,
-    //   - and that its dst has an expected prefix,
-    // - that every child node
-    //   - has exactly one parent
-    //   - has a parent with the correct type
-    view.checkInvariants();
-    // as currently written, GV checks invariants on construction.
-    // we call the method explicitly as a defensive step.
   });
 });
