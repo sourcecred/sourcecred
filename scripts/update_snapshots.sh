ab259a8123efea9398c4718a1f5e423ceae8baf8
Dandelion Mané <decentralion@dandelion.io>
2019-06-04 02:45:37 +0300

    update_snapshots.sh updates generated flow types
    The generated GitHub GraphQL flow types are a kind of snapshot, and it
    can be hard to remember/discover how to update them when making changes
    to schema. Therefore, I've included them in the snapshot update script.
    
    Test plan: I used it to update the types, and it worked as intended.

--- scripts/update_snapshots.sh
+++ scripts/update_snapshots.sh
@@ -7,6 +7,8 @@ toplevel="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
 cd "${toplevel}"
 
 yarn backend
+echo "Updating GitHub graphql flow types"
+node ./bin/generateGithubGraphqlFlowTypes.js > src/plugins/github/graphqlTypes.js
 echo "Updating for sharness/test_load_example_github.t"
 (cd sharness; UPDATE_SNAPSHOT=1 ./test_load_example_github.t -l)
 echo "Updating git/loadRepositoryTest.sh"
