ff8ec71c663dbd32e565e544ccac6605e02848bb
Dandelion Mané <decentralion@dandelion.io>
2019-07-11 05:15:53 +0100

    Upgrade babel to 7
    I moved `config/babel.js` to `.babelrc.js` because it seemed like babel
    7 really wanted that. I also blew away our (complicated, copied from
    create-react-app) config and replaced it with a much, much simpler one.
    
    Test plan: `yarn test` passes, `yarn start` still serves a working
    server, and `scripts/build_static_site.sh` still produces a working
    site.
    
    Possibly we lost some nice features re: React debugging; if so I'll add
    them back as I miss them.

---
+++ .babelrc.js
@@ -0,0 +1,22 @@
+// @flow
+
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      targets: {
+        edge: "17",
+        firefox: "60",
+        chrome: "67",
+        safari: "11.1",
+        node: true,
+      },
+    },
+  ],
+  "@babel/preset-react",
+  "@babel/preset-flow",
+];
+
+const plugins = ["@babel/plugin-proposal-class-properties"];
+
+module.exports = {presets, plugins};
