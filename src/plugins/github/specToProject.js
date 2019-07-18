0a34c8b0369f8e71b7706a27866a2e7d42024f5e
Dandelion Mané <decentralion@dandelion.io>
2019-07-18 17:10:15 +0100

    add github/specToProject
    This module builds on the project logic added in #1238, and makes it
    easy to create projects based on a simple string configuration.
    Basically, the spec `foo/bar` creates a project containing just the repo
    foo/bar, and the spec `@foo` creates a project containing all of the
    repos from the user/organization named foo.
    
    This is pulled out of #1233, but I've enhanced it to support
    organizations out of the box.
    
    The method is thoroughly tested.

---
+++ src/plugins/github/specToProject.js
@@ -0,0 +1,48 @@
+// @flow
+
+import {type Project} from "../../core/project";
+import {
+  stringToRepoId,
+  githubOwnerPattern,
+  githubRepoPattern,
+} from "../../core/repoId";
+import {fetchGithubOrg} from "./fetchGithubOrg";
+
+/**
+ * Convert a string repository spec into a project.
+ *
+ * The spec may take one of two forms:
+ * - $REPO_OWNER/$REPO_NAME, as in 'sourcecred/example-github'
+ * - @$REPO_OWNER, as in '@sourcecred'
+ *
+ * In either case, we will create a project with the spec as its
+ * id. In the first construction, the project will have a single
+ * RepoId, matching the spec string. In the second construction,
+ * the project will have a RepoId for every repository owned by that
+ * owner.
+ *
+ * A valid GitHub token must be provided, so that it's possible to
+ * enumerate the repos for an org.
+ */
+export async function specToProject(
+  spec: string,
+  token: string
+): Promise<Project> {
+  const repoSpecMatcher = new RegExp(
+    `^${githubOwnerPattern}/${githubRepoPattern}$`
+  );
+  const ownerSpecMatcher = new RegExp(`^@${githubOwnerPattern}$`);
+  if (spec.match(repoSpecMatcher)) {
+    const project: Project = {
+      id: spec,
+      repoIds: [stringToRepoId(spec)],
+    };
+    return project;
+  } else if (spec.match(ownerSpecMatcher)) {
+    const owner = spec.slice(1);
+    const org = await fetchGithubOrg(owner, token);
+    const project: Project = {id: spec, repoIds: org.repos};
+    return project;
+  }
+  throw new Error(`invalid spec: ${spec}`);
+}
