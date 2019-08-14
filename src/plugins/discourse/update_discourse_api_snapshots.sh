d9533fd52608c23b1d2e75ef2f2a235c01b03853
Dandelion Mané <decentralion@dandelion.io>
2019-08-14 16:21:22 +0200

    Address some review feedback and prettify json
    First off, this addresses excellent feedback from @wchargin's [first
    review][1].
    
    After running the Discourse updater.
    
    [1]: https://github.com/sourcecred/sourcecred/pull/1265#pullrequestreview-272334984

--- src/plugins/discourse/update_discourse_api_snapshots.sh
+++ src/plugins/discourse/update_discourse_api_snapshots.sh
@@ -9,20 +9,25 @@ test_instance_username="credbot"
 if [ -z "${DISCOURSE_TEST_API_KEY:-}" ]; then
   printf >&2 'Please set the DISCOURSE_TEST_API_KEY environment variable.\n'
   printf >&2 'Contact the SourceCred maintainers to get the key.\n'
-  return 1
+  exit 1
+fi
+
+if [ ! "$(jq --version)" ]; then
+  printf >&2 'This script depends on jq. Please install it.\n'
+  exit 1
 fi
 
 toplevel="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
 cd "${toplevel}"
 
 fetch() {
-  url="${test_instance_url}${1}"
-  filename=$(echo -n "${url}" | base64 -w 0 | tr -d '=' | tr '/+' '_-')
+  url="${test_instance_url}$1"
+  filename="$(printf '%s' "${url}" | base64 -w 0 | tr -d '=' | tr '/+' '_-')"
   path="${snapshots_dir}/${filename}"
-  curl $url \
-    -H "Api-Key:${DISCOURSE_TEST_API_KEY}" \
-    -H "Api-Username:${test_instance_username}" \
-    > "${path}"
+  curl -sfL "$url" \
+    -H "Api-Key: ${DISCOURSE_TEST_API_KEY}" \
+    -H "Api-Username: ${test_instance_username}" \
+    | jq '.' > "${path}"
 }
 
 rm -r "${snapshots_dir}"
@@ -31,4 +36,3 @@ fetch "/latest.json?order=created"
 fetch "/posts.json"
 fetch "/t/11.json"
 fetch "/posts/14.json"
-
