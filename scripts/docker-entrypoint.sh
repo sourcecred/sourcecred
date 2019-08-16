24310a97308cef21f21422bb24aa47d95c1ed68d
Robin IT <info@robin-it.com>
2019-08-16 12:17:36 +0200

    Docker: prefix webpack dev-server tasks with "dev-"
    To make it more clear it shouldn't be used in production.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-# serves on 8080 in docker container, should be bound to host
-node /code/bin/sourcecred.js "${@}" || exit 1
-exec yarn start --host 0.0.0.0
+case "$1" in
+	# Offer a shortcut for load + dev-server
+	"dev-preview")
+		shift
+		node /code/bin/sourcecred.js load "$@" || exit 1
+		exec yarn -s start --host 0.0.0.0
+		;;
+	# Expose several webpack operations
+	"dev-server")
+		exec yarn -s start --host 0.0.0.0
+		;;
+	"build")
+		shift
+		exec yarn -s build "$@"
+		;;
+	# Everything else, pass on to sourcecred.js
+	*)
+		exec node /code/bin/sourcecred.js "$@"
+		;;
+esac
