110b8d49f2599f06f9b1a7c8f8189710d9c098af
William Chargin <wchargin@gmail.com>
2019-08-20 23:34:03 -0700

    flow: add libdefs for `deep-freeze`
    Summary:
    This dependency was added in #1249 without typedefs, and so is
    implicitly `any`-typed.
    
    Depends on #1309 to fix a bug that would otherwise be a true positive
    type error.
    
    Addresses part of #1308.
    
    Generated with `flow-typed install deep-freeze@0.0.1`.
    
    Test Plan:
    Running `yarn flow` passes.
    
    wchargin-branch: libdefs-deep-freeze

---
+++ flow-typed/npm/deep-freeze_v0.0.1.js
@@ -0,0 +1,6 @@
+// flow-typed signature: dd48a9b9eb482a63ddb9915bdeefacee
+// flow-typed version: c6154227d1/deep-freeze_v0.0.1/flow_>=v0.104.x
+
+declare module 'deep-freeze' {
+  declare module.exports: <T>(o: T) => T;
+}
