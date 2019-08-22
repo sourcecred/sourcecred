ecd15ed3c4467843ba49e03d7e7acc82491d7467
William Chargin <wchargin@gmail.com>
2019-08-22 08:49:40 -0700

    flow: add libdefs that have clean updates (#1311)
    Summary:
    Generated by running `flow-typed install --skip --overwrite` and
    reverting a minimal set of libdefs such that the change does not
    introduce any Flow errors (except Prettier, which is covered by #1307).
    
    Addresses parts of #1308.
    
    Changes:
    
      - `chalk`: upgraded v1.x.x to v2.x.x
      - `flow-bin`: no-op; explicit Flow window widening
      - `isomorphic-fetch`: no-op; formatting change
      - `jest`: updates for Flow v0.104.x (explicit inexact objects), and
        also some functional additions
      - `object-assign`: no-op; explicit Flow window widening
      - `rimraf`: added new at v2.x.x
    
    Test Plan:
    Flow passes, by construction.
    
    wchargin-branch: libdefs-clean

--- flow-typed/npm/object-assign_v4.x.x.js
+++ flow-typed/npm/object-assign_v4.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 0163683fe464be718f5e440cbb72d8b0
-// flow-typed version: 62c71f6105/object-assign_v4.x.x/flow_>=v0.25.x
+// flow-typed signature: f3107da4bbfb4bddf9eb6bd0c81bd577
+// flow-typed version: c6154227d1/object-assign_v4.x.x/flow_>=v0.104.x
 
 declare module "object-assign" {
   declare module.exports: Object$Assign;
