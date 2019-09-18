1770edbbdb475bfb0886e96786267351a342092d
Dandelion Mané <decentralion@dandelion.io>
2019-09-18 14:51:26 +0200

    Add the identity plugin
    This commit adds the new SourceCred identity plugin. As described in the
    README.md file:
    
    This folder contains the Identity plugin. Unlike most other plugins, the
    Identity plugin does not add any new contributions to the graph. Instead, it
    allows collapsing different user accounts together into a shared 'identity'
    node.
    
    To see why this is valuable, imagine that a contributor has an account on both
    GitHub and Discourse (potentially with a different username on each service).
    We would like to combine these two identities together, so that we can
    represent that user's combined cred properly. The Identity plugin enables this.
    
    Specifically, the instance maintainer can provide a (locally unique) username
    for the user, along with a list of aliases the user is known by, e.g.
    `github/username` and `discourse/other_username`. The aliases are simple string
    representations, that are intended to be easy to maintain by hand in a
    configuration file. Then, the identity plugin will provide a list of
    `NodeContraction`s that can be used by `Graph.contractNodes` to combine the
    user identities as described.
    
    The plugin is broken up into a few submoudles:
    - `declaration.js` provides the PluginDeclaration. It has a single node
    type (the identity node).
    - `identity.js` declares the `Identity` type (a username and list of
    aliases), allows constructing identity nodes, and does some validation
    on the identity username.
    - `alias.js` implements the logic for parsing aliases like
    "github/decentralion" or "discourse/s_ben" into a node address.
    - `nodeContractions.js` provides logic for turning a list of Identities
    into a list of NodeContractions, suitable for use in
    `Graph.contractNodes`.
    
    The plugin is not yet integrated; that will come in a followon commit.
    
    Test plan: Unit tests added; `yarn test` passes.

---
+++ src/plugins/identity/alias.js
@@ -0,0 +1,44 @@
+// @flow
+
+import {type NodeAddressT} from "../../core/graph";
+import {loginAddress as githubAddress} from "../github/nodes";
+import {userAddress as discourseAddress} from "../discourse/createGraph";
+
+/** An Alias is a string specification of an identity within another plugin.
+ *
+ * For now, the supported alias forms are `github/${githubUsername}` and
+ * `discourse/${discourseUsername}`. As a courtesty, if the user has put an @
+ * before the username, we strip it for them.
+ *
+ * This format has been chosen to be moderately extensible and easy to maintain
+ * by hand, as in the near future the alias files will be maintained by hand.
+ * This system will not scale well when user-provided plugins need to add to the
+ * aliasing scheme, so at that point we will rewrite this system.
+ */
+export type Alias = string;
+
+export function resolveAlias(
+  alias: Alias,
+  discourseUrl: string | null
+): NodeAddressT {
+  const re = /(\w+)\/@?(\w+)/g;
+  const match = re.exec(alias);
+  if (match == null) {
+    throw new Error(`Unable to parse alias: ${alias}`);
+  }
+  const prefix = match[1];
+  const name = match[2];
+  switch (prefix) {
+    case "github": {
+      return githubAddress(name);
+    }
+    case "discourse": {
+      if (discourseUrl == null) {
+        throw new Error(`Can't parse alias ${alias} without Discourse url`);
+      }
+      return discourseAddress(discourseUrl, name);
+    }
+    default:
+      throw new Error(`Unknown type for alias: ${alias}`);
+  }
+}
---
+++ src/plugins/identity/alias.test.js
@@ -0,0 +1,52 @@
+// @flow
+
+import {resolveAlias} from "./alias";
+import {loginAddress as githubAddress} from "../github/nodes";
+import {userAddress as discourseAddress} from "../discourse/createGraph";
+
+describe("src/plugins/identity/alias", () => {
+  describe("resolveAlias", () => {
+    describe("errors on", () => {
+      it("an empty alias", () => {
+        expect(() => resolveAlias("", null)).toThrow("Unable to parse");
+      });
+      it("an alias without a /-delimited prefix", () => {
+        expect(() => resolveAlias("@credbot", null)).toThrow("Unable to parse");
+      });
+      it("an alias with an unknown prefix", () => {
+        expect(() => resolveAlias("foo/bar", null)).toThrow(
+          "Unknown type for alias"
+        );
+      });
+      it("a discourse alias without a url", () => {
+        expect(() => resolveAlias("discourse/foo", null)).toThrow(
+          "without Discourse url"
+        );
+      });
+    });
+    describe("works on", () => {
+      it("a github login", () => {
+        const actual = resolveAlias("github/login", null);
+        const expected = githubAddress("login");
+        expect(actual).toEqual(expected);
+      });
+      it("a discourse login", () => {
+        const url = "https://example.com";
+        const actual = resolveAlias("discourse/login", url);
+        const expected = discourseAddress(url, "login");
+        expect(actual).toEqual(expected);
+      });
+      it("a github login with prefixed @", () => {
+        const a = resolveAlias("github/login", null);
+        const b = resolveAlias("github/@login", null);
+        expect(a).toEqual(b);
+      });
+      it("a discourse login with prefixed @", () => {
+        const url = "https://example.com";
+        const a = resolveAlias("discourse/login", url);
+        const b = resolveAlias("discourse/@login", url);
+        expect(a).toEqual(b);
+      });
+    });
+  });
+});
