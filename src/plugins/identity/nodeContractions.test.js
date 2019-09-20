9a9f2119012a41cd307b1bc3c06a909a2747e4b5
Dandelion Mané <decentralion@dandelion.io>
2019-09-20 11:50:59 +0200

    Add the identity plugin (#1384)
    This commit adds the new SourceCred identity plugin. As described in the
    README.md file:
    
    This folder contains the Identity plugin. Unlike most other plugins, the
    Identity plugin does not add any new contributions to the graph. Instead, it
    allows collapsing different user accounts together into a shared 'identity'
    node.
    
    To see why this is valuable, imagine that a contributor has an account on both
    GitHub and Discourse (potentially with a different username on each service).
    We would like to combine these two identities together, so that we can
    represent that user's combined cred properly. The Identity plugin enables this.
    
    Specifically, the instance maintainer can provide a (locally unique) username
    for the user, along with a list of aliases the user is known by, e.g.
    `github/username` and `discourse/other_username`. The aliases are simple string
    representations, that are intended to be easy to maintain by hand in a
    configuration file. Then, the identity plugin will provide a list of
    `NodeContraction`s that can be used by `Graph.contractNodes` to combine the
    user identities as described.
    
    The plugin is broken up into a few submoudles:
    - `declaration.js` provides the PluginDeclaration. It has a single node
    type (the identity node).
    - `identity.js` declares the `Identity` type (a username and list of
    aliases), allows constructing identity nodes, and does some validation
    on the identity username.
    - `alias.js` implements the logic for parsing aliases like
    "github/decentralion" or "discourse/s_ben" into a node address.
    - `nodeContractions.js` provides logic for turning a list of Identities
    into a list of NodeContractions, suitable for use in
    `Graph.contractNodes`.
    
    The plugin is not yet integrated; that will come in a followon commit.
    
    Test plan: Unit tests added; `yarn test` passes.

---
+++ src/plugins/identity/nodeContractions.test.js
@@ -0,0 +1,68 @@
+// @flow
+
+import {nodeContractions, _contraction} from "./nodeContractions";
+import {resolveAlias} from "./alias";
+import {identityNode} from "./identity";
+
+describe("src/plugins/identity/nodeContractions", () => {
+  describe("_contraction", () => {
+    it("processes an empty identity", () => {
+      const identity = {username: "empty", aliases: []};
+      const actual = _contraction(identity, null);
+      const expected = {old: [], replacement: identityNode(identity)};
+      expect(actual).toEqual(expected);
+    });
+    it("processes a single-alias identity", () => {
+      const alias = "github/foo";
+      const identity = {username: "foo", aliases: [alias]};
+      const actual = _contraction(identity, null);
+      const expected = {
+        old: [resolveAlias(alias, null)],
+        replacement: identityNode(identity),
+      };
+      expect(actual).toEqual(expected);
+    });
+    it("processes a multi-alias identity", () => {
+      const aliases = ["github/foo", "discourse/bar"];
+      const identity = {username: "foo", aliases};
+      const url = "https://example.com";
+      const actual = _contraction(identity, url);
+      const expected = {
+        old: aliases.map((x) => resolveAlias(x, url)),
+        replacement: identityNode(identity),
+      };
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe("nodeContractions", () => {
+    it("errors if any username is duplicated", () => {
+      const identities = [
+        {username: "foo", aliases: ["github/foo", "github/bar"]},
+        {username: "foo", aliases: []},
+      ];
+      expect(() => nodeContractions(identities, null)).toThrowError(
+        "Duplicate username"
+      );
+    });
+    it("errors if any alias is duplicated", () => {
+      const identities = [
+        {username: "foo", aliases: ["github/foo", "github/bar"]},
+        {username: "bar", aliases: ["github/foo"]},
+      ];
+      expect(() => nodeContractions(identities, null)).toThrowError(
+        "Duplicate alias"
+      );
+    });
+    it("produces a contraction for each identity", () => {
+      const identities = [
+        {username: "foo", aliases: ["discourse/foo"]},
+        {username: "bar", aliases: ["github/bar"]},
+      ];
+      const url = "https://example.com";
+      expect(nodeContractions(identities, url)).toEqual(
+        identities.map((i) => _contraction(i, url))
+      );
+    });
+  });
+});
