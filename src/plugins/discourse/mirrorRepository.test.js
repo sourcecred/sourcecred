323708599fa48c9655fbd6c0d3e06b2ece202801
Robin <robin@robin-it.com>
2019-11-13 22:11:44 +0100

    Discourse: adds bumpedMsForTopic and topicsInCategories queries
    bumpedMsForTopic
    For the given topic ID, retrieves the bumpedMs value.
    Returns null, when the topic wasn't found.
    Used by the new update code as a fallback value when making API
    calls that don't contain the bumpedMs field.
    
    topicsInCategories
    Finds the TopicIds of topics that have one of the categoryIds as
    it's category.
    Useful to find out which topics a set of categories contains.
    For example to implement the `recheckTopicsInCategories` mirror
    option, or to locate topics for the initiative plugin.

--- src/plugins/discourse/mirrorRepository.test.js
+++ src/plugins/discourse/mirrorRepository.test.js
@@ -4,6 +4,7 @@ import Database from "better-sqlite3";
 import fs from "fs";
 import tmp from "tmp";
 import {SqliteMirrorRepository} from "./mirrorRepository";
+import type {Topic} from "./fetch";
 
 describe("plugins/discourse/mirrorRepository", () => {
   it("rejects a different server url without changing the database", () => {
@@ -23,5 +24,118 @@ describe("plugins/discourse/mirrorRepository", () => {
 
     expect(() => new SqliteMirrorRepository(db, url1)).not.toThrow();
     expect(fs.readFileSync(filename).toJSON()).toEqual(data);
+  });
+
+  it("bumpedMsForTopic finds an existing topic's bumpedMs", () => {
+    // Given
+    const db = new Database(":memory:");
+    const url = "http://example.com";
+    const repository = new SqliteMirrorRepository(db, url);
+    const topic: Topic = {
+      id: 123,
+      categoryId: 1,
+      title: "Sample topic",
+      timestampMs: 456789,
+      bumpedMs: 456999,
+      authorUsername: "credbot",
+    };
+
+    // When
+    repository.addTopic(topic);
+    const bumpedMs = repository.bumpedMsForTopic(topic.id);
+
+    // Then
+    expect(bumpedMs).toEqual(topic.bumpedMs);
+  });
+
+  it("bumpedMsForTopic returns null when missing topic", () => {
+    // Given
+    const db = new Database(":memory:");
+    const url = "http://example.com";
+    const repository = new SqliteMirrorRepository(db, url);
+
+    // When
+    const bumpedMs = repository.bumpedMsForTopic(123);
+
+    // Then
+    expect(bumpedMs).toEqual(null);
+  });
+
+  it("topicsInCategories finds an an existing topic by categoryId", () => {
+    // Given
+    const db = new Database(":memory:");
+    const url = "http://example.com";
+    const repository = new SqliteMirrorRepository(db, url);
+    const topic: Topic = {
+      id: 123,
+      categoryId: 42,
+      title: "Sample topic",
+      timestampMs: 456789,
+      bumpedMs: 456999,
+      authorUsername: "credbot",
+    };
+
+    // When
+    repository.addTopic(topic);
+    const topicIds = repository.topicsInCategories([topic.categoryId]);
+
+    // Then
+    expect(topicIds).toEqual([topic.id]);
+  });
+
+  it("topicsInCategories with several categoryIds returns all matching topics", () => {
+    // Given
+    const db = new Database(":memory:");
+    const url = "http://example.com";
+    const repository = new SqliteMirrorRepository(db, url);
+    const topic1: Topic = {
+      id: 123,
+      categoryId: 42,
+      title: "Sample topic 1",
+      timestampMs: 456789,
+      bumpedMs: 456999,
+      authorUsername: "credbot",
+    };
+    const topic2: Topic = {
+      id: 456,
+      categoryId: 16,
+      title: "Sample topic 2",
+      timestampMs: 456789,
+      bumpedMs: 456999,
+      authorUsername: "credbot",
+    };
+
+    // When
+    repository.addTopic(topic1);
+    repository.addTopic(topic2);
+    const topicIds = repository.topicsInCategories([
+      topic1.categoryId,
+      topic2.categoryId,
+    ]);
+
+    // Then
+    expect(topicIds).toEqual([topic1.id, topic2.id]);
+  });
+
+  it("topicsInCategories without categoryIds gives no topicIds", () => {
+    // Given
+    const db = new Database(":memory:");
+    const url = "http://example.com";
+    const repository = new SqliteMirrorRepository(db, url);
+    const topic: Topic = {
+      id: 123,
+      categoryId: 42,
+      title: "Sample topic",
+      timestampMs: 456789,
+      bumpedMs: 456999,
+      authorUsername: "credbot",
+    };
+
+    // When
+    repository.addTopic(topic);
+    const topicIds = repository.topicsInCategories([]);
+
+    // Then
+    expect(topicIds).toEqual([]);
   });
 });
