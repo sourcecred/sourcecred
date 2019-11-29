3ceb4fb7fa8b7644cef737d48714046618b1a4fe
Robin van Boven <497556+Beanow@users.noreply.github.com>
2019-11-29 11:53:07 +0100

    GitHub: update token validation function (#1471)
    Previously an inline check was used for this.
    It only accepted the personal access token format.
    This adds installation tokens as requested in #1461.
    
    With more complex logic, we'd benefit from tests.
    Therefore it's a separate function with a test suite.

---
+++ src/plugins/github/token.test.js
@@ -0,0 +1,101 @@
+// @flow
+
+import {validateToken} from "./token";
+
+describe("plugins/github/token", () => {
+  describe("validateToken", () => {
+    function spyWarn(): JestMockFn<[string], void> {
+      return ((console.warn: any): JestMockFn<any, void>);
+    }
+    beforeEach(() => {
+      jest.spyOn(console, "warn").mockImplementation(() => {});
+    });
+    afterEach(() => {
+      try {
+        expect(console.warn).not.toHaveBeenCalled();
+      } finally {
+        spyWarn().mockRestore();
+      }
+    });
+
+    it("should throw on empty tokens", () => {
+      // Given
+      const token = "";
+
+      // When
+      const fn = () => validateToken(token);
+
+      // Then
+      expect(fn).toThrow(
+        "The token supplied to $SOURCECRED_GITHUB_TOKEN doesn't match any format known to work.\n" +
+          'Please verify the token "" is correct, or report a bug if you think it should work.'
+      );
+    });
+
+    it("should throw on an unknown format token", () => {
+      // Given
+      const token = "EXAMPLE-INVALID-TOKEN-1082369";
+
+      // When
+      const fn = () => validateToken(token);
+
+      // Then
+      expect(fn).toThrow(
+        "The token supplied to $SOURCECRED_GITHUB_TOKEN doesn't match any format known to work.\n" +
+          'Please verify the token "EXAMPLE-INVALID-TOKEN-1082369" is correct, or report a bug if you think it should work.'
+      );
+    });
+
+    it("should accept a personal access token format", () => {
+      // Given
+      const token = "1bfb713d900c4962586ec615260b3902438b1d3c";
+
+      // When
+      validateToken(token);
+
+      // Then
+      // Shouldn't throw.
+    });
+
+    it("should accept an installation access token format", () => {
+      // Given
+      const token = "v1.1bfb713d900c4962";
+
+      // When
+      validateToken(token);
+
+      // Then
+      // Shouldn't throw.
+    });
+
+    it("should warn when installation access token has an unexpected version", () => {
+      // Given
+      const token = "v5.1bfb713d900c4962";
+
+      // When
+      validateToken(token);
+
+      // Then
+      expect(console.warn).toHaveBeenCalledWith(
+        'Warning: GitHub installation access token has an unexpected version "v5".'
+      );
+      expect(console.warn).toHaveBeenCalledTimes(1);
+      spyWarn().mockReset();
+    });
+
+    it("should warn when installation access token has an unexpected length", () => {
+      // Given
+      const token = "v1.1bfb713d900c49621bfb713d900c4962";
+
+      // When
+      validateToken(token);
+
+      // Then
+      expect(console.warn).toHaveBeenCalledWith(
+        "Warning: GitHub installation access token has an unexpected hexadecimal component length of 32."
+      );
+      expect(console.warn).toHaveBeenCalledTimes(1);
+      spyWarn().mockReset();
+    });
+  });
+});
