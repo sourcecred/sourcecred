7188ff576bb8fd97abe70c6a6e7a817a7e54ac4d
Robin <robin@robin-it.com>
2019-12-20 15:00:59 +0100

    Release version 0.4.1 (backport) (#1504)
    As explained in #1331 we don't currently have a backport of our
    docker builds for v0.4.0. However we do have a :latest docker
    image which is going to be the default way for people to try out
    the docker image for the first time. That is based on the
    08408a9 commit.
    
    However, considering the #1503 security patch for our docker image,
    I would like to make sure the :latest tag receives this patch as well.
    The most straightforward way is to create a v0.4.1 patch release.

--- src/core/version.js
+++ src/core/version.js
@@ -61,7 +61,7 @@ const environment = parseEnvironment(process.env.NODE_ENV);
 export const VERSION_INFO: VersionInfo = deepFreeze({
   major: 0,
   minor: 4,
-  patch: 0,
+  patch: 1,
   gitState,
   environment,
 });
