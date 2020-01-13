063c08d97435400dd9715b79b6afa3875ec3c3c0
Robin van Boven <497556+Beanow@users.noreply.github.com>
2020-01-13 14:13:30 +0100

    UniRef: add GithubReferenceDetector (#1543)
    As a RelationalView is not designed for multiple repositories, we
    should implement our own merging of mappings obtained from
    RelationalViews.
    
    fromRelationalViews is a factory function which does this for us.
    And by accepting an array of RelationalViews it's more apparent
    it should be used this way.

---
+++ src/plugins/github/referenceDetector.js
@@ -0,0 +1,37 @@
+// @flow
+
+import dedent from "../../util/dedent";
+import {type NodeAddressT, NodeAddress} from "../../core/graph";
+import {MappedReferenceDetector, type URL} from "../../core/references";
+import {RelationalView} from "./relationalView";
+
+/**
+ * Builds a GithubReferenceDetector using multiple RelationalView.
+ * As RelationalView should only be used for one repository at a time, you will
+ * commonly want to compose several of them into one GithubReferenceDetector.
+ *
+ * Note: duplicates are normally expected. However for any URL, the corresponding
+ * NodeAddressT should be the same, or we'll throw an error.
+ */
+export function fromRelationalViews(
+  views: $ReadOnlyArray<RelationalView>
+): GithubReferenceDetector {
+  const map: Map<URL, NodeAddressT> = new Map();
+  for (const view of views) {
+    for (const [url, addr] of view.urlReferenceMap().entries()) {
+      const existing = map.get(url);
+      if (existing && existing != addr) {
+        throw new Error(dedent`\
+          An entry for ${url} already existed, but with a different NodeAddressT.
+          This is probably a bug with SourceCred. Please report it on GitHub.
+          Old: ${NodeAddress.toString(existing)}
+          New: ${NodeAddress.toString(addr)}
+        `);
+      }
+      map.set(url, addr);
+    }
+  }
+  return new GithubReferenceDetector(map);
+}
+
+export const GithubReferenceDetector = MappedReferenceDetector;
