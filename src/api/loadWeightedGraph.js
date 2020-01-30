b5be554d63aef2135306d8353da94f24b2b23cd0
Dandelion Mané <decentralion@dandelion.io>
2020-01-30 15:22:57 -0800

    add api/loadWeightedGraph (#1604)
    This adds a new module the api directory which loads a combined
    WeightedGraph across all available plugins. This is intended as a key
    piece of a future, less-tightly-coupled load pipeline which will produce
    WeightedGraphs, as required by #1557.
    
    Test plan:
    The "clean" logic (combining graphs, applying transformations,
    overriding weights) is tested explicitly. The "unclean" logic, which
    involves directly generating graphs from Discourse/GitHub, are untested.
    Arguably we could test with mocks, I'm dubious that doing so would add
    real value. I think most of the potential issues (especially
    refactoring-induced issues) would get caught by Flow. This is also one
    of those "works perfectly or is totally broken" type situations. (Thus,
    the likelihood of costly "subtle failures" is low.)

---
+++ src/api/loadWeightedGraph.js
@@ -0,0 +1,76 @@
+// @flow
+
+import * as WeightedGraph from "../core/weightedGraph";
+import {type WeightedGraph as WeightedGraphT} from "../core/weightedGraph";
+import * as Weights from "../core/weights";
+import {type Weights as WeightsT} from "../core/weights";
+import {type NodeContraction} from "../core/graph";
+import {TaskReporter} from "../util/taskReporter";
+import {type IdentitySpec} from "../plugins/identity/identity";
+import {contractWeightedGraph} from "../plugins/identity/contractIdentities";
+import {nodeContractions} from "../plugins/identity/nodeContractions";
+import * as Discourse from "../plugins/discourse/loadWeightedGraph";
+import * as Github from "../plugins/github/loadWeightedGraph";
+
+export type LoadWeightedGraphOptions = {|
+  +discourseOptions: ?Discourse.Options,
+  +githubOptions: ?Github.Options,
+  +identitySpec: IdentitySpec,
+  +weightsOverrides: WeightsT,
+|};
+
+export async function loadWeightedGraph(
+  options: LoadWeightedGraphOptions,
+  taskReporter: TaskReporter
+): Promise<WeightedGraphT> {
+  taskReporter.start("load-weighted-graph");
+  const {
+    discourseOptions,
+    githubOptions,
+    identitySpec,
+    weightsOverrides,
+  } = options;
+  const pluginGraphs = await _loadPluginGraphs(
+    discourseOptions,
+    githubOptions,
+    taskReporter
+  );
+  const contractions = nodeContractions(identitySpec);
+  const result = _combineGraphs(pluginGraphs, contractions, weightsOverrides);
+  taskReporter.finish("load-weighted-graph");
+  return result;
+}
+
+export function _loadPluginGraphs(
+  discourseOptions: ?Discourse.Options,
+  githubOptions: ?Github.Options,
+  taskReporter: TaskReporter
+): Promise<$ReadOnlyArray<WeightedGraphT>> {
+  const promises: Promise<WeightedGraphT>[] = [];
+  if (discourseOptions) {
+    const promise = Discourse.loadWeightedGraph(discourseOptions, taskReporter);
+    promises.push(promise);
+  }
+  if (githubOptions) {
+    const promise = Github.loadWeightedGraph(githubOptions, taskReporter);
+    promises.push(promise);
+  }
+  // It's important to use Promise.all so that we can load the plugins in
+  // parallel -- since loading is often IO-bound, this can be a big performance
+  // improvement.
+  return Promise.all(promises);
+}
+
+export function _combineGraphs(
+  graphs: $ReadOnlyArray<WeightedGraphT>,
+  contractions: $ReadOnlyArray<NodeContraction>,
+  weightsOverrides: WeightsT
+): WeightedGraphT {
+  const merged = WeightedGraph.merge(graphs);
+  const contracted = contractWeightedGraph(merged, contractions);
+  const weights = Weights.merge([contracted.weights, weightsOverrides], {
+    nodeResolver: (a, b) => b,
+    edgeResolver: (a, b) => b,
+  });
+  return {graph: contracted.graph, weights};
+}
