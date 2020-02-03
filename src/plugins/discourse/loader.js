c9c42335e7113ad9d1ed9af5e83b74395bf60460
Robin <robin@robin-it.com>
2020-02-03 14:18:25 +0100

    Backend: implement PluginLoaders.updateMirror
    Note, the return type is a CachedProject. See #1586 for discussion.
    Having this type allows us to create new functions with a semantic
    of requiring the project is mirrored into cache. It is opaque,
    because only the "all plugins" semantic which PluginsLoaders has
    could know when mirroring of a Project has been completed.
    
    Additionally MirrorEnv is not a strict type. We're expecting this
    to be a subset of parameters. We'll use Flow to ensure we only use
    the ones we need from it.

--- src/plugins/discourse/loader.js
+++ src/plugins/discourse/loader.js
@@ -1,12 +1,50 @@
 // @flow
 
+import base64url from "base64url";
+import {TaskReporter} from "../../util/taskReporter";
+import {type CacheProvider} from "../../backend/cache";
 import {type PluginDeclaration} from "../../analysis/pluginDeclaration";
+import {type MirrorOptions, Mirror} from "./mirror";
+import {SqliteMirrorRepository} from "./mirrorRepository";
 import {declaration} from "./declaration";
+import {Fetcher} from "./fetch";
+
+export type DiscourseServer = {|
+  +serverUrl: string,
+  +mirrorOptions?: $Shape<MirrorOptions>,
+|};
 
 export interface Loader {
   declaration(): PluginDeclaration;
+  updateMirror(
+    server: DiscourseServer,
+    cache: CacheProvider,
+    reporter: TaskReporter
+  ): Promise<void>;
 }
 
 export default ({
   declaration: () => declaration,
+  updateMirror,
 }: Loader);
+
+export async function updateMirror(
+  server: DiscourseServer,
+  cache: CacheProvider,
+  reporter: TaskReporter
+): Promise<void> {
+  const {serverUrl, mirrorOptions} = server;
+  const repo = await repository(cache, serverUrl);
+  const fetcher = new Fetcher({serverUrl});
+  const mirror = new Mirror(repo, fetcher, serverUrl, mirrorOptions);
+  await mirror.update(reporter);
+}
+
+async function repository(
+  cache: CacheProvider,
+  serverUrl: string
+): Promise<SqliteMirrorRepository> {
+  // TODO: should replace base64url with hex, to be case insensitive.
+  const db = await cache.database(base64url.encode(serverUrl));
+  return new SqliteMirrorRepository(db, serverUrl);
+}
