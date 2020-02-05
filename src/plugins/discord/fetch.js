ec16d651082f74a3735f9a6d03a9b318f4c3c7bf
Brian Litwin <blitwin@horizoninvestments.com>
2020-02-05 10:38:10 -0500

    emoji query

--- src/plugins/discord/fetch.js
+++ src/plugins/discord/fetch.js
@@ -5,7 +5,8 @@ import sortBy from "lodash.sortby";
 const DISCORD_SERVER = "https://discordapp.com/api"
 const guildid = "453243919774253079"
 const channel = "543168537062014987"
-const url = `https://discordapp.com/api/v6/channels/${channel}/messages?667618906734592031&limit=10`
+const urlChannel = `${DISCORD_SERVER}/channels/${channel}`
+const url = `${urlChannel}/messages?667618906734592031&limit=100`
 const CLIENT_ID = "673208303567765516"
 const CLIENT_SECRET = "sRemXufBbspQ6akkk_n-eSq3uQejYmv_"
 const redirect = "http://localhost:8000"
@@ -22,6 +23,14 @@ const fetchOptions = {
 
 //before=667596440058462209
 
+const postRequest = (before) => {
+  if (before !== null) {
+    return fetch(url + `&before=${before}`, fetchOptions)
+  } else {
+    return fetch(url, fetchOptions)
+  }
+}
+
 export const f = () => {
   // fetch(url, fetchOptions).then((response) => {
   //   response.json().then((x) =>
@@ -32,26 +41,68 @@ export const f = () => {
   //       }
   //   })
   // })
-  const postRequest = () => fetch(url, fetchOptions)
-  loadMessages(postRequest)
+  loadMessages(postRequest, null)
+}
+
+//reactions/%F0%9F%91%8D/%40me
+
+function emojiQuery(reaction) {
+  const name = reaction.emoji.name
+  const id = reaction.emoji.id
+  const encodedName = encodeURI(`${reaction.emoji.name}`)
+  if (name === null && id === null) {
+    return ''
+  } else if (name === null) {
+    return id
+  } else if (id === null) {
+    return encodedName
+  } else {
+    return `${encodedName}%3A${id}`
+  }
+}
+
+async function fetchReactions(message, reaction) {
+  const query = emojiQuery(reaction)
+  const url = `${urlChannel}/messages/${message.id}/reactions/${query}?limit=100`
+  // const url = "https://discordapp.com/api/v6/channels/543168537062014987/messages/674125310228430858/reactions/sourcecred%3A626763367893303303?limit=100"
+  console.log(`fetching url ${url}`)
+  try {
+    const response = await fetch(url, fetchOptions)
+    const data = await response.json()
+    if (data.code === 10014) {
+  
+    }
+
+  } catch(e)  {
+    console.log(e)
+    console.log(url)
+  }
 }
 
 
-async function loadMessages(postRequest) {
+async function loadMessages(postRequest, before) {
   try {
-    const response = await postRequest()
+    const response = await postRequest(before)
     const messages = await response.json()
 
-    if (messages.length > 0) {
-      const lastMessage = sortBy(messages, (x) => x.timestamp)[0].id 
+    for (const message of messages) {
+      const reactions = message.reactions
+      if (reactions !== undefined) {
+        for (const reaction of reactions) {
+          fetchReactions(message, reaction)
+        }
+      }
+    }
 
+    if (messages.length > 0) {
+      const lastMessage = sortBy(messages, (x) => x.timestamp)[0]
+      //loadMessages(postRequest, lastMessage.id)
+
+    } else {
 
     }
 
-
   } catch (err) {
-
-  }
-
-
+    console.log(err)
+ }
 }
