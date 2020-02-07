756209d8b1ad8da33ff94370fd656d6e4ca690e6
Robin <robin@robin-it.com>
2020-02-07 12:05:15 +0100

    Initiatives: validate and read files from local directory
    Helper functions intended to be used in succession by `loadDirectory`.
    Only `_validatePath` provides helfpul error messages. It's the caller's
    responsiblity to do this first.
    
    Introduces dependency `globby` for globbing with a Promises API.

--- src/plugins/initiatives/initiativesDirectory.test.js
+++ src/plugins/initiatives/initiativesDirectory.test.js
@@ -1,5 +1,8 @@
 // @flow
 
+import tmp from "tmp";
+import path from "path";
+import fs from "fs-extra";
 import {NodeAddress} from "../../core/graph";
 import {createId, addressFromId} from "./initiative";
 import {
@@ -9,6 +12,9 @@ import {
   toJSON,
   initiativeFileURL,
   _initiativeFileId,
+  _validatePath,
+  _findFiles,
+  _readFiles,
 } from "./initiativesDirectory";
 
 const exampleInitiativeFile = (): InitiativeFile => ({
@@ -77,6 +83,104 @@ describe("plugins/initiatives/initiativesDirectory", () => {
 
       // Then
       expect(id).toEqual(createId("INITIATIVE_FILE", dir.remoteUrl, fileName));
+    });
+  });
+
+  describe("_validatePath", () => {
+    it("should resolve relative paths", async () => {
+      // Given
+      const localPath = `${__dirname}/./test/../example/`;
+
+      // When
+      const actual = await _validatePath(localPath);
+
+      // Then
+      expect(actual).toEqual(path.join(__dirname, "example"));
+    });
+
+    it("should throw when directory doesn't exist", async () => {
+      // Given
+      const localPath = path.join(tmp.dirSync().name, "findFiles_test");
+
+      // When
+      const p = _validatePath(localPath);
+
+      // Then
+      await expect(p).rejects.toThrow(
+        `Provided initiatives directory does not exist at: ${localPath}`
+      );
+    });
+
+    it("should throw when directory is not a directory", async () => {
+      // Given
+      const localPath = path.join(tmp.dirSync().name, "findFiles_test");
+      await fs.writeFile(localPath, "");
+
+      // When
+      const p = _validatePath(localPath);
+
+      // Then
+      await expect(p).rejects.toThrow(
+        `Provided initiatives directory is not a directory at: ${localPath}`
+      );
+    });
+  });
+
+  describe("_findFiles", () => {
+    it("should locate all *.json files in a local path", async () => {
+      // Given
+      const localPath = path.join(__dirname, "example");
+
+      // When
+      const fileNames = await _findFiles(localPath);
+
+      // Then
+      // Shallow copy to sort, because the array is read-only.
+      const actualNames = [...fileNames].sort();
+      expect(actualNames).toEqual(["initiative-A.json", "initiative-B.json"]);
+    });
+  });
+
+  describe("_readFiles", () => {
+    it("should read provided initiativeFiles, sorted by name", async () => {
+      // Given
+      const localPath = path.join(__dirname, "example");
+      const fileNames = ["initiative-B.json", "initiative-A.json"];
+
+      // When
+      const map = await _readFiles(localPath, fileNames);
+
+      // Then
+      expect([...map.keys()]).toEqual([
+        "initiative-A.json",
+        "initiative-B.json",
+      ]);
+      expect(map).toMatchSnapshot();
+    });
+
+    it("should throw when directory doesn't exist", async () => {
+      // Given
+      const localPath = path.join(tmp.dirSync().name, "findFiles_test");
+      const fileNames = ["initiative-B.json", "initiative-A.json"];
+
+      // When
+      const p = _readFiles(localPath, fileNames);
+
+      // Then
+      await expect(p).rejects.toThrow("Could not find Initiative file at:");
+    });
+
+    it("should throw when directory is not a directory", async () => {
+      // Given
+      const localPath = path.join(tmp.dirSync().name, "findFiles_test");
+      const fileNames = ["initiative-B.json", "initiative-A.json"];
+      await fs.writeFile(localPath, "");
+
+      // When
+      const p = _readFiles(localPath, fileNames);
+
+      // Then
+      await expect(p).rejects.toThrow("Could not find Initiative file at:");
     });
   });
 });
