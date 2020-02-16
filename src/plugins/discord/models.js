6c5017003036891d3c77428edee2201f451c6bf2
Robin <robin@robin-it.com>
2020-02-16 02:22:43 +0100

    WIP: flesh out fetcher and models

--- src/plugins/discord/models.js
+++ src/plugins/discord/models.js
@@ -1,13 +1,102 @@
 // @flow
 
-type Snowflake = string;
-
-export type GuildId = Snowflake;
+// https://discordapp.com/developers/docs/reference#snowflakes
+export type Snowflake = string;
+export const ZeroSnowflake: Snowflake = "0";
 
 export type Guild = {|
-  +id: GuildId,
+  +id: Snowflake,
   +name: string,
   +permissions: number,
 |};
 
 export type BotToken = string;
+
+// https://discordapp.com/developers/docs/resources/channel#channel-object-channel-types
+export type ChannelType =
+  | "GUILD_TEXT"
+  | "DM"
+  | "GUILD_VOICE"
+  | "GROUP_DM"
+  | "GUILD_CATEGORY"
+  | "GUILD_NEWS"
+  | "GUILD_STORE";
+
+export function channelTypeFromId(id: number): ChannelType {
+  switch (id) {
+    case 0:
+      return "GUILD_TEXT";
+    case 1:
+      return "DM";
+    case 2:
+      return "GUILD_VOICE";
+    case 3:
+      return "GROUP_DM";
+    case 4:
+      return "GUILD_CATEGORY";
+    case 5:
+      return "GUILD_NEWS";
+    case 6:
+      return "GUILD_STORE";
+    default: {
+      throw new Error(`Unknown channel type ID: ${id}`);
+    }
+  }
+}
+
+export type Channel = {|
+  +id: Snowflake,
+  +type: ChannelType,
+  +name: string,
+|};
+
+export type Role = {|
+  +id: Snowflake,
+  +name: string,
+|};
+
+export type User = {|
+  +id: Snowflake,
+  +username: string,
+  +discriminator: string,
+  +bot: boolean,
+|};
+
+export type GuildMember = {|
+  +user: User,
+  +nick: ?string,
+  +roles: $ReadOnlyArray<Snowflake>,
+|};
+
+export type Emoji = {|
+  +id: ?Snowflake,
+  +name: string,
+|};
+
+export type EmojiRef = string;
+export function emojiToRef({id, name}: Emoji): EmojiRef {
+  // Built-in emoji, unicode names.
+  if (!id) return name;
+
+  // Custom emoji.
+  return `${name}:${id}`;
+}
+
+export type Message = {|
+  +id: Snowflake,
+  +authorId: Snowflake,
+  // Could be a message from a webhook, meaning the authorId isn't a user.
+  +nonUserAuthor: boolean,
+  +timestampMs: number,
+  +content: string,
+  // Normally includes reaction counters, but we don't care about counters.
+  // We could filter based on which types of emoji have been added though.
+  +reactionEmoji: $ReadOnlyArray<Emoji>,
+|};
+
+export type Reaction = {|
+  +channelId: Snowflake,
+  +messageId: Snowflake,
+  +authorId: Snowflake,
+  +emoji: EmojiRef,
+|};
