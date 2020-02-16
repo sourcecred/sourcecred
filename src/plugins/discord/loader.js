d1d682a10b9709983f54e01d5ce75ccb2d535ef4
Robin <robin@robin-it.com>
2020-02-16 09:45:54 -0700

    WIP: implement reaction-minted createGraph

--- src/plugins/discord/loader.js
+++ src/plugins/discord/loader.js
@@ -5,7 +5,8 @@ import {type WeightedGraph} from "../../core/weightedGraph";
 import {TaskReporter} from "../../util/taskReporter";
 import {type CacheProvider} from "../../backend/cache";
 import {SqliteMirrorRepository} from "./mirrorRepository";
-import {createGraph as _createGraph} from "./createGraph";
+import {weightsForDeclaration} from "../../analysis/pluginDeclaration";
+import {type EmojiWeightMap, createGraph as _createGraph} from "./createGraph";
 import {declaration} from "./declaration";
 import * as Model from "./models";
 import {Fetcher} from "./fetcher";
@@ -13,16 +14,8 @@ import {Mirror} from "./mirror";
 
 export interface Loader {
   declaration(): PluginDeclaration;
-  updateMirror(
-    guild: Model.Snowflake,
-    token: Model.BotToken,
-    cache: CacheProvider,
-    reporter: TaskReporter
-  ): Promise<void>;
-  createGraph(
-    guild: Model.Snowflake,
-    cache: CacheProvider
-  ): Promise<WeightedGraph>;
+  updateMirror: typeof updateMirror;
+  createGraph: typeof createGraph;
 }
 
 export default ({
@@ -45,10 +38,12 @@ export async function updateMirror(
 
 export async function createGraph(
   guild: Model.Snowflake,
-  cache: CacheProvider
+  cache: CacheProvider,
+  emojiWeights: EmojiWeightMap
 ): Promise<WeightedGraph> {
   const repo = await repository(cache, guild);
-  return await _createGraph(guild, repo);
+  const declarationWeights = weightsForDeclaration(declaration);
+  return await _createGraph(guild, repo, declarationWeights, emojiWeights);
 }
 
 async function repository(
