aa2a52bd24f6db8debdeed291459a78de297fe2f
Robin <robin@robin-it.com>
2020-05-08 21:00:45 +0200

    Refactor: separate pagination to it's own util functions

--- src/plugins/discord/mirror.js
+++ src/plugins/discord/mirror.js
@@ -6,9 +6,6 @@ import {type Snowflake} from "./models";
 import * as Model from "./models";
 import * as nullUtil from "../../util/null";
 
-// Unused
-// type FetchType<T> = (Snowflake) => Promise<ResultPage<T>>;
-
 /**
  * Mirrors data from the Discord API into a local sqlite db.
  */
@@ -16,6 +13,7 @@ export class Mirror {
   +_guildId: Snowflake;
   +_sqliteMirror: SqliteMirror;
   +_fetcher: DiscordFetcher;
+  +_streams: StreamingFetcher;
 
   constructor(
     guildId: Snowflake,
@@ -25,71 +23,17 @@ export class Mirror {
     this._guildId = guildId;
     this._sqliteMirror = sqliteMirror;
     this._fetcher = fetcher;
-  }
-
-  async *_streamPages<T>(
-    fetchPage: (endCursor: Snowflake) => Promise<ResultPage<T>>,
-    endCursor: Snowflake
-  ): AsyncGenerator<T, void, void> {
-    let after = endCursor;
-    let hasNextPage = true;
-
-    while (hasNextPage) {
-      const {results, pageInfo} = await fetchPage(after);
-      for (const result of results) {
-        yield result;
-      }
-      hasNextPage = pageInfo.hasNextPage;
-      if (hasNextPage) {
-        after = nullUtil.get(
-          pageInfo.endCursor,
-          `Found null endCursor with hasNextPage = true`
-        );
-      }
-    }
-  }
-
-  _members(
-    guildId: Snowflake,
-    after: ?Snowflake
-  ): AsyncGenerator<Model.GuildMember, void, void> {
-    return this._streamPages(
-      (after: Snowflake) => this._fetcher.members(guildId, after),
-      after || "0"
-    );
-  }
-
-  _messages(
-    channel: Snowflake,
-    after: ?Snowflake
-  ): AsyncGenerator<Model.Message, void, void> {
-    return this._streamPages(
-      (after: Snowflake) => this._fetcher.messages(channel, after),
-      after || "0"
-    );
-  }
-
-  _reactions(
-    channel: Snowflake,
-    message: Snowflake,
-    emoji: Model.Emoji,
-    after: ?Snowflake
-  ): AsyncGenerator<Model.Reaction, void, void> {
-    return this._streamPages(
-      (after: Snowflake) =>
-        this._fetcher.reactions(channel, message, emoji, after),
-      after || "0"
-    );
+    this._streams = streamingFetcher(fetcher);
   }
 
   async updateChannel(channel: Snowflake) {
     // TODO: don't load messages from "0" when we can use cache.
-    const messages = this._messages(channel, "0");
+    const messages = this._streams.messages(channel, "0");
     for await (const message of messages) {
       this._sqliteMirror.addMessage(message);
 
       for (const emoji of message.reactionEmoji) {
-        const reactions = this._reactions(channel, message.id, emoji);
+        const reactions = this._streams.reactions(channel, message.id, emoji);
         for await (const reaction of reactions) {
           this._sqliteMirror.addReaction(reaction);
         }
@@ -99,7 +43,7 @@ export class Mirror {
 
   async update() {
     // Updates the server members. We always get all of them.
-    for await (const member of this._members(this._guildId)) {
+    for await (const member of this._streams.members(this._guildId)) {
       this._sqliteMirror.addMember(member);
     }
 
@@ -107,6 +51,82 @@ export class Mirror {
     for (const channel of channels) {
       this._sqliteMirror.addChannel(channel);
       await this.updateChannel(channel.id);
+    }
+  }
+}
+
+// Note: most of this is really about wrapping pagination.
+interface StreamingFetcher {
+  members(
+    guildId: Snowflake,
+    after: ?Snowflake
+  ): AsyncGenerator<Model.GuildMember, void, void>;
+  messages(
+    channel: Snowflake,
+    after: ?Snowflake
+  ): AsyncGenerator<Model.Message, void, void>;
+  reactions(
+    channel: Snowflake,
+    message: Snowflake,
+    emoji: Model.Emoji,
+    after: ?Snowflake
+  ): AsyncGenerator<Model.Reaction, void, void>;
+}
+
+function streamingFetcher(fetcher: DiscordFetcher): StreamingFetcher {
+  return {
+    members(
+      guildId: Snowflake,
+      after: ?Snowflake
+    ): AsyncGenerator<Model.GuildMember, void, void> {
+      return streamPages(
+        (after: Snowflake) => fetcher.members(guildId, after),
+        after || "0"
+      );
+    },
+
+    messages(
+      channel: Snowflake,
+      after: ?Snowflake
+    ): AsyncGenerator<Model.Message, void, void> {
+      return streamPages(
+        (after: Snowflake) => fetcher.messages(channel, after),
+        after || "0"
+      );
+    },
+
+    reactions(
+      channel: Snowflake,
+      message: Snowflake,
+      emoji: Model.Emoji,
+      after: ?Snowflake
+    ): AsyncGenerator<Model.Reaction, void, void> {
+      return streamPages(
+        (after: Snowflake) => fetcher.reactions(channel, message, emoji, after),
+        after || "0"
+      );
+    },
+  };
+}
+
+async function* streamPages<T>(
+  fetchPage: (endCursor: Snowflake) => Promise<ResultPage<T>>,
+  endCursor: Snowflake
+): AsyncGenerator<T, void, void> {
+  let after = endCursor;
+  let hasNextPage = true;
+
+  while (hasNextPage) {
+    const {results, pageInfo} = await fetchPage(after);
+    for (const result of results) {
+      yield result;
+    }
+    hasNextPage = pageInfo.hasNextPage;
+    if (hasNextPage) {
+      after = nullUtil.get(
+        pageInfo.endCursor,
+        `Found null endCursor with hasNextPage = true`
+      );
     }
   }
 }
