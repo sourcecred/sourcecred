338ab3b01656e6072b1057f68ad38a1f7fda10e7
Robin van Boven <497556+Beanow@users.noreply.github.com>
2020-05-08 16:31:26 +0200

    Release: bump version to 0.5.0 (#1787)
    See tracking issue #1679 for release notes.

--- src/core/version.js
+++ src/core/version.js
@@ -60,7 +60,7 @@ const environment = parseEnvironment(process.env.NODE_ENV);
 
 export const VERSION_INFO: VersionInfo = deepFreeze({
   major: 0,
-  minor: 4,
+  minor: 5,
   patch: 0,
   gitState,
   environment,
