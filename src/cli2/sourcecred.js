0449c9ea3746bbbc2f459a4a715e4c0463212c9c
William Chargin <wchargin@gmail.com>
2020-05-28 18:33:04 -0700

    cli2: add skeleton of new CLI (#1809)
    Summary:
    This patch creates a new binary, `./bin/sc2`, which will be the home for
    a rewrite of the CLI intended to implement an instance system. See:
    <https://discourse.sourcecred.io/t/sourcecred-instance-system/244>
    
    Paired with @decentralion.
    
    Test Plan:
    Run `yarn backend && node ./bin/sc2.js`, which should nicely fail with a
    “not yet implemented” message.
    
    wchargin-branch: cli2-skeleton

---
+++ src/cli2/sourcecred.js
@@ -0,0 +1,10 @@
+// @flow
+
+import type {Command} from "./command";
+
+const sourcecred: Command = async (args, std) => {
+  std.err("SourceCred CLI v2 not yet implemented");
+  return 1;
+};
+
+export default sourcecred;
