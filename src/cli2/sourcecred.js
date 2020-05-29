01af94f14d2e6e6220266c40189e03a706a3d928
William Chargin <wchargin@gmail.com>
2020-05-28 18:55:09 -0700

    [update patch]
    wchargin-branch: cli2-load
    wchargin-source: 529877b5799df838fc42af78c5d56977d79111d8

--- src/cli2/sourcecred.js
+++ src/cli2/sourcecred.js
@@ -2,7 +2,6 @@
 
 import type {Command} from "./command";
 
-<<<<<<< HEAD
 import load from "./load";
 
 const sourcecred: Command = async (args, std) => {
@@ -17,11 +16,6 @@ const sourcecred: Command = async (args, std) => {
       std.err("fatal: unknown command: " + JSON.stringify(args[0]));
       return 1;
   }
-=======
-const sourcecred: Command = async (args, std) => {
-  std.err("SourceCred CLI v2 not yet implemented");
-  return 1;
->>>>>>> 0f6a76556958d00ea65a54fbd2aa666f0812162b
 };
 
 export default sourcecred;
