8d88ea937b1a802ddd4bbab7a2d98db579259cd1
William Chargin <wchargin@gmail.com>
2020-05-28 19:01:03 -0700

    sc2: add graph subcommand (#1811)
    Summary:
    Paired with @decentralion.
    
    Test Plan:
    Follow the test plan for #1810, then additionally run
    
    ```
    (cd /tmp/test-instance && node "$OLDPWD/bin/sc2.js" graph)
    ```
    
    and note that the `output/graphs/...` directory has a graph JSON file.
    
    wchargin-branch: cli2-graph

--- src/cli2/sourcecred.js
+++ src/cli2/sourcecred.js
@@ -3,6 +3,7 @@
 import type {Command} from "./command";
 
 import load from "./load";
+import graph from "./graph";
 
 const sourcecred: Command = async (args, std) => {
   if (args.length === 0) {
@@ -12,6 +13,8 @@ const sourcecred: Command = async (args, std) => {
   switch (args[0]) {
     case "load":
       return load(args.slice(1), std);
+    case "graph":
+      return graph(args.slice(1), std);
     default:
       std.err("fatal: unknown command: " + JSON.stringify(args[0]));
       return 1;
