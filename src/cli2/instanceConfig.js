109ebb04175bd669e79308333a1927b1a6c2651b
William Chargin <wchargin@gmail.com>
2020-05-30 16:04:04 -0700

    combo: convert instance and GitHub configs (#1820)
    Summary:
    This patch converts the `instanceConfig` and `github/config` parsers
    from hand-written traversals to combinator-based structures. The
    resulting code is much simpler and easier to read.
    
    Test Plan:
    Running the `sc2` CLI for `load` and `graph` still works in the happy
    path, with the expected errors if keys in the config files are changed
    to the wrong strings.
    
    wchargin-branch: combo-instance-github

--- src/cli2/instanceConfig.js
+++ src/cli2/instanceConfig.js
@@ -1,5 +1,6 @@
 // @flow
 
+import * as Combo from "../util/combo";
 import {CliPlugin} from "./cliPlugin";
 import {bundledPlugins as getAllBundledPlugins} from "./bundledPlugins";
 
@@ -19,37 +20,25 @@ export type RawInstanceConfig = {|
 // dependencies.
 export type BundledPluginSpec = string;
 
-type JsonObject =
-  | string
-  | number
-  | boolean
-  | null
-  | JsonObject[]
-  | {[string]: JsonObject};
+const parser: Combo.Parser<InstanceConfig> = (() => {
+  const C = Combo;
+  function makePluginMap(bundledPluginNames) {
+    const allBundledPlugins = getAllBundledPlugins();
+    const bundledPlugins = new Map();
+    for (const name of bundledPluginNames) {
+      const plugin = allBundledPlugins[name];
+      if (plugin == null) {
+        throw new Error("bad bundled plugin: " + JSON.stringify(name));
+      }
+      bundledPlugins.set(name, plugin);
+    }
+    return bundledPlugins;
+  }
+  return C.object({
+    bundledPlugins: C.fmap(C.array(C.string), makePluginMap),
+  });
+})();
 
-export function parse(raw: JsonObject): InstanceConfig {
-  if (raw == null || typeof raw !== "object" || Array.isArray(raw)) {
-    throw new Error("bad config: " + JSON.stringify(raw));
-  }
-  const {bundledPlugins: rawBundledPlugins} = raw;
-  if (!Array.isArray(rawBundledPlugins)) {
-    console.warn(JSON.stringify(raw));
-    throw new Error(
-      "bad bundled plugins: " + JSON.stringify(rawBundledPlugins)
-    );
-  }
-  const allBundledPlugins = getAllBundledPlugins();
-  const bundledPlugins = new Map();
-  for (const name of rawBundledPlugins) {
-    if (typeof name !== "string") {
-      throw new Error("bad bundled plugin: " + JSON.stringify(name));
-    }
-    const plugin = allBundledPlugins[name];
-    if (plugin == null) {
-      throw new Error("bad bundled plugin: " + JSON.stringify(name));
-    }
-    bundledPlugins.set(name, plugin);
-  }
-  const result = {bundledPlugins};
-  return result;
+export function parse(raw: Combo.JsonObject): InstanceConfig {
+  return parser.parseOrThrow(raw);
 }
