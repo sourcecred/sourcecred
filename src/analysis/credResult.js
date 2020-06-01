ce34b1ab06b52693046df2bf8a3e1efb43f3335a
Dandelion Mané <decentralion@dandelion.io>
2020-05-31 21:29:43 -0700

    Add the CredResult module
    This module has a concise and clean type for storing all the output data
    from running SourceCred, including:
    - The graph
    - The weights
    - The cred scores and flows
    - The interval timing info
    - The plugins used
    - The parameters used
    
    We also have a method `compute` which computes one of these given the
    weighted graph, parameters, and plugins. It's all quite clean and
    simple. I feel good about thsi API.
    
    Test plan:
    The main `compute` function is only sanity checked via flow and unit
    testing, which is appropriate since it's a pure-piping function with
    little than can go wrong. I've also added JSON serialization; this is
    tested with round trip testing.

---
+++ src/analysis/credResult.js
@@ -0,0 +1,87 @@
+// @flow
+
+import {
+  type WeightedGraph,
+  type WeightedGraphJSON,
+  toJSON as wgToJSON,
+  fromJSON as wgFromJSON,
+} from "../core/weightedGraph";
+import {type Compatible, toCompat, fromCompat} from "../util/compat";
+import {
+  type TimelineCredParameters,
+  type TimelineCredParametersJSON,
+  paramsToJSON,
+  paramsFromJSON,
+} from "./timeline/params";
+import {
+  type PluginDeclarations,
+  type PluginDeclarationsJSON,
+  toJSON as pluginsToJSON,
+  fromJSON as pluginsFromJSON,
+} from "./pluginDeclaration";
+import {timelinePagerank} from "../core/algorithm/timelinePagerank";
+import {type CredData, computeCredData} from "./credData";
+import {distributionToCred} from "../core/algorithm/distributionToCred";
+
+/**
+ * Comprehensive cred output data, including the graph, the scores, the params, and the plugins.
+ */
+export type CredResult = {|
+  // The Graph on which this cred data was computed.
+  +weightedGraph: WeightedGraph,
+  // Cred level information, always stored in graph address order.
+  +credData: CredData,
+  // Parameters used to compute this cred data
+  +params: TimelineCredParameters,
+  // Plugin declarations used to compute this cred data
+  +plugins: PluginDeclarations,
+|};
+
+export async function compute(
+  wg: WeightedGraph,
+  params: TimelineCredParameters,
+  plugins: PluginDeclarations
+): Promise<CredResult> {
+  const {graph} = wg;
+  const nodeOrder = Array.from(graph.nodes()).map((x) => x.address);
+  const userTypes = [].concat(...plugins.map((x) => x.userTypes));
+  const scorePrefixes = userTypes.map((x) => x.prefix);
+  const distribution = await timelinePagerank(
+    wg,
+    params.intervalDecay,
+    params.alpha
+  );
+  const credScores = distributionToCred(distribution, nodeOrder, scorePrefixes);
+  const credData = computeCredData(credScores);
+  return {weightedGraph: wg, credData, params, plugins};
+}
+
+const COMPAT_INFO = {type: "sourcecred/credResult", version: "0.1.0"};
+
+export type CredResultJSON = Compatible<{|
+  +weightedGraph: WeightedGraphJSON,
+  +credData: CredData,
+  +params: TimelineCredParametersJSON,
+  +plugins: PluginDeclarationsJSON,
+|}>;
+
+export function toJSON(x: CredResult): CredResultJSON {
+  const {weightedGraph, credData, params, plugins} = x;
+  return toCompat(COMPAT_INFO, {
+    weightedGraph: wgToJSON(weightedGraph),
+    credData,
+    params: paramsToJSON(params),
+    plugins: pluginsToJSON(plugins),
+  });
+}
+
+export function fromJSON(j: CredResultJSON): CredResult {
+  const x = fromCompat(COMPAT_INFO, j);
+  const {weightedGraph, credData, params, plugins} = x;
+  return {
+    weightedGraph: wgFromJSON(weightedGraph),
+    credData,
+    params: paramsFromJSON(params),
+    plugins: pluginsFromJSON(plugins),
+  };
+}
