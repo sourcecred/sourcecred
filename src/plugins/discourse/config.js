64169da12801bffbb9f5d5ffa0c36e9fcf9c100a
William Chargin <wchargin@gmail.com>
2020-05-31 21:52:05 -0700

    discourse: use parser combinators for config (#1824)
    Summary:
    This patch changes the Discourse plugin’s config parsing to use the
    parser combinator approach, which simplifies it and removes the need for
    all the tests.
    
    Test Plan:
    Prior to deleting the tests, they passed, except for the test that
    mirror options could not be partially populated; the partial parsing now
    works correctly.
    
    wchargin-branch: discourse-config-combinator

--- src/plugins/discourse/config.js
+++ src/plugins/discourse/config.js
@@ -1,67 +1,33 @@
 // @flow
 
-import {type MirrorOptions} from "./mirror";
+import * as Combo from "../../util/combo";
+import {optionsShapeParser, type MirrorOptions} from "./mirror";
 
 export type DiscourseConfig = {|
   +serverUrl: string,
   +mirrorOptions?: $Shape<MirrorOptions>,
 |};
 
-type JsonObject =
-  | string
-  | number
-  | boolean
-  | null
-  | JsonObject[]
-  | {[string]: JsonObject};
+const parser: Combo.Parser<DiscourseConfig> = (() => {
+  const C = Combo;
+  return C.object(
+    {
+      serverUrl: C.fmap(C.string, (serverUrl) => {
+        const httpRE = new RegExp(/^https?:\/\//);
+        if (!httpRE.test(serverUrl)) {
+          throw new Error(
+            "expected server url to start with 'https://' or 'http://'"
+          );
+        }
+        return serverUrl;
+      }),
+    },
+    {
+      mirrorOptions: optionsShapeParser,
+    }
+  );
+})();
 
-export function parseConfig(raw: JsonObject): DiscourseConfig {
-  if (raw == null || typeof raw !== "object" || Array.isArray(raw)) {
-    throw new Error("bad config: " + JSON.stringify(raw));
-  }
-  let mirrorOptions = undefined;
-  const {serverUrl} = raw;
-  if (typeof serverUrl !== "string") {
-    throw new Error("serverUrl not string: " + JSON.stringify(serverUrl));
-  }
-  const httpRE = new RegExp(/^https?:\/\//);
-  if (!httpRE.test(serverUrl)) {
-    throw new Error(
-      "expected server url to start with 'https://' or 'http://'"
-    );
-  }
-  if (raw.mirrorOptions !== undefined) {
-    const {mirrorOptions: rawMO} = raw;
-    if (rawMO == null || typeof rawMO !== "object" || Array.isArray(rawMO)) {
-      throw new Error("bad config: " + JSON.stringify(rawMO));
-    }
-    const {recheckCategoryDefinitionsAfterMs} = rawMO;
-    let {recheckTopicsInCategories} = rawMO;
-
-    if (!Array.isArray(recheckTopicsInCategories)) {
-      throw new Error(
-        "mirrorOptions.recheckTopicsInCategories must be array, got " +
-          JSON.stringify(recheckTopicsInCategories)
-      );
-    }
-    if (!recheckTopicsInCategories.every((x) => typeof x === "number")) {
-      throw new Error(
-        "mirrorOptions.recheckTopicsInCategories must all be numbers, got " +
-          JSON.stringify(recheckTopicsInCategories)
-      );
-    }
-    recheckTopicsInCategories = recheckTopicsInCategories.map((x) => Number(x));
-    if (typeof recheckCategoryDefinitionsAfterMs !== "number") {
-      throw new Error(
-        "recheckCategoryDefinitionsAfterMs must be number, got " +
-          JSON.stringify(recheckCategoryDefinitionsAfterMs)
-      );
-    }
-    mirrorOptions = {
-      recheckCategoryDefinitionsAfterMs,
-      recheckTopicsInCategories,
-    };
-  }
-
-  return {serverUrl, mirrorOptions};
+export function parseConfig(raw: Combo.JsonObject): DiscourseConfig {
+  return parser.parseOrThrow(raw);
 }
