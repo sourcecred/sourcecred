747c0a014755a19097861be55682177e505c6c97
Kevin Siegler <kevinsiegler54@gmail.com>
2020-06-14 15:00:23 -0500

    Create new initiative type for use with Editor
    This is the initial proposal for what a new initiative json file type
    might look like.
    This is very close to how the intiative editor service stores
    intitiatives in `db.json`. No compatibility functions are submitted for
    review as of yet, but compatibility might be achieved through use of
    plugin-specific functions that derive urls from their respective node
    addresses.
    
    However, if a user-friendly interface such as the initiatives editor is
    provided, compatibility is probably not necessary, since the
    human-readability of the JSON database file can be less of a priority in
    that case.
    
    Test plan:
    Beyond passing flow and linting tests, this addition is not yet handled
    dynamically so unit tests are included

--- src/plugins/initiatives/initiativeFile.js
+++ src/plugins/initiatives/initiativeFile.js
@@ -8,6 +8,7 @@ import {type InitiativeWeight, type InitiativeId, createId} from "./initiative";
 import {type InitiativesDirectory} from "./initiativesDirectory";
 import {type EdgeSpecJson} from "./edgeSpec";
 import {initiativeNodeType} from "./declaration";
+import {type NodeEntryJson} from "./nodeEntry";
 
 export const INITIATIVE_FILE_SUBTYPE = "INITIATIVE_FILE";
 
@@ -18,6 +19,28 @@ export const INITIATIVE_FILE_SUBTYPE = "INITIATIVE_FILE";
  * make sense to use this outside of the context of an InitiativesDirectory.
  */
 export type InitiativeFile = InitiativeFileV020;
+
+/**
+ * Stored inside a initiatives.json db file with other initiatives.
+ * presently the `id` prop is used as the key inside the db file
+ */
+export type InitiativeFileV100 = {|
+  id: string, // json file name right now. probably not needed if this is stored in side a json db file
+  // Compat info is usually stored within a header object, but in the db file parsing is more
+  // straightforward if it's folded inside the primary data object
+  type: string,
+  version: string,
+  title: string,
+  timestampIso: TimestampISO,
+  weight: InitiativeWeight,
+  completed: boolean,
+  // should be user nodes: may need a a more specific type
+  champions: ?$ReadOnlyArray<NodeAddressT>,
+  dependencies: ?$ReadOnlyArray<NodeAddressT>,
+  // should be an activity node. Will be more difficult to specify this
+  references: ?$ReadOnlyArray<NodeAddressT>,
+  contributions: ?$ReadOnlyArray<NodeEntryJson>,
+|};
 
 export type InitiativeFileV020 = {|
   +title: string,
