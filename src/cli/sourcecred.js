eb734d677916b0e5891b4d2d72c07928ffe63ef4
Dandelion Mané <decentralion@dandelion.io>
2020-06-21 23:08:20 -0700

    Make cli2 report the version and provide help
    We need the `--version` flag, both for general goodness, and because our
    Docker publish pipeline depends on it. While I was at it, I
    re-integrated the `help` command as well, along with some unit testing.
    
    Test plan: `yarn test` passes; run `yarn backend` and then run the
    command both without args (to see a dummy help message), or with the
    `--help` or `help` args, or with `--version` args. Unit tests cover all
    thsi behavior.

--- src/cli/sourcecred.js
+++ src/cli/sourcecred.js
@@ -1,17 +1,25 @@
 // @flow
 
 import type {Command} from "./command";
+import {VERSION_SHORT} from "../core/version";
 
 import load from "./load";
 import graph from "./graph";
 import score from "./score";
+import help from "./help";
 
 const sourcecred: Command = async (args, std) => {
   if (args.length === 0) {
-    std.err("fatal: specify a command");
+    help([], {out: std.err, err: std.err});
     return 1;
   }
   switch (args[0]) {
+    case "--version":
+      std.out("sourcecred " + VERSION_SHORT);
+      return 0;
+    case "--help":
+    case "help":
+      return help(args.slice(1), std);
     case "load":
       return load(args.slice(1), std);
     case "graph":
@@ -20,6 +28,7 @@ const sourcecred: Command = async (args, std) => {
       return score(args.slice(1), std);
     default:
       std.err("fatal: unknown command: " + JSON.stringify(args[0]));
+      std.err("fatal: run 'sourcecred help' for commands and usage");
       return 1;
   }
 };
