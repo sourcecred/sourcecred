11610ebf55aa173e2cedab7134e08a3bf6263c20
Kevin Siegler <kevinsiegler54@gmail.com>
2020-06-26 10:41:05 -0500

    address @hammadj 's review feedback

--- src/ui/mock/fakeInitiatives.js
+++ src/ui/mock/fakeInitiatives.js
@@ -1,19 +1,25 @@
 export const fakeInitiatives = {
   initiatives: [
     {
-      id: "fd60ed0f-162a-4152-8690-b3e97e16f7d3",
-      timestampMs: "2020-06-11",
+      id: "08a2945c-3e62-4d7a-bf4c-b38b42441bdd",
+      timestampMs: 1592110800000,
       champions: [],
       dependencies: [],
       references: [],
-      contributions: [],
+      contributions: [
+        {
+          key: "28f79984-a7c6-4c54-b603-a90bf5a63e24",
+          title: "Directory Helper functions",
+          timestampMs: 1591246800000,
+        },
+      ],
       weight: {incomplete: 0, complete: 0},
       completed: true,
-      title: "Topocount Implements Rough Frontend logic",
+      title: "Topocount Creates Prototype Node Service",
     },
     {
       id: "6a974075-6094-4cc0-b6b6-0f4e376240a6",
-      timestampMs: "2020-06-12",
+      timestampMs: 1592542800000,
       champions: [],
       dependencies: [],
       references: [],
@@ -23,25 +29,8 @@ export const fakeInitiatives = {
       title: "Dandelion Creates webpack Dev Service",
     },
     {
-      id: "08a2945c-3e62-4d7a-bf4c-b38b42441bdd",
-      timestampMs: "2020-06-15",
-      champions: [],
-      dependencies: [],
-      references: [],
-      contributions: [
-        {
-          key: "28f79984-a7c6-4c54-b603-a90bf5a63e24",
-          title: "Directory Helper functions",
-          timestampMs: "2020-06-05",
-        },
-      ],
-      weight: {incomplete: 0, complete: 0},
-      completed: true,
-      title: "Topocount Creates Prototype Node Service",
-    },
-    {
       id: "95f29621-4761-42c9-815a-59e6c5217069",
-      timestampMs: "2020-06-24T19:21:32.981Z",
+      timestampMs: 1592888400000,
       champions: [],
       dependencies: [],
       references: [],
@@ -49,23 +38,34 @@ export const fakeInitiatives = {
         {
           key: "9c6766e1-5940-4f38-8e32-3bb6e15f7631",
           title: "Hammad and Topocount troubleshoot SSR issues",
-          timestampMs: "2020-06-17",
+          timestampMs: 1592283600000,
         },
         {
           key: "9c6766e1-5940-4f38-8e32-3bb6e15f7631",
           title: "Hammad implements SSR patches and logs bugs",
-          timestampMs: "2020-06-22",
+          timestampMs: 1592715600000,
         },
         {
           key: "9c6766e1-5940-4f38-8e32-3bb6e15f7631",
           title:
             "Topocount ports over the frontend Initiatives Logic to the Dev Server",
-          timestampMs: "2020-06-24",
+          timestampMs: 1592888400000,
         },
       ],
       weight: {incomplete: 0, complete: 0},
       completed: false,
-      title: "Get Frontend working on Weback Dev Service",
+      title: "Get Frontend working on Webpack Dev Service",
+    },
+    {
+      id: "fd60ed0f-162a-4152-8690-b3e97e16f7d3",
+      timestampMs: 1591851600000,
+      champions: [],
+      dependencies: [],
+      references: [],
+      contributions: [],
+      weight: {incomplete: 0, complete: 0},
+      completed: true,
+      title: "Topocount Implements Rough Frontend logic",
     },
   ],
 };
