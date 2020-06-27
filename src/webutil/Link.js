848e8cdd7db3fbadf2584638799caa96c9ec5a8b
Hammad Jutt <jutt@ualberta.ca>
2020-06-26 19:17:36 -0600

    Update Link component to react-router v5
    React router v5 has moved its Link component into a separate package: react-router-dom. This PR
    just updates the place we import the Link component for our own internal wrapper around it.
    
    TestPlan: Ensure that our Link component is still functional

--- src/webutil/Link.js
+++ src/webutil/Link.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, {Component} from "react";
-import {Link as RouterLink} from "react-router";
+import {Link as RouterLink} from "react-router-dom";
 import {StyleSheet, css} from "aphrodite/no-important";
 
 import Colors from "./Colors";
