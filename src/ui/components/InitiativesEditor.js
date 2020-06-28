40d53c6df0bb1b0f5b016a799936cf17f0bac288
Kevin Siegler <kevinsiegler54@gmail.com>
2020-06-28 12:43:32 -0500

    address @decentralion 's review
    simply the sourceCred integration immensely by utilizing explorer logic.
    Remove unnecessary filters
    
    One thing that needs to be considered in the future is the concept of an
    "activity" node. While the filter removed in this commit is brittle, it
    improved UX pretty significantly by filtering out a lot of noise from
    the autocomplete lists. It'd be interesting to define this node subclass
    for all plugins and expose all relevant nodes via CredView
    
    test plan: same as before, manual testing of the initiative creation,
         modificiation and export flows

--- src/ui/components/InitiativesEditor.js
+++ src/ui/components/InitiativesEditor.js
@@ -12,26 +12,7 @@ import {fakeInitiatives} from "../mock/fakeInitiatives";
 import {createMemoryHistory} from "history";
 import {createMuiTheme} from "@material-ui/core/styles";
 import pink from "@material-ui/core/colors/pink";
-import {Graph, type Node} from "../../core/graph";
-import {type WeightedGraphJSON} from "../../core/weightedGraph";
-import {type PluginDeclaration} from "../../analysis/pluginDeclaration";
-import {type CredResult} from "../../analysis/credResult";
-import {fromCompat, type Compatible} from "../../util/compat";
-import {loadUsers, loadActivities} from "../initiativeUtils";
-
-async function loadAndReport(
-  path: string
-): Promise<{
-  plugins: Compatible<PluginDeclaration[]>,
-  weightedGraph: WeightedGraphJSON,
-}> {
-  const response = await fetch(path);
-  if (!response.ok) {
-    console.error(path, response);
-  }
-  const res: Compatible<CredResult> = await response.json();
-  return fromCompat({type: "sourcecred/credResult", version: "0.1.0"}, res);
-}
+import {load, type Props, type State} from "./ExplorerApp";
 
 const dataProvider = fakeDataProvider(fakeInitiatives, true);
 
@@ -44,59 +25,50 @@ const theme = createMuiTheme({
   },
 });
 
-export type AppState = {|
-  graph: ?Graph,
-  plugins: ?(PluginDeclaration[]),
-  users: Node[],
-  activities: Node[],
-  loaded: boolean,
-|};
-
-export default class App extends React.Component<{||}, AppState> {
+export default class App extends React.Component<Props, State> {
   constructor(props: Object) {
     super(props);
     this.state = {
-      graph: null,
-      plugins: null,
-      users: [],
-      activities: [],
-      loaded: false,
+      loadResult: null,
     };
   }
   async componentDidMount() {
-    const {
-      weightedGraph: [, {graphJSON}],
-      plugins: [, plugins],
-    } = await loadAndReport("output/credResult.json");
-    const graph = Graph.fromJSON(graphJSON);
-    const users = loadUsers(plugins, graph);
-    const activities = loadActivities(plugins, graph);
     this.setState({
-      graph,
-      plugins,
-      users,
-      activities,
-      loaded: true,
+      loadResult: await load(),
     });
   }
 
   render() {
-    if (!this.state.loaded)
+    const {loadResult} = this.state;
+    if (!loadResult) {
       return (
         <Loading
           loadingPrimary="Fetching cred details..."
           loadingSecondary="Your patience is appreciated"
         />
       );
-    return (
-      <Admin theme={theme} dataProvider={dataProvider} history={history}>
-        <Resource
-          name="initiatives"
-          list={InitiativeList}
-          create={InitiativeCreate(this.state)}
-          edit={InitiativeEdit(this.state)}
-        />
-      </Admin>
-    );
+    }
+    switch (loadResult.type) {
+      case "FAILURE":
+        return (
+          <div>
+            <h1>Load Failure</h1>
+            <p>Check console for details.</p>
+          </div>
+        );
+      case "SUCCESS":
+        return (
+          <Admin theme={theme} dataProvider={dataProvider} history={history}>
+            <Resource
+              name="initiatives"
+              list={InitiativeList}
+              create={InitiativeCreate(loadResult.credView)}
+              edit={InitiativeEdit(loadResult.credView)}
+            />
+          </Admin>
+        );
+      default:
+        throw new Error((loadResult.type: empty));
+    }
   }
 }
