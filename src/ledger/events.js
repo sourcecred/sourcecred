a59bf4bb89e1e6c0daae077cc6ef2114c0fd604f
Beanow <497556+Beanow@users.noreply.github.com>
2020-07-06 00:00:31 +0200

    Functions to create event instances

--- src/ledger/events.js
+++ src/ledger/events.js
@@ -1,8 +1,8 @@
 // @flow
 
-import type {UserId, Username} from "./user";
-import {type NodeAddressT} from "../core/graph";
-import {type TimestampMs} from "../util/timestamp";
+import type {User, UserId, Username} from "./user";
+import type {NodeAddressT} from "../core/graph";
+import type {TimestampMs} from "../util/timestamp";
 
 /*
   Events are historical facts and outcomes of a command. Hence they are named
@@ -25,6 +25,14 @@ export type UserCreated = {|
   +name: Username,
 |};
 
+export const userCreated = (user: User): UserCreated => ({
+  type: "USER_CREATED",
+  version: 1,
+  timestamp: Date.now(),
+  userId: user.id,
+  name: user.name,
+});
+
 // Note this event body is identical to UserCreated, but it's semantics are
 // different. One demands that the user doesn't exist yet, the other that it
 // does already exist.
@@ -36,6 +44,14 @@ export type UsernameChanged = {|
   +name: Username,
 |};
 
+export const usernameChanged = (user: User): UsernameChanged => ({
+  type: "USERNAME_CHANGED",
+  version: 1,
+  timestamp: Date.now(),
+  userId: user.id,
+  name: user.name,
+});
+
 // Overrides the current set of aliases with a new set.
 // Difference can be obtained by comparing with the previous set.
 export type UserAliasesChanged = {|
@@ -43,5 +59,13 @@ export type UserAliasesChanged = {|
   +version: 1,
   +timestamp: TimestampMs,
   +userId: UserId,
-  +aliases: Set<NodeAddressT>,
+  +aliases: $ReadOnlyArray<NodeAddressT>,
 |};
+
+export const userAliasesChanged = (user: User): UserAliasesChanged => ({
+  type: "USER_ALIASES_CHANGED",
+  version: 1,
+  timestamp: Date.now(),
+  userId: user.id,
+  aliases: user.aliases,
+});
