8fa1b03ba4ec1a9af2a16e607913eb8b99f40728
Beanow <497556+Beanow@users.noreply.github.com>
2020-07-06 15:44:49 +0200

    Rewrite commands as generators

--- src/ledger/commands.js
+++ src/ledger/commands.js
@@ -1,6 +1,6 @@
 // @flow
 
-import {State} from "./state";
+import {type ProjectionQueries as Queries} from "./projection";
 import {type User, aliasesDiffer} from "./user";
 import {
   type Event,
@@ -9,38 +9,56 @@ import {
   userAliasesChanged,
 } from "./events";
 
-export function trackUser(state: State, user: User): $ReadOnlyArray<Event> {
-  const existing: ?User = state.getUser(user.id);
+/*
+  These Commands are more strict than typical projection logic is.
+  The idea is to avoid emitting invalid Events when possible.
 
-  // User don't exist, create a new one.
-  if (!existing) {
-    if (state.usernameExists(user.name)) {
-      throw new Error(
-        `Can't create new user, the username ${user.name} is already taken`
-      );
-    }
+  Uses generators because a conditional yield is much more practical
+  than building an array of events to return.
+*/
 
-    const events: Event[] = [];
-    events.push(userCreated(user));
-    // If the aliases are non-empty, create an event to set them too.
-    if (aliasesDiffer([], user.aliases)) {
-      events.push(userAliasesChanged(user));
-    }
-    return events;
+export function* trackUser(state: Queries, user: User): Iterable<Event> {
+  if (!state.getUser(user.id)) {
+    yield* createUser(state, user);
   } else {
-    const events: Event[] = [];
-    if (user.name !== existing.name) {
-      if (state.usernameExists(user.name)) {
-        throw new Error(
-          `Can't change username, the username ${user.name} is already taken`
-        );
-      }
-      events.push(usernameChanged(user));
-    }
-    if (aliasesDiffer(existing.aliases, user.aliases)) {
-      events.push(userAliasesChanged(user));
-    }
-    return events;
+    yield* updateUser(state, user);
   }
 }
 
+export function* createUser(state: Queries, user: User): Iterable<Event> {
+  if (state.getUser(user.id)) {
+    throw new Error(`User ID ${user.id} is already taken`);
+  }
+
+  if (state.usernameExists(user.name)) {
+    throw new Error(`Username ${user.name} is already taken`);
+  }
+
+  // We know the required fields aren't taken, create the user.
+  yield userCreated(user);
+
+  // If the user already has aliases, create an event for those too.
+  if (aliasesDiffer([], user.aliases)) {
+    yield userAliasesChanged(user);
+  }
+}
+
+export function* updateUser(state: Queries, user: User): Iterable<Event> {
+  const existing: ?User = state.getUser(user.id);
+  if (!existing) {
+    throw new Error(`User with ID ${user.id} doesn't exist`);
+  }
+
+  // See if the username changed.
+  if (user.name !== existing.name) {
+    if (state.usernameExists(user.name)) {
+      throw new Error(`Can't change username, "${user.name}" is already taken`);
+    }
+    yield usernameChanged(user);
+  }
+
+  // See if the aliases changed.
+  if (aliasesDiffer(existing.aliases, user.aliases)) {
+    yield userAliasesChanged(user);
+  }
+}
