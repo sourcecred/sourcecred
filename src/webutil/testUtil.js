7ce135c5354946af338d39579ee4eb6fa4d9b4ed
Kevin Siegler <kevinsiegler54@gmail.com>
2020-07-07 22:20:26 -0500

    implement V4 endpoints
    implement block handler to disable path changes
    
    implement proxy handlers for delegate state so relativeHistory behaves
    like any other history object without maintaining its own state
    
    test plan: thorough unit tests are available for the new state accesses
    and block method

--- src/webutil/testUtil.js
+++ src/webutil/testUtil.js
@@ -26,3 +26,56 @@ export function configureAphrodite() {
     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
   });
 }
+
+export const relativeEntries = [
+  {
+    hash: "",
+    pathname: "/foo/bar/",
+    search: "",
+    state: undefined,
+  },
+  {hash: "", pathname: "/1/", search: "", state: undefined},
+  {hash: "", pathname: "/2/", search: "", state: undefined},
+  {hash: "", pathname: "/3/", search: "", state: undefined},
+  {hash: "", pathname: "/4/", search: "", state: undefined},
+  {hash: "", pathname: "/5/", search: "", state: undefined},
+];
+
+export const memoryEntries = [
+  {
+    pathname: "/my/gateway/foo/bar/",
+    search: "",
+    hash: "",
+    state: undefined,
+  },
+  {
+    pathname: "/my/gateway/1/",
+    search: "",
+    hash: "",
+    state: undefined,
+  },
+  {
+    pathname: "/my/gateway/2/",
+    search: "",
+    hash: "",
+    state: undefined,
+  },
+  {
+    pathname: "/my/gateway/3/",
+    search: "",
+    hash: "",
+    state: undefined,
+  },
+  {
+    pathname: "/my/gateway/4/",
+    search: "",
+    hash: "",
+    state: undefined,
+  },
+  {
+    pathname: "/my/gateway/5/",
+    search: "",
+    hash: "",
+    state: undefined,
+  },
+];
