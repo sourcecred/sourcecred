997c9e5f78ad3c04915a7cd826c7cbda552faf01
Hammad Jutt <jutt@ualberta.ca>
2020-07-11 19:15:06 -0600

    Extract InitiativeForm into a common re-used component for Edit/Create
    The implementation of the InitiativeForm was almost identical in InitiativeCreate and InitiativeEdit,
    so I extracted the common parts into a new InitiativeForm component to keep things DRY.
    
    Also made some performance optimizations by wrapping the credView nodes filtering in useMemo so its
    not recomputed on every render.
    
    TestPlan: Create new initiatives and edit existing ones to ensure they still function as desired and that
    editing pre-populates all the existing fields correctly

--- src/ui/components/Initiatives/InitiativeCreate.js
+++ src/ui/components/Initiatives/InitiativeCreate.js
@@ -1,27 +1,14 @@
 // @flow
 import React from "react";
-import {v4 as uuid} from "uuid";
-import {
-  AutocompleteArrayInput,
-  DateInput,
-  ArrayInput,
-  NumberInput,
-  SimpleForm,
-  TextInput,
-  Create,
-  SimpleFormIterator,
-  BooleanInput,
-} from "react-admin";
-import {
-  getPlainDescFromMd,
-  dateFormatter,
-  dateParser,
-} from "../../initiativeUtils";
-import {CredView} from "../../../analysis/credView";
+import { v4 as uuid } from "uuid";
+import { Create } from "react-admin";
+import { CredView } from "../../../analysis/credView";
+import { InitiativeForm } from "./InitiativeForm";
 
 export const InitiativeCreate = (credView: CredView) => (props: Object) => {
-  const defaultInitiativeValues = {
+  const defaultInitiativeValues = React.useMemo(() => ({
     id: uuid(),
+    title: '',
     timestampMs: Date.now(),
     champions: [],
     dependencies: [],
@@ -29,90 +16,14 @@ export const InitiativeCreate = (credView: CredView) => (props: Object) => {
     contributions: [],
     weight: {incomplete: 0, complete: 0},
     completed: false,
-  };
+  }), []);
+  
   return (
     <Create title="Create New Initiative" {...props}>
-      <SimpleForm initialValues={defaultInitiativeValues}>
-        <TextInput
-          source="id"
-          label="Initiative ID"
-          disabled
-          type="hidden"
-          style={{display: "none"}}
-        />
-        <TextInput label="Title" source="title" />
-        <DateInput
-          format={dateFormatter}
-          parse={dateParser}
-          label="Date"
-          source="timestampMs"
-        />
-        <NumberInput
-          label="Weight While Incomplete"
-          source="weight.incomplete"
-        />
-        <NumberInput label="Weight When Completed" source="weight.complete" />
-        <BooleanInput label="Completed" source="completed" />
-        <AutocompleteArrayInput
-          source="champions"
-          allowDuplicates={false}
-          translateChoice={false}
-          choices={credView.userNodes()}
-          optionValue="address"
-          optionText={getPlainDescFromMd}
-          label="Champions"
-          suggestionLimit={10}
-        />
-        <AutocompleteArrayInput
-          source="dependencies"
-          allowDuplicates={false}
-          translateChoice={false}
-          choices={credView.nodes()}
-          optionValue="address"
-          optionText={getPlainDescFromMd}
-          label="Dependencies"
-          suggestionLimit={10}
-        />
-        <AutocompleteArrayInput
-          source="references"
-          allowDuplicates={false}
-          translateChoice={false}
-          choices={credView.nodes()}
-          optionValue="address"
-          optionText={getPlainDescFromMd}
-          label="References"
-          suggestionLimit={10}
-        />
-        <ArrayInput label="Contributions" source="contributions">
-          <SimpleFormIterator>
-            <TextInput
-              source="key"
-              label="Contribution Key"
-              disabled
-              initialValue={uuid()}
-              style={{display: "none"}}
-            />
-            <TextInput label="Contribution Name" source="title" />
-            <DateInput
-              format={dateFormatter}
-              parse={dateParser}
-              label="Date"
-              source="timestampMs"
-            />
-            <NumberInput label="Weight" source="weight" />
-            <AutocompleteArrayInput
-              source="contributors"
-              allowDuplicates={false}
-              translateChoice={false}
-              choices={credView.userNodes()}
-              optionValue="address"
-              optionText={getPlainDescFromMd}
-              label="Contributors"
-              suggestionLimit={10}
-            />
-          </SimpleFormIterator>
-        </ArrayInput>
-      </SimpleForm>
+      <InitiativeForm
+        initialValues={defaultInitiativeValues}
+        credView={credView}
+      />
     </Create>
   );
 };
