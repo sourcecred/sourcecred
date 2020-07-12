ee1753fd6a55785dd72e862fc231e744a3ed1875
Hammad Jutt <jutt@ualberta.ca>
2020-07-11 19:10:13 -0600

    Refactor InitiativesEditor to use React Hooks
    Replaces the class component with a functional implementation using Hooks, which is the recommended way
    of writing React components now.
    
    TestPlan: Ensure the Admin UI renders and loads the cred data

--- src/ui/components/InitiativesEditor.js
+++ src/ui/components/InitiativesEditor.js
@@ -2,17 +2,13 @@
 
 import React from "react";
 import {Admin, Resource, Loading} from "react-admin";
-import {
-  InitiativeList,
-  InitiativeCreate,
-  InitiativeEdit,
-} from "../components/Initiatives";
+import {InitiativeList, InitiativeCreate, InitiativeEdit} from "./Initiatives";
 import fakeDataProvider from "ra-data-fakerest";
 import {fakeInitiatives} from "../mock/fakeInitiatives";
 import {createMemoryHistory} from "history";
 import {createMuiTheme} from "@material-ui/core/styles";
 import pink from "@material-ui/core/colors/pink";
-import {load, type Props, type State} from "./ExplorerApp";
+import {load, type LoadResult} from "./ExplorerApp";
 
 const dataProvider = fakeDataProvider(fakeInitiatives, true);
 
@@ -25,50 +21,42 @@ const theme = createMuiTheme({
   },
 });
 
-export default class App extends React.Component<Props, State> {
-  constructor(props: Object) {
-    super(props);
-    this.state = {
-      loadResult: null,
-    };
-  }
-  async componentDidMount() {
-    this.setState({
-      loadResult: await load(),
-    });
-  }
+const InitiativesEditor = () => {
+  const [loadResult, setLoadResult] = React.useState<LoadResult | null>(null);
+  React.useEffect(() => {
+    load().then(setLoadResult);
+  }, []);
 
-  render() {
-    const {loadResult} = this.state;
-    if (!loadResult) {
-      return (
-        <Loading
-          loadingPrimary="Fetching cred details..."
-          loadingSecondary="Your patience is appreciated"
-        />
-      );
-    }
-    switch (loadResult.type) {
-      case "FAILURE":
-        return (
-          <div>
-            <h1>Load Failure</h1>
-            <p>Check console for details.</p>
-          </div>
-        );
-      case "SUCCESS":
-        return (
-          <Admin theme={theme} dataProvider={dataProvider} history={history}>
-            <Resource
-              name="initiatives"
-              list={InitiativeList}
-              create={InitiativeCreate(loadResult.credView)}
-              edit={InitiativeEdit(loadResult.credView)}
-            />
-          </Admin>
-        );
-      default:
-        throw new Error((loadResult.type: empty));
-    }
+  if (!loadResult) {
+    return (
+      <Loading
+        loadingPrimary="Fetching cred details..."
+        loadingSecondary="Your patience is appreciated"
+      />
+    );
   }
-}
+  switch (loadResult.type) {
+    case "FAILURE":
+      return (
+        <div>
+          <h1>Load Failure</h1>
+          <p>Check console for details.</p>
+        </div>
+      );
+    case "SUCCESS":
+      return (
+        <Admin theme={theme} dataProvider={dataProvider} history={history}>
+          <Resource
+            name="initiatives"
+            list={InitiativeList}
+            create={InitiativeCreate(loadResult.credView)}
+            edit={InitiativeEdit(loadResult.credView)}
+          />
+        </Admin>
+      );
+    default:
+      throw new Error((loadResult.type: empty));
+  }
+};
+
+export default InitiativesEditor;
