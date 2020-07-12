1054ec41452638c0c5a1d3a55caade53c60770c8
Hammad Jutt <jutt@ualberta.ca>
2020-07-11 19:27:20 -0600

    Reformat with prettier
    Test Plan: Ensure check-pretty passes

--- src/ui/components/Initiatives/InitiativeCreate.js
+++ src/ui/components/Initiatives/InitiativeCreate.js
@@ -1,23 +1,26 @@
 // @flow
 import React from "react";
-import { v4 as uuid } from "uuid";
-import { Create } from "react-admin";
-import { CredView } from "../../../analysis/credView";
-import { InitiativeForm } from "./InitiativeForm";
+import {v4 as uuid} from "uuid";
+import {Create} from "react-admin";
+import {CredView} from "../../../analysis/credView";
+import {InitiativeForm} from "./InitiativeForm";
 
 export const InitiativeCreate = (credView: CredView) => (props: Object) => {
-  const defaultInitiativeValues = React.useMemo(() => ({
-    id: uuid(),
-    title: '',
-    timestampMs: Date.now(),
-    champions: [],
-    dependencies: [],
-    references: [],
-    contributions: [],
-    weight: {incomplete: 0, complete: 0},
-    completed: false,
-  }), []);
-  
+  const defaultInitiativeValues = React.useMemo(
+    () => ({
+      id: uuid(),
+      title: "",
+      timestampMs: Date.now(),
+      champions: [],
+      dependencies: [],
+      references: [],
+      contributions: [],
+      weight: {incomplete: 0, complete: 0},
+      completed: false,
+    }),
+    []
+  );
+
   return (
     <Create title="Create New Initiative" {...props}>
       <InitiativeForm
