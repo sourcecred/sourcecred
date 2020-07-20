3c1548deb7a0c66997b1234b2da182be01e9e6ef
Kevin Siegler <kevinsiegler54@gmail.com>
2020-07-20 00:50:23 -0500

    load bundledPlugins into frontend
    bundled plugins are needed to see what plugins should be activated in
    the frontend, namely initiatives
    
    test plan: ensure this load function doesn't fail or error on render

--- src/ui/components/ExplorerApp.js
+++ src/ui/components/ExplorerApp.js
@@ -4,25 +4,32 @@ import React from "react";
 import {fromJSON as credResultFromJSON} from "../../analysis/credResult";
 import {CredView} from "../../analysis/credView";
 import {Explorer} from "./Explorer.js";
+import * as pluginId from "../../api/pluginId";
 
 export type LoadResult = LoadSuccess | LoadFailure;
 export type LoadSuccess = {|
   +type: "SUCCESS",
   +credView: CredView,
+  +bundledPlugins: $ReadOnlyArray<pluginId.PluginId>,
 |};
 export type LoadFailure = {|+type: "FAILURE", +error: any|};
 
 export async function load(): Promise<LoadResult> {
-  const response = await fetch("output/credResult.json");
-  if (!response.ok) {
-    console.error(response);
-    return {type: "FAILURE", error: response.status};
+  const queries = [fetch("output/credResult.json"), fetch("/sourcecred.json")];
+  const responses = await Promise.all(queries);
+
+  for (const response of responses) {
+    if (!response.ok) {
+      console.error(response);
+      return {type: "FAILURE", error: response.status};
+    }
   }
   try {
-    const json = await response.json();
+    const json = await responses[0].json();
     const credResult = credResultFromJSON(json);
     const credView = new CredView(credResult);
-    return {type: "SUCCESS", credView};
+    const {bundledPlugins} = await responses[1].json();
+    return {type: "SUCCESS", credView, bundledPlugins};
   } catch (e) {
     console.error(e);
     return {type: "FAILURE", error: e};
