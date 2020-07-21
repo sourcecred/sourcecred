09a3fca7260430a9b5f04eabc018b448c3da6d59
Kevin Siegler <kevinsiegler54@gmail.com>
2020-07-21 13:29:06 -0500

    implement ledger identity editor POC
    The ledger identity editor enables users to collect grain by allowing
    them to claim aliases from various plugins and unite them under a
    single identity.
    
    This editor allows for the creation and modification of identity
    usernames, along with the assignment of aliases.
    
    Note: aliases cannot yet be unassigned as there is no graceful way to
    accomplish this in the backend yet.
    
    test plan:
     - run the dev web server and browse to the ledger admin view.
     - Try to create an identity, then assign a couple aliases to it.
     - Click on the newly created identity name in the alias list to
     reset the view and create a new identity
     - Create a second identity and assign a couple more aliases to it.
     - Note that assigned aliases are no longer visible in the dropdown
     - click between users and ensure the name and alias lists repopulate
     with the correct information

---
+++ src/ui/components/LedgerAdmin.js
@@ -0,0 +1,106 @@
+// @flow
+
+import React, {useState} from "react";
+import {type Identity} from "../../ledger/identity";
+import {Ledger} from "../../ledger/ledger";
+import {CredView} from "../../analysis/credView";
+import {AliasSelector} from "./AliasSelector";
+
+export type Props = {|
+  +credView: CredView,
+|};
+
+export const LedgerAdmin = ({credView}: Props) => {
+  const [ledger, setLedger] = useState<Ledger>(new Ledger());
+  const [nextIdentityName, setIdentityName] = useState<string>("");
+  const [currentIdentity, setCurrentIdentity] = useState<Identity | null>(null);
+  const [promptString, setPromptString] = useState<string>("Add Identity:");
+
+  // TODO: fix any
+  function changeIdentityName(event: any) {
+    setIdentityName(event.target.value);
+  }
+
+  function createOrUpdateIdentity(event: any) {
+    event.preventDefault();
+    if (!currentIdentity) {
+      const newID = ledger.createIdentity("USER", nextIdentityName);
+      setLedger(ledger);
+      setIdentityName("");
+      setActiveIdentity(ledger.account(newID).identity);
+    } else {
+      const {id} = currentIdentity;
+      ledger.renameIdentity(id, nextIdentityName);
+      setLedger(ledger);
+      setIdentityName("");
+      setCurrentIdentity(null);
+    }
+  }
+
+  function setActiveIdentity(identity: Identity) {
+    const {name} = identity;
+    if (currentIdentity && name === currentIdentity.name) {
+      setIdentityName("");
+      setCurrentIdentity(null);
+      setPromptString("Add Identity: ");
+    } else {
+      setIdentityName(name);
+      setCurrentIdentity(identity);
+      setPromptString("Update Identity: ");
+    }
+  }
+  return (
+    <div
+      style={{
+        width: "80%",
+        margin: "0 auto",
+        background: "white",
+        padding: "0 5em 5em",
+      }}
+    >
+      <h1>Identities:</h1>
+      <ul>{renderIdentities()}</ul>
+      <h1>{promptString}</h1>
+      <form onSubmit={(e) => createOrUpdateIdentity(e)}>
+        <p>
+          <label htmlFor="Name">Name</label> <br />
+          <input
+            type="text"
+            name="Name"
+            value={nextIdentityName}
+            onChange={(e) => changeIdentityName(e)}
+          />
+        </p>
+        <input type="submit" value="Submit" />
+      </form>
+      <div>
+        {/* Warning: don't conditionally render this because it contains react hooks*/}
+        {AliasSelector({
+          currentIdentity,
+          ledger,
+          setLedger,
+          setCurrentIdentity,
+          credView,
+        })}
+      </div>
+    </div>
+  );
+
+  function renderIdentities() {
+    function renderIdentity(i: Identity) {
+      return (
+        <li onClick={() => setActiveIdentity(i)} key={i.id}>
+          {i.name}
+        </li>
+      );
+    }
+    return (
+      <div>
+        {ledger
+          .accounts()
+          .map((a) => a.identity)
+          .map(renderIdentity)}
+      </div>
+    );
+  }
+};
