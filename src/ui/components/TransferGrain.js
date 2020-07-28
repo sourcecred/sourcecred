d2db6edbf3c0c562449e197da02b43a8c8d85830
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2020-07-28 15:05:46 -0500

    implement grain transfer interface (#2035)
    * implement grain transfer interface
    
    this interface allows for grain transfers between user identities. There
    are validations in place to ensure an amount exceeding the senders
    balance cannot be submitted.
    
    Currently there is no completely lossless library function for
    converting a stringified float to grain. This is needed to maintain
    fidelity between the frontend and the backend and is currently
    WIP (#2034)
    
    test-plan: send grain between accounts. try to send a grain amount
    exceeding the sender's balance. Ensure the entered value is stored
    correctly in the ledger.json file.
    
    * implement better input parsing
    
    - utilize `fromFloatString` to get exact float values from inputs
    - set `step="any"` on the input field since FireFox defaults to 1 and
        throws a validation error when a number input is not whole
    - make the basic form validation a bit more strict so 0-value transfers
        don't pollute the ledger and otherwise invalid forms cannot be submitted
        to the grain backend before erroring
    
    test-plan: ensure non-whole grain values can be subitted in fire fox.
    ensure form validation doesn't allow any invalid submissions.
    
    * Remove stray comment
    
    Co-authored-by: Dandelion Mané <decentralion@dandelion.io>

---
+++ src/ui/components/TransferGrain.js
@@ -0,0 +1,119 @@
+// @flow
+
+import React, {useState} from "react";
+import {
+  div,
+  format,
+  gt,
+  fromInteger,
+  fromFloatString,
+  type Grain,
+} from "../../ledger/grain";
+import {type Identity} from "../../ledger/identity";
+import {Ledger, type Account} from "../../ledger/ledger";
+import AccountDropdown from "./AccountSelector";
+
+export type Props = {|
+  +initialLedger: Ledger,
+|};
+
+export const TransferGrain = ({initialLedger}: Props) => {
+  const [ledger, setLedger] = useState<Ledger>(initialLedger);
+  const [sourceIdentity, setSourceIdentity] = useState<Identity | null>(null);
+  const [destIdentity, setDestIdentity] = useState<Identity | null>(null);
+  const [amount, setAmount] = useState<string>("0");
+  const [maxAmount, setMaxAmount] = useState<Grain>(fromInteger(0));
+  const [memo, setMemo] = useState<string>("");
+
+  const setSender = (acct: Account) => {
+    setMaxAmount(acct.balance);
+    setSourceIdentity(acct.identity);
+  };
+
+  const setReceiver = (acct: Account) => {
+    setDestIdentity(acct.identity);
+  };
+
+  const submitTransfer = (e) => {
+    e.preventDefault();
+    if (sourceIdentity && destIdentity) {
+      const nextLedger = ledger.transferGrain({
+        from: sourceIdentity.id,
+        to: destIdentity.id,
+        amount: fromFloatString(amount),
+        memo: memo,
+      });
+      setLedger(nextLedger);
+      setAmount(fromInteger(0));
+      setSender(nextLedger.account(sourceIdentity.id));
+      setMemo("");
+    }
+  };
+
+  return (
+    <div
+      style={{
+        width: "80%",
+        margin: "0 auto",
+        background: "white",
+        padding: "0 5em 5em",
+      }}
+    >
+      <h1>Transfer Grain</h1>
+      <form onSubmit={(e) => submitTransfer(e)}>
+        <label>From</label>
+        <AccountDropdown ledger={ledger} setCurrentIdentity={setSender} />
+        <br />
+        <label>To</label>
+        <AccountDropdown ledger={ledger} setCurrentIdentity={setReceiver} />
+        <p>
+          <label htmlFor="amount">Amount</label> <br />
+          <input
+            type="number"
+            name="amount"
+            min="0"
+            step="any"
+            placeholder={`max: ${maxAmount}`}
+            required
+            value={amount}
+            onChange={(e) => setAmount(e.currentTarget.value)}
+          />
+          <span>{sourceIdentity && ` max: ${format(maxAmount, 5)}`}</span>
+        </p>
+        <p>
+          <label htmlFor="memo">Memo</label> <br />
+          <input
+            type="text"
+            name="memo"
+            value={memo}
+            onChange={(e) => setMemo(e.currentTarget.value)}
+          />
+        </p>
+        <input
+          disabled={
+            !Number(amount) ||
+            !(sourceIdentity && destIdentity) ||
+            gt(fromFloatString(amount), maxAmount)
+          }
+          type="submit"
+          value="transfer grain"
+        />
+        <br />
+        <input
+          type="button"
+          value="save ledger to disk"
+          onClick={() => {
+            fetch("data/ledger.json", {
+              headers: {
+                Accept: "text/plain",
+                "Content-Type": "text/plain",
+              },
+              method: "POST",
+              body: ledger.serialize(),
+            });
+          }}
+        />
+      </form>
+    </div>
+  );
+};
