1b149268787110e977fdeee06df129d53747f0ad
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2020-07-30 14:09:21 -0500

    bugfix: remove resource menu link generator
    Currently we are not using react admin resources for any interfaces, so
    this logic is unnecessary. Originally, it'd render a react admin
    initiatives interface but since it hasn't been merged, this logic only
    renders the dummy resource we don't actually want to show
    
    test plan: any instances with the `sourcecred/initiatives` plugin
    enabled should _not_ show the dummy resource in the left menu.

--- src/ui/components/Menu.js
+++ src/ui/components/Menu.js
@@ -1,8 +1,7 @@
 // @flow
 import React from "react";
-import {createElement} from "react";
 import {useSelector} from "react-redux";
-import {MenuItemLink, getResources} from "react-admin";
+import {MenuItemLink} from "react-admin";
 import {type LoadSuccess} from "../load";
 import TrendingUpIcon from "@material-ui/icons/TrendingUp";
 import DefaultIcon from "@material-ui/icons/ViewList";
@@ -12,7 +11,6 @@ type menuProps = {|onMenuClick: Function|};
 
 const Menu = ({bundledPlugins}: LoadSuccess) => ({onMenuClick}: menuProps) => {
   const open = useSelector((state) => state.admin.ui.sidebarOpen);
-  const resources = useSelector(getResources);
   return (
     <>
       <MenuItemLink
@@ -22,24 +20,6 @@ const Menu = ({bundledPlugins}: LoadSuccess) => ({onMenuClick}: menuProps) => {
         onClick={onMenuClick}
         sidebarIsOpen={open}
       />
-
-      {bundledPlugins.includes("sourcecred/initiatives") &&
-        resources.map((resource) => {
-          return (
-            <MenuItemLink
-              key={resource.name}
-              to={`/${resource.name}`}
-              primaryText={
-                (resource.options && resource.options.label) || resource.name
-              }
-              leftIcon={
-                resource.icon ? createElement(resource.icon) : <DefaultIcon />
-              }
-              onClick={onMenuClick}
-              sidebarIsOpen={open}
-            />
-          );
-        })}
       <MenuItemLink
         to="/grain"
         primaryText="Grain Accounts"
