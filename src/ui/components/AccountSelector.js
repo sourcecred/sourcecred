597294d064ef8240e06a722e82e19bef23258ac7
Elijah Baldwin <befitsandpiper@gmail.com>
2020-08-10 13:07:22 -0400

    Remove Autocomplete balance display
    cleanup, rely on MUI theme

--- src/ui/components/AccountSelector.js
+++ src/ui/components/AccountSelector.js
@@ -1,72 +1,46 @@
 // @flow
-
-import React, {useState} from "react";
-import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
 import {TextField} from "@material-ui/core";
-import {format, fromInteger} from "../../ledger/grain";
 import {Autocomplete} from "@material-ui/lab";
 import {Ledger, type Account} from "../../ledger/ledger";
 
 type DropdownProps = {|
   +ledger: Ledger,
-  +setCurrentIdentity: (Account | null) => void,
+  +setCurrentAccount: (Account | null) => void,
   +placeholder?: string,
 |};
 
-const theme = createMuiTheme({
-  overrides: {
-    text: {
-      color: "black",
-    },
-  },
-});
+const useStyles = makeStyles({combobox: {margin: "0px 16px 16px"}});
 
 export default function AccountDropdown({
   placeholder,
-  setCurrentIdentity,
+  setCurrentAccount,
   ledger,
 }: DropdownProps) {
+  const classes = useStyles();
   const items = ledger.accounts().filter((a) => a.active);
-  const [selectedAccount, setSelectedAccount] = useState<Account | null>(null);
-
-  const onComboChange = (e, inputObj) => {
-    setSelectedAccount(inputObj);
-    setCurrentIdentity(inputObj);
-  };
-
-  const renderInput = (params) => {
-    const name = selectedAccount ? selectedAccount.identity.name : "";
-    const balStr = selectedAccount
-      ? ` (${format(selectedAccount.balance, 2)})`
-      : "";
-    return (
-      <TextField
-        fullWidth
-        {...params}
-        inputProps={{...params.inputProps, value: `${name}${balStr}`}}
-        label={placeholder}
-      />
-    );
-  };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Autocomplete
-        onChange={onComboChange}
-        fullWidth
-        options={items}
-        getOptionLabel={(item: Account) => {
-          const balStr = format(
-            selectedAccount && selectedAccount.balance !== fromInteger(0)
-              ? selectedAccount.balance
-              : item.balance,
-            2
-          );
-          return `${item.identity.name} (${balStr})`;
-        }}
-        style={{margin: "20px", marginTop: "-16px"}}
-        renderInput={renderInput}
-      />
-    </ThemeProvider>
+    <Autocomplete
+      size="medium"
+      className={classes.combobox}
+      onChange={(...args) => {
+        const [, inputObj] = args;
+        setCurrentAccount(inputObj);
+      }}
+      fullWidth
+      options={items}
+      getOptionLabel={(item: Account) => item.identity.name}
+      renderInput={(params) => (
+        <TextField
+          size="large"
+          fullWidth
+          {...params}
+          InputProps={{...params.InputProps, disableUnderline: true}}
+          label={placeholder}
+        />
+      )}
+    />
   );
 }
--- src/ui/components/TransferGrain.js
+++ src/ui/components/TransferGrain.js
@@ -1,15 +1,8 @@
 // @flow
-
 import React, {useState} from "react";
-import {Button, Input} from "@material-ui/core";
+import {Button, Container, TextField} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
-import {
-  div,
-  format,
-  gt,
-  fromInteger,
-  fromFloatString,
-} from "../../ledger/grain";
+import {div, format, gt, lt, fromFloatString} from "../../ledger/grain";
 import {Ledger, type Account} from "../../ledger/ledger";
 import AccountDropdown from "./AccountSelector";
 
@@ -18,192 +11,153 @@ export type Props = {|
   +setLedger: (Ledger) => void,
 |};
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: "1100px",
+    width: "80%",
+    minWidth: "1100px",
     margin: "0 auto",
-    background: "white",
     padding: "0 5em 5em",
   },
   arrowBody: {
-    width: "300px",
-    marginLeft: "20px",
-    background: "#C9CED2",
+    color: theme.palette.text.primary,
+    flex: 1,
+    background: theme.palette.background.paper,
     padding: "5px 20px",
+    display: "flex",
+    alignItems: "center",
   },
   triangle: {
     width: 0,
     height: 0,
-    background: "white",
+    background: theme.palette.background,
     borderTop: "30px solid transparent",
     borderBottom: "30px solid transparent",
-    borderLeft: "30px solid #C9CED2",
+    borderLeft: `30px solid ${theme.palette.background.paper}`,
   },
-  dropdown: {
-    width: "300px",
-    height: "50px",
+  dropdownWrapper: {
     border: "1px solid #9EADBA",
-    borderRadius: "500px",
-    background: "#F2F5F7",
+    borderRadius: "50px",
   },
   centerRow: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
   },
-  third: {
-    width: "300px",
-    height: "50px",
-    margin: "30px 15px",
-  },
-  half: {
-    width: "450px",
-    height: "50px",
-    margin: "30px",
-  },
-  memo: {
-    width: "960px",
-    height: "50px",
-    border: "1px solid #9EADBA",
-    paddingLeft: "20px",
-    color: "black",
-  },
-});
+  element: {flex: 1, margin: "20px"},
+  arrowInput: {width: "40%", display: "inline-block"},
+  pageHeader: {color: theme.palette.text.primary},
+}));
 
 export const TransferGrain = ({ledger, setLedger}: Props) => {
   const classes = useStyles();
-  const [sourceAccount, setSourceAccount] = useState<Account | null>(null);
-  const [destAccount, setDestAccount] = useState<Account | null>(null);
-  const [amount, setAmount] = useState<string>("0");
+  const [sender, setSender] = useState<Account | null>(null);
+  const [receiver, setReceiver] = useState<Account | null>(null);
+  const [amount, setAmount] = useState<string>("");
   const [memo, setMemo] = useState<string>("");
 
-  const setSender = (acct: Account | null) => {
-    setSourceAccount(acct);
-  };
-
-  const setReceiver = (acct: Account | null) => {
-    setDestAccount(acct);
-  };
-
-  const submitTransfer = (e) => {
-    e.preventDefault();
-    if (
-      sourceAccount &&
-      sourceAccount.identity &&
-      destAccount &&
-      destAccount.identity
-    ) {
+  const submitTransfer = () => {
+    if (sender && receiver) {
       const nextLedger = ledger.transferGrain({
-        from: sourceAccount.identity.id,
-        to: destAccount.identity.id,
+        from: sender.identity.id,
+        to: receiver.identity.id,
         amount: fromFloatString(amount),
         memo: memo,
       });
       setLedger(nextLedger);
-      setAmount(fromInteger(0));
-      setSender(nextLedger.account(sourceAccount.identity.id));
-      setReceiver(nextLedger.account(destAccount.identity.id));
+      setAmount("");
+      setSender(nextLedger.account(sender.identity.id));
+      setReceiver(nextLedger.account(receiver.identity.id));
       setMemo("");
     }
   };
 
+  const postLedger = () =>
+    fetch("data/ledger.json", {
+      headers: {
+        Accept: "text/plain",
+        "Content-Type": "text/plain",
+      },
+      method: "POST",
+      body: ledger.serialize(),
+    });
+
   return (
-    <form onSubmit={(e) => submitTransfer(e)}>
-      <div className={classes.root}>
-        <div className={classes.centerRow}>
-          <h1>Transfer Grain</h1>
-        </div>
-        <div className={classes.centerRow}>
-          <div
-            className={`${classes.dropdown} ${classes.centerRow} ${classes.third}`}
-          >
-            <AccountDropdown
-              ledger={ledger}
-              setCurrentIdentity={setSender}
-              placeholder="From..."
-            />
-          </div>
-          <div className={`${classes.centerRow} ${classes.third}`}>
-            <div className={classes.arrowBody}>
-              <div style={{width: "100px", display: "inline-block"}}>
-                <Input
-                  style={{color: "black", "-webkit-appearance": "none"}}
-                  disableUnderline
-                  type="number"
-                  name="amount"
-                  min="0"
-                  placeholder="(amount)"
-                  value={amount !== "0" ? amount : ""}
-                  onChange={(e) => setAmount(e.currentTarget.value)}
-                />
-              </div>
-              <span>
-                {sourceAccount &&
-                  sourceAccount.identity &&
-                  ` max: ${format(sourceAccount.balance, 2)}`}
-              </span>
-            </div>
-            <div className={classes.triangle} />
-          </div>
-          <div
-            className={`${classes.dropdown} ${classes.centerRow} ${classes.third}`}
-          >
-            <AccountDropdown
-              ledger={ledger}
-              setCurrentIdentity={setReceiver}
-              placeholder="To..."
-            />
-          </div>
-        </div>
-        <div className={classes.centerRow}>
-          <Input
-            className={classes.memo}
-            disableUnderline
-            type="text"
-            name="memo"
-            value={memo}
-            placeholder="Memo"
-            onChange={(e) => setMemo(e.currentTarget.value)}
+    <Container className={classes.root}>
+      <h1 className={`${classes.centerRow} ${classes.pageHeader}`}>
+        Transfer Grain
+      </h1>
+      <div className={classes.centerRow}>
+        <div
+          className={`${classes.dropdownWrapper} ${classes.centerRow} ${classes.element}`}
+        >
+          <AccountDropdown
+            ledger={ledger}
+            setCurrentAccount={setSender}
+            placeholder="From..."
           />
         </div>
-        <div className={classes.centerRow}>
-          <Button
-            className={`${classes.centerRow} ${classes.half}`}
-            style={{background: "#6558F5"}}
-            variant="contained"
-            type="submit"
-            disabled={
-              !Number(amount) ||
-              !(
-                sourceAccount &&
-                sourceAccount.identity &&
-                destAccount &&
-                destAccount.identity
-              ) ||
-              gt(fromFloatString(amount), sourceAccount.balance)
-            }
-          >
-            transfer grain
-          </Button>
-          <Button
-            className={`${classes.centerRow} ${classes.half}`}
-            style={{background: "#C9CED2"}}
-            variant="contained"
-            onClick={() => {
-              fetch("data/ledger.json", {
-                headers: {
-                  Accept: "text/plain",
-                  "Content-Type": "text/plain",
-                },
-                method: "POST",
-                body: ledger.serialize(),
-              });
-            }}
-          >
-            save ledger to disk
-          </Button>
+        <div className={`${classes.centerRow} ${classes.element}`}>
+          <div className={classes.arrowBody}>
+            <TextField
+              className={classes.arrowInput}
+              size="medium"
+              InputProps={{disableUnderline: true}}
+              type="number"
+              placeholder="(amount)"
+              value={amount}
+              onChange={(e) => setAmount(e.currentTarget.value)}
+            />
+            <span>{sender && ` max: ${format(sender.balance, 2)}`}</span>
+          </div>
+          <div className={classes.triangle} />
+        </div>
+        <div
+          className={`${classes.dropdownWrapper} ${classes.centerRow} ${classes.element}`}
+        >
+          <AccountDropdown
+            ledger={ledger}
+            setCurrentAccount={setReceiver}
+            placeholder="To..."
+          />
         </div>
       </div>
-    </form>
+      <div className={classes.centerRow}>
+        <TextField
+          variant="outlined"
+          size="medium"
+          className={classes.element}
+          type="text"
+          value={memo}
+          placeholder="Memo"
+          onChange={(e) => setMemo(e.currentTarget.value)}
+        />
+      </div>
+      <div className={classes.centerRow}>
+        <Button
+          size="large"
+          color="primary"
+          variant="contained"
+          className={classes.element}
+          disabled={
+            !Number(amount) ||
+            !(sender && receiver) ||
+            gt(fromFloatString(amount), sender.balance) ||
+            lt(fromFloatString(amount), fromFloatString("0"))
+          }
+          onClick={submitTransfer}
+        >
+          transfer grain
+        </Button>
+        <Button
+          size="large"
+          color="primary"
+          variant="contained"
+          className={classes.element}
+          onClick={postLedger}
+        >
+          save ledger to disk
+        </Button>
+      </div>
+    </Container>
   );
 };
