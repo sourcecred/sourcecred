05822af1ea685a70c6af4ca6c8dad3860d4a5465
Dandelion Mané <decentralion@dandelion.io>
2020-08-12 13:51:55 -0600

    Simplify the identity plugin declaration
    This reduces the number of node types in the identity plugin declaration
    from 4 to 1. We no longer distinguish at the node type level between
    different identity subtypes--this is a requirement, because identity
    addresses need to be permanent, but the identity type is not permanent, so we
    simply have to stop encoding the type in the address.
    
    Also, this whole declaration is going to disappear soon. See #2109 for
    details.
    
    While I was here, I updated the test-instance ledger so that it has some
    valid aliases, so it was easier to test whether the identities were
    still being handled properly by the frontend.
    
    Test plan: `yarn test --full` (since it changes snapshots), also manual
    inspection of the test instance frontend to make sure that identities
    still appear in the UI.

--- src/ledger/identity/declaration.js
+++ src/ledger/identity/declaration.js
@@ -4,7 +4,7 @@ import {type PluginDeclaration} from "../../analysis/pluginDeclaration";
 import {type NodeType} from "../../analysis/types";
 import {NodeAddress, EdgeAddress} from "../../core/graph";
 
-// It's not in the typical [owner, login] format because it isn't provided by a plugin.
+// It's not in the typical [owner, name] format because it isn't provided by a plugin.
 // Instead, it's a raw type owned by SourceCred project.
 export const IDENTITY_PREFIX = NodeAddress.fromParts([
   "sourcecred",
@@ -12,40 +12,14 @@ export const IDENTITY_PREFIX = NodeAddress.fromParts([
   "IDENTITY",
 ]);
 
-const userNodeType: NodeType = {
-  name: "user",
-  pluralName: "users",
+const identityNodeType: NodeType = {
+  name: "identity",
+  pluralName: "identities",
   defaultWeight: 0,
-  description: "a canonical user identity",
-  prefix: NodeAddress.append(IDENTITY_PREFIX, "USER"),
+  description: "a canonical participant identity",
+  prefix: IDENTITY_PREFIX,
 };
-const projectNodeType: NodeType = {
-  name: "project",
-  pluralName: "projects",
-  defaultWeight: 0,
-  description: "a canonical project identity",
-  prefix: NodeAddress.append(IDENTITY_PREFIX, "PROJECT"),
-};
-const organizationNodeType: NodeType = {
-  name: "organization",
-  pluralName: "organizations",
-  defaultWeight: 0,
-  description: "a canonical organization identity",
-  prefix: NodeAddress.append(IDENTITY_PREFIX, "ORGANIZATION"),
-};
-const botNodeType: NodeType = {
-  name: "bot",
-  pluralName: "bots",
-  defaultWeight: 0,
-  description: "a canonical bot identity",
-  prefix: NodeAddress.append(IDENTITY_PREFIX, "BOT"),
-};
-const nodeTypes = [
-  userNodeType,
-  projectNodeType,
-  organizationNodeType,
-  botNodeType,
-];
+const nodeTypes = [identityNodeType];
 
 export const declaration: PluginDeclaration = {
   name: "Identity",
