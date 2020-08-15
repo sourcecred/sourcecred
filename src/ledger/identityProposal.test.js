e860300efdf5a1922dd29acd7d5a7b8cb4dc6cea
Dandelion Mané <decentralion@dandelion.io>
2020-08-14 18:27:06 -0600

    Add a system for identity proposals
    This commit adds the `identityProposal` module, which specifies how a
    plugin may propose a new identity to be added to the Ledger. The
    proposal includes a name, the name of the plugin (for
    disambiguation), the type of identity that should be created, and
    the initial alias the identity should own.
    
    If some existing identity has an alias with that address already, then
    no action will be taken. However, if there is no identity matching that
    alias, then a new identity will be created, based on the name (and, if
    necessary, pluginName) of the proposal.
    
    This will allow the Ledger to be auto-populated with new identities as
    provided by plugins, which will save instance adminstrators from the
    very tedious task of making an identity for each user in the instance.
    This will also have other downstream benefits, like letting us do away
    with the plugin node types in favor of assuming that all user accounts
    correspond to identities (and, thus, identity addresses).
    
    This is a big step towards #2109.
    
    Test plan: Unit tests included, as per usual.
    
    Note: I considered putting the identity proposal logic into the ledger
    directly, but I think it's cleaner to write it as a consumer of the
    ledger APIs. Primarily because by forcing it to go through API
    boundaries, it's easier to reason about the implementation, and doesn't
    add any bloat to the Ledger module, which is already quite complex.

---
+++ src/ledger/identityProposal.test.js
@@ -0,0 +1,111 @@
+// @flow
+
+import deepFreeze from "deep-freeze";
+import {NodeAddress} from "../core/graph";
+import {Ledger} from "./ledger";
+import {nameFromString} from "./identity";
+import {ensureIdentityExists, _chooseIdentityName} from "./identityProposal";
+
+describe("ledger/identityProposal", () => {
+  const alias = {description: "example", address: NodeAddress.empty};
+  const proposal = deepFreeze({
+    name: nameFromString("foo"),
+    pluginName: nameFromString("bar"),
+    alias,
+    type: "USER",
+  });
+  describe("ensureIdentityExists", () => {
+    it("doesn't mutate the ledger if the address is already taken", () => {
+      const ledger = new Ledger();
+      const address = NodeAddress.empty;
+      const id = ledger.createIdentity("USER", "foo");
+      ledger.addAlias(id, alias);
+      const log = ledger.eventLog();
+      const otherAlias = {description: "different description", address};
+      const proposal = {
+        name: nameFromString("foo"),
+        pluginName: nameFromString("bar"),
+        alias: otherAlias,
+        type: "USER",
+      };
+      ensureIdentityExists(ledger, proposal);
+      // Verify that the ledger didn't mutate.
+      expect(ledger.eventLog()).toEqual(log);
+    });
+    it("creates a new identity (with alias) if address isn't taken", () => {
+      const ledger = new Ledger();
+      ensureIdentityExists(ledger, proposal);
+      const account = ledger.accountByAddress(alias.address);
+      if (account == null) {
+        throw new Error("identity not created");
+      }
+      expect(account.identity.name).toEqual(proposal.name);
+      expect(account.identity.aliases).toEqual([alias]);
+    });
+    it("uses the discriminator logic from _chooseIdentityName if needed", () => {
+      const ledger = new Ledger();
+      ledger.createIdentity("USER", "foo");
+      ensureIdentityExists(ledger, proposal);
+      const account = ledger.accountByAddress(alias.address);
+      if (account == null) {
+        throw new Error("identity not created");
+      }
+      expect(account.identity.name).toEqual("foo-bar");
+      expect(account.identity.aliases).toEqual([alias]);
+    });
+    it("creates an identity with the correct type", () => {
+      for (const type of ["USER", "PROJECT", "ORGANIZATION", "BOT"]) {
+        const ledger = new Ledger();
+        const typedProposal = {
+          name: nameFromString("foo"),
+          pluginName: nameFromString("bar"),
+          alias,
+          type,
+        };
+        ensureIdentityExists(ledger, typedProposal);
+        const account = ledger.accountByAddress(alias.address);
+        if (account == null) {
+          throw new Error("identity not created");
+        }
+        expect(account.identity.subtype).toEqual(type);
+      }
+    });
+  });
+  describe("_chooseIdentityName", () => {
+    it("returns the default name if available", () => {
+      function checkAvailable() {
+        return true;
+      }
+      expect(_chooseIdentityName(proposal, checkAvailable)).toEqual("foo");
+    });
+    it("adds a plugin discriminator if needed", () => {
+      function checkAvailable(n) {
+        return n !== "foo";
+      }
+      expect(_chooseIdentityName(proposal, checkAvailable)).toEqual("foo-bar");
+    });
+    it("adds a further numeric discriminator if needed", () => {
+      function checkAvailable(n) {
+        return n !== "foo" && n !== "foo-bar";
+      }
+      expect(_chooseIdentityName(proposal, checkAvailable)).toEqual(
+        "foo-bar-1"
+      );
+    });
+    it("increments the numeric discriminator if needed", () => {
+      function checkAvailable(n) {
+        return n !== "foo" && n !== "foo-bar" && n !== "foo-bar-1";
+      }
+      expect(_chooseIdentityName(proposal, checkAvailable)).toEqual(
+        "foo-bar-2"
+      );
+    });
+    it("fails if it can't find any valid discriminator after many tries", () => {
+      function checkAvailable() {
+        return false;
+      }
+      const thunk = () => _chooseIdentityName(proposal, checkAvailable);
+      expect(thunk).toThrowError("unable to find an identity name");
+    });
+  });
+});
