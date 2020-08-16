39e97949c45112a257f05301bee7c5481f6b5ef9
Dandelion Mané <decentralion@dandelion.io>
2020-08-16 14:56:19 -0600

    Support merging identities in the ledger admin
    As of #2134, we create identities for every user. Thus, we no longer
    want to add aliases to identities, but instead we merge them together
    (this functionality from #2117). This commit updates the frontend so we
    can merge identities.
    
    Thanks to @hammadj for major help letting this land in a bug free
    fashion.
    
    Test plan: Run `yarn start`, and merge some identities together.

---
+++ src/ui/components/AliasView.js
@@ -0,0 +1,44 @@
+// @flow
+
+import React from "react";
+import {type IdentityId} from "../../ledger/identity";
+import Markdown from "react-markdown";
+
+import {makeStyles} from "@material-ui/core/styles";
+import {List, ListItem} from "@material-ui/core";
+import {useLedger} from "../utils/LedgerContext";
+
+type Props = {|
+  +selectedId: IdentityId,
+|};
+
+const useStyles = makeStyles({
+  element: {margin: "20px"},
+  aliasesHeader: {margin: "20px", marginBottom: 0},
+});
+
+export function AliasView({selectedId}: Props) {
+  const {ledger} = useLedger();
+  const classes = useStyles();
+  const selectedAccount = ledger.account(selectedId);
+
+  return (
+    <>
+      {selectedAccount.identity.aliases.length > 0 && (
+        <>
+          <h3 className={classes.aliasesHeader}>Aliases:</h3>
+          <List dense>
+            {selectedAccount.identity.aliases.map((alias) => (
+              <ListItem key={alias.address}>
+                <Markdown
+                  renderers={{paragraph: "span"}}
+                  source={alias.description}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </>
+      )}
+    </>
+  );
+}
