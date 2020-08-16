cdd5192cadf3e449a52229b4cd2a99f8cf1ebb1d
Dandelion Mané <decentralion@dandelion.io>
2020-08-16 13:33:22 -0600

    github: add createIdentities (#2132)
    This adds a method for the GitHub plugin to create identities, per
    the identity proposal system added in #2128.
    
    Test plan: Unit tests. `yarn test`.

---
+++ src/plugins/github/createIdentities.js
@@ -0,0 +1,40 @@
+// @flow
+
+import {type IdentityProposal} from "../../ledger/identityProposal";
+import {coerce, nameFromString} from "../../ledger/identity/name";
+import {RelationalView, Userlike} from "./relationalView";
+import {toRaw} from "./nodes";
+
+export function _createIdentity(u: Userlike): IdentityProposal {
+  const alias = {
+    description: u.description(),
+    address: toRaw(u.address()),
+  };
+  function chooseType(u: Userlike) {
+    const subtype = u.address().subtype;
+    // TODO: We should let it infer ORGANIZATION or PROJECT types too,
+    // but at present we'll let the maintainer set this directly in the UI.
+    switch (subtype) {
+      case "USER": {
+        return "USER";
+      }
+      case "BOT": {
+        return "BOT";
+      }
+      default:
+        throw new Error(`unknown userlike subtype: ${(subtype: empty)}`);
+    }
+  }
+  return {
+    pluginName: nameFromString("github"),
+    name: coerce(u.login()),
+    type: chooseType(u),
+    alias,
+  };
+}
+
+export function createIdentities(
+  rv: RelationalView
+): $ReadOnlyArray<IdentityProposal> {
+  return Array.from(rv.userlikes()).map(_createIdentity);
+}
