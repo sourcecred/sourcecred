b68da58ce845ccf716eb4186cd0d32817d434287
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2020-09-07 17:28:30 -0500

    de-anonymize anonymous components
    the react eslint plugin bump has enabled a new rule (react/display-name)
    that disallows unnamed react components. Changes to the AppLayout and
    Menu components were needed to align with this new rule.
    
    test plan: yarn lint, and ensuring the frontend builds and renders
    correct from `yarn start` and browsing to localhost:8080

--- src/ui/components/Menu.js
+++ src/ui/components/Menu.js
@@ -9,46 +9,51 @@ import {type CurrencyDetails} from "../../api/currencyConfig";
 
 type menuProps = {|onMenuClick: Function|};
 
-const Menu = (hasBackend: Boolean, {name: currencyName}: CurrencyDetails) => ({
-  onMenuClick,
-}: menuProps) => {
-  const open = useSelector((state) => state.admin.ui.sidebarOpen);
-  return (
-    <>
-      <MenuItemLink
-        to="/explorer"
-        primaryText="Explorer"
-        leftIcon={<TrendingUpIcon />}
-        onClick={onMenuClick}
-        sidebarIsOpen={open}
-      />
-      <MenuItemLink
-        to="/accounts"
-        primaryText={`${currencyName} Accounts`}
-        leftIcon={<DefaultIcon />}
-        onClick={onMenuClick}
-        sidebarIsOpen={open}
-      />
-      {hasBackend && (
-        <>
-          <MenuItemLink
-            to="/admin"
-            primaryText="Ledger Admin"
-            leftIcon={<DefaultIcon />}
-            onClick={onMenuClick}
-            sidebarIsOpen={open}
-          />
-          <MenuItemLink
-            to="/transfer"
-            primaryText={`Transfer ${currencyName}`}
-            leftIcon={<TransformIcon />}
-            onClick={onMenuClick}
-            sidebarIsOpen={open}
-          />
-        </>
-      )}
-    </>
-  );
+const createMenu = (
+  hasBackend: Boolean,
+  {name: currencyName}: CurrencyDetails
+) => {
+  const Menu = ({onMenuClick}: menuProps) => {
+    const open = useSelector((state) => state.admin.ui.sidebarOpen);
+    return (
+      <>
+        <MenuItemLink
+          to="/explorer"
+          primaryText="Explorer"
+          leftIcon={<TrendingUpIcon />}
+          onClick={onMenuClick}
+          sidebarIsOpen={open}
+        />
+        <MenuItemLink
+          to="/accounts"
+          primaryText={`${currencyName} Accounts`}
+          leftIcon={<DefaultIcon />}
+          onClick={onMenuClick}
+          sidebarIsOpen={open}
+        />
+        {hasBackend && (
+          <>
+            <MenuItemLink
+              to="/admin"
+              primaryText="Ledger Admin"
+              leftIcon={<DefaultIcon />}
+              onClick={onMenuClick}
+              sidebarIsOpen={open}
+            />
+            <MenuItemLink
+              to="/transfer"
+              primaryText={`Transfer ${currencyName}`}
+              leftIcon={<TransformIcon />}
+              onClick={onMenuClick}
+              sidebarIsOpen={open}
+            />
+          </>
+        )}
+      </>
+    );
+  };
+
+  return Menu;
 };
 
-export default Menu;
+export default createMenu;
