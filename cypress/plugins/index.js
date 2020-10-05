ab42eae94e0bf371da4d550d71865e5743ff72cd
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2020-10-05 12:17:25 -0500

    Set Up Cypress
    Cypress is an versatile testing framework that can be used for unit and
    end-to-end frontend tests. It utilizes mocha and chai libraries for
    writing tests.
    
    The flow libdefs for all of these libraries are quite lacking, however
    and I'm strongly considering using a .flow file that is localized to the
    cypress folder to resolve this, and additionally, mitigate collisions
    with Jest.
    
    Cypress has not been hooked up to the larger testing sequence we have,
    	as no useful test have been written yet.
    
    test plan: ensure all existing automated test pass

---
+++ cypress/plugins/index.js
@@ -0,0 +1,34 @@
+// @flow
+
+const browserify = require("@cypress/browserify-preprocessor");
+
+/*::
+declare type Cypress$EventHook = (
+  name: string,
+  exe: (err: {message: string, ...}, runnable: any) => boolean | void
+) => void;
+*/
+
+// ***********************************************************
+// This can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+// TODO (@topocount): create Cypress Config File flow type
+module.exports = (on /*: Cypress$EventHook*/, _unused_config /*: any*/) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+
+  // utilize the cypress browserify plugin to strip flow type syntax
+  const options = browserify.defaultOptions;
+  options.browserifyOptions.transform[1][1].presets.push("@babel/preset-flow");
+  on("file:preprocessor", browserify(options));
+};
