4a711368c5e52e6ac957a85fc9a356318ef1d432
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2020-10-14 22:52:53 -0500

    Eth Plugin: Create the EthAddress file and subcomponents
    EthAddress files file stores of ethereum addresses. For now, these
    addresses are intended to just be an identity alias for participants
    primarily for the purpose of authenticating in a self-service frontend.
    
    Serialized compat files don't preserve the readability of jsonLogs, so
    there is little use in utilizing it now. Some compat serialization
    logic is needed to preserve readability. This will be important to
    implement before wider release in order to better accomodate readable
    diffs in a UI and within Git.
    
    test plan: unit tests are provided for the functions utilized by
    parsers.

---
+++ src/plugins/ethereum/ethAddress.js
@@ -0,0 +1,39 @@
+// @flow
+
+import {isAddress, toChecksumAddress} from "web3-utils";
+
+import * as C from "../../util/combo";
+import {compatibleParser} from "../../util/compat";
+
+export opaque type EthAddress: string = string;
+
+export function parseAddress(s: string): EthAddress {
+  if (!isAddress(s)) {
+    throw new Error(`not a valid ethereum address: ${s}`);
+  }
+  return toChecksumAddress(s);
+}
+
+// Utilized for a more readable and recognizable address. This is
+// needed to allow the address to fit within the Identity name requirements
+export function truncateEthAddress(address: EthAddress): string {
+  const prefix = address.slice(0, 6);
+  const suffix = address.slice(-4);
+  return `${prefix}...${suffix}`;
+}
+
+export const ethAddressParser: C.Parser<EthAddress> = C.fmap(
+  C.string,
+  parseAddress
+);
+
+export const COMPAT_INFO = {
+  type: "sourcecred/ethAddressFile",
+  version: "0.0.1",
+};
+
+const addressEntriesParser = C.array(ethAddressParser);
+
+export const parser: C.Parser<
+  Array<EthAddress>
+> = compatibleParser(COMPAT_INFO.type, {"0.0.1": addressEntriesParser});
---
+++ src/plugins/ethereum/ethAddress.test.js
@@ -0,0 +1,65 @@
+// @flow
+
+import {
+  parseAddress,
+  truncateEthAddress as truncate,
+  COMPAT_INFO,
+  parser,
+  type EthAddress,
+} from "./ethAddress";
+import {toCompat} from "../../util/compat";
+
+describe("plugins/ethereum/ethAddress", () => {
+  describe("parseAddress", () => {
+    it("can parse a well-formed ethereum address", () => {
+      [
+        "2Ccc7cD913677553766873483ed9eEDdB77A0Bb0",
+        "0x2Ccc7cD913677553766873483ed9eEDdB77A0Bb0",
+        "0x2Ccc7cD913677553766873483ed9eEDdB77A0Bb0".toUpperCase(),
+        "0x2Ccc7cD913677553766873483ed9eEDdB77A0Bb0".toLowerCase(),
+      ].forEach((a: string) => {
+        expect(parseAddress(a)).toBe(
+          "0x2Ccc7cD913677553766873483ed9eEDdB77A0Bb0"
+        );
+      });
+    });
+    it("throws when attempting to parse a malformed address", () => {
+      [
+        "0x",
+        // malformed mixed-case
+        "0x2ccc7cD913677553766873483ed9eEDdB77A0Bb0",
+        "abc123",
+        "",
+      ].forEach((a: string) => {
+        expect(() => parseAddress(a)).toThrow(
+          `not a valid ethereum address: ${a}`
+        );
+      });
+    });
+  });
+
+  describe("truncateEthAddres", () => {
+    it("creates well-formed truncated addresses", () => {
+      [
+        //$FlowExpectedError[incompatible-type]
+        ["0x2Ccc7cD913677553766873483ed9eEDdB77A0Bb0", "0x2Ccc...0Bb0"],
+        //$FlowExpectedError[incompatible-type]
+        ["0xb4124cEB3451635DAcedd11767f004d8a28c6eE7", "0xb412...6eE7"],
+      ].forEach(([a, t]: [EthAddress, string]) => {
+        expect(truncate(a)).toBe(t);
+      });
+    });
+  });
+
+  describe("compatible JsonLog", () => {
+    const address1 = "0x2Ccc7cD913677553766873483ed9eEDdB77A0Bb0";
+    const address2 = "0xb4124cEB3451635DAcedd11767f004d8a28c6eE7";
+    const address3 = "0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb";
+    const addressArray = [address1, address2, address3];
+    it("can create an exported JsonLog instance", () => {
+      const compatibleEthLog = toCompat(COMPAT_INFO, addressArray);
+      const result = parser.parseOrThrow(compatibleEthLog).values();
+      expect(Array.from(result)).toEqual(addressArray);
+    });
+  });
+});
