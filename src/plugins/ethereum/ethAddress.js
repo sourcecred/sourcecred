bc6655dd4ab6942b11cea948c58b0d8c5d8f52af
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2020-10-23 13:41:32 -0500

    Add docstring to parseAddress function

--- src/plugins/ethereum/ethAddress.js
+++ src/plugins/ethereum/ethAddress.js
@@ -7,6 +7,27 @@ import {compatibleParser} from "../../util/compat";
 
 export opaque type EthAddress: string = string;
 
+/**
+ * parseAddress will accept any 20-byte hexadecimal ethereum address encoded as
+ * a string, optionally prefixed with `0x`.
+ *
+ * Per EIP-55 (https://eips.ethereum.org/EIPS/eip-55), parseAddress throws if
+ * the provided string is mixed-case but not checksum-encoded. All valid
+ * addresses in lower- and upper-case format will not throw.
+ *
+ * For consistency, all valid addresses are converted and returned in
+ * mixed-case form with the `0x` prefix included
+ *
+ * valid formats:
+ * "2Ccc7cD913677553766873483ed9eEDdB77A0Bb0"
+ * "0x2Ccc7cD913677553766873483ed9eEDdB77A0Bb0"
+ * "0X2CCC7CD913677553766873483ED9EEDDB77A0BB0"
+ * "0x2ccc7cd913677553766873483ed9eeddb77a0bb0"
+ *
+ * invalid formats:
+ * "0x2ccc7cD913677553766873483ed9eEDdB77A0Bb0"
+ * "2ccc7cD913677553766873483ed9eEDdB77A0Bb0"
+ */
 export function parseAddress(s: string): EthAddress {
   if (!isAddress(s)) {
     throw new Error(`not a valid ethereum address: ${s}`);
