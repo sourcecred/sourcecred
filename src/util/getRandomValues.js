49fc294407a95fae743852d19344f6600dd1e84b
William Chargin <wchargin@gmail.com>
2020-10-29 18:07:48 -0700

    api: fix Observable compatibility
    Summary:
    This patch replaces our `getRandomValues` polyfill with a fork that
    works in the browser, in Node, in Jest, and in Observable. It’s slightly
    hacky because we can’t figure out how to get Webpack’s externs to play
    nicely, but the hackiness is entirely contained in this file.
    
    Paired with @topocount.
    
    Fixes #2351.
    
    Test Plan:
    Run `yarn build:api`. Test in Node:
    
    ```
    $ node
    > (new (require("./dist/api").default.ledger.ledger.Ledger)()).createIdentity("USER", "z")
    'GtJslqp9kl7J8Dr869xGKw'
    ```
    
    Test in Observable:
    
     1. Visit <https://observablehq.com/new>.
     2. Click the “Notebook actions” horizontal ellipsis button to the left
        of the “Publish” button in the top of the screen.
     3. Select “File attachments”.
     4. Click “Upload”, and select `dist/api.js` under your repo.
     5. This will upload `api.js`, or `api@N.js` for some value of `N` if
        you’ve done this before. If you see `api@N.js`, adjust accordingly
        in the following steps.
     6. In an Observable cell, run:
    
        ```javascript
        {
          const sc = (await require(await FileAttachment("api.js").url()));
          return new sc.default.ledger.ledger.Ledger().createIdentity("USER", "z");
        }
        ```
    
     7. Observe that it spits out a UUID without crashing.
    
    wchargin-branch: api-observable-build
    wchargin-source: f0aaf8d80ba7a0a92187d76b2985b4cd7e5b502a

---
+++ src/util/getRandomValues.js
@@ -0,0 +1,54 @@
+// @flow
+
+/*
+ * API shim for `window.crypto.getRandomValues` in the browser, for
+ * compatibility with browser, Node, Jest, and Observable.
+ *
+ * Forked from NPM `get-random-bytes` by Kenan Yildirim, which is
+ * released under the MIT License.
+ */
+
+/**
+ * Fill the given buffer with cryptographically secure random bytes. The
+ * buffer length must not exceed 65536.
+ */
+function getRandomValues(buf: Uint8Array): Uint8Array {
+  if (typeof window !== "undefined") {
+    if (window.crypto && window.crypto.getRandomValues) {
+      return window.crypto.getRandomValues(buf);
+    }
+    if (
+      typeof window.msCrypto === "object" &&
+      typeof window.msCrypto.getRandomValues === "function"
+    ) {
+      return window.msCrypto.getRandomValues(buf);
+    }
+  }
+
+  if (typeof require !== "undefined") {
+    // Late-import `crypto` to avoid `require` in Observable notebooks,
+    // and avoid using a literal `require(...)` to prevent Webpack from
+    // rewriting this. (Getting Webpack externals to work properly is .)
+    const realRequire =
+      // $FlowExpectedError[cannot-resolve-name]
+      typeof __non_webpack_require__ !== "undefined"
+        ? __non_webpack_require__
+        : require; /* needed for Jest */
+    const nodeCrypto = realRequire("crypto");
+    if (!nodeCrypto.randomBytes) {
+    }
+    if (buf.length > 65536) {
+      const e = new Error();
+      (e: any).code = 22;
+      e.message = `Quota exceeded: requested ${buf.length} > 65536 bytes`;
+      e.name = "QuotaExceededError";
+      throw e;
+    }
+    const bytes = nodeCrypto.randomBytes(buf.length);
+    buf.set(bytes);
+    return buf;
+  }
+  throw new Error("No secure random number generator available.");
+}
+
+module.exports = getRandomValues;
