ee950965f1b78cb0e3fdc67be2049d39f6a19f4e
Angela Gilhotra <angelagilhotra@gmail.com>
2020-11-11 19:15:23 +0530

    Prettify & Fix lint

--- src/plugins/slack/tests/fetch.test.js
+++ src/plugins/slack/tests/fetch.test.js
@@ -3,38 +3,37 @@
  */
 
 // import { testFetcher } from './mockFetch';
-import { Fetcher } from "../fetch";
+import {Fetcher} from "../fetch";
 
 /**
  * This is in no form a good test suite. Only written to check sanity of the code. Need to update to include better snapshot testing.
  */
 
-describe ("plugins/slack/fetch", () => {
+describe("plugins/slack/fetch", () => {
   const fetcher = new Fetcher(process.env.SLACK_TOKEN);
   jest.setTimeout(30000); // 30 seconds timeout (takes longer due to pagination)
 
   describe("testing with the kernel org", () => {
-    it ("loads users", async () => {
-    //   expect (await fetcher.users());
+    it("loads users", async () => {
+      //   expect (await fetcher.users());
       const users = await fetcher.users();
-      console.log ('all users:', users);
-    })
+      console.log("all users:", users);
+    });
 
-    it ("loads channels", async () => {
-    //   expect (await fetcher.channels());
+    it("loads channels", async () => {
+      //   expect (await fetcher.channels());
       const channels = await fetcher.channels();
-      console.log('all channels:', channels);
-    })
+      console.log("all channels:", channels);
+    });
 
-    it ("loads messages", async() => {
+    it("loads messages", async () => {
       const channel = {
-        id: 'C01CPGVGXSB', name: '#testing-out-things'
-      }
-    //   expect (await fetcher.messages(channel))
+        id: "C01CPGVGXSB",
+        name: "#testing-out-things",
+      };
+      //   expect (await fetcher.messages(channel))
       const messages = await fetcher.messages(channel);
-      console.log ('all messages:', messages);
-    })
-
-  })
-
-})
+      console.log("all messages:", messages);
+    });
+  });
+});
