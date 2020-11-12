87142503b245e451eca41d992960091e9eeb6bd1
Hammad Jutt <jutt@ualberta.ca>
2020-11-12 11:36:27 -0700

    Implement function for diffing event histories of two Ledgers (#2504)
    This diffLedger function will be useful for syncing two different Ledgers and improving
    the front end UX for showing what changes a user has made to their local Ledger. It will
    be used in the LedgerManager class to help avoid merge conflicts and breaking ledger entries
    for self-service admin.
    
    Test Plan: Wrote unit tests to verify functionality

---
+++ src/core/ledger/diffLedger.test.js
@@ -0,0 +1,99 @@
+// @flow
+
+import {diffLedger} from "./diffLedger";
+import * as uuid from "../../util/uuid";
+import {Ledger} from "./ledger"; // for spy purposes
+
+describe("core/ledger/diffLedger", () => {
+  const randomMock = jest.spyOn(uuid, "random");
+
+  let nextFakeUuidIndex = 0;
+  function resetFakeUuid() {
+    nextFakeUuidIndex = 0;
+  }
+  function nextFakeUuid(): uuid.Uuid {
+    const uuidString = String(nextFakeUuidIndex).padStart(21, "0") + "A";
+    nextFakeUuidIndex++;
+    return uuid.fromString(uuidString);
+  }
+
+  randomMock.mockImplementation(nextFakeUuid);
+  function setNextUuid(x: uuid.Uuid) {
+    randomMock.mockImplementationOnce(() => x);
+  }
+
+  const id1 = uuid.fromString("YVZhbGlkVXVpZEF0TGFzdA");
+  const id2 = uuid.fromString("URgLrCxgvjHxtGJ9PgmckQ");
+
+  function ledgerWithIdentities() {
+    resetFakeUuid();
+    const ledger = new Ledger();
+    setNextUuid(id1);
+    ledger.createIdentity("USER", "steven");
+    setNextUuid(id2);
+    ledger.createIdentity("ORGANIZATION", "crystal-gems");
+    return ledger;
+  }
+
+  it("should handle the empty case", () => {
+    const a = new Ledger();
+    const b = new Ledger();
+
+    expect(diffLedger(a, b)).toEqual([]);
+  });
+
+  it("should return all events from a if b is empty", () => {
+    const a = ledgerWithIdentities();
+    const b = new Ledger();
+
+    expect(diffLedger(a, b)).toEqual(a.eventLog());
+  });
+
+  it("should return an empty array if a is empty and b has events", () => {
+    const a = new Ledger();
+    const b = ledgerWithIdentities();
+
+    expect(diffLedger(a, b)).toEqual([]);
+  });
+
+  it("should return only the events in a that dont exist in b", () => {
+    const b = ledgerWithIdentities();
+
+    const a = Ledger.fromEventLog(b.eventLog());
+    a.activate(id1);
+    a.activate(id2);
+
+    const id1ActivationEvent = {
+      ledgerTimestamp: expect.anything(),
+      uuid: expect.anything(),
+      action: {type: "TOGGLE_ACTIVATION", identityId: id1},
+      version: "1",
+    };
+
+    const id2ActivationEvent = {
+      ledgerTimestamp: expect.anything(),
+      uuid: expect.anything(),
+      action: {type: "TOGGLE_ACTIVATION", identityId: id2},
+      version: "1",
+    };
+    expect(diffLedger(a, b)).toEqual([id1ActivationEvent, id2ActivationEvent]);
+
+    const aLedgerLog = a.eventLog();
+
+    // Create a duplicate event out-of-order in b to test if it gets filtered
+    setNextUuid(aLedgerLog[aLedgerLog.length - 1].uuid);
+    b.activate(id2);
+
+    // Create a new event in b to test if it gets ignored
+    b.createIdentity("USER", "bob");
+
+    expect(diffLedger(a, b)).toEqual([
+      {
+        ledgerTimestamp: expect.anything(),
+        uuid: expect.anything(),
+        action: {type: "TOGGLE_ACTIVATION", identityId: id1},
+        version: "1",
+      },
+    ]);
+  });
+});
