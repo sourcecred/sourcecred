14e901e2ba04cf931fc239aaa3db69c787672059
Brian Belhumeur <[my last name]@gmail.com>
2021-02-01 22:32:36 +0100

    Add pagination, filtering and sorting to ExplorerHome

--- src/ui/components/ExplorerHome/ExplorerHome.js
+++ src/ui/components/ExplorerHome/ExplorerHome.js
@@ -1,5 +1,5 @@
 // @flow
-import React, {useState, type Node as ReactNode} from "react";
+import React, {useState, useMemo, type Node as ReactNode} from "react";
 import {
   Checkbox,
   Container,
@@ -14,14 +14,23 @@ import {
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
+  TablePagination,
   TableRow,
   TextField,
 } from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import deepFreeze from "deep-freeze";
 import {CredGrainView} from "../../../core/credGrainView";
-import sortBy from "../../../util/sortBy";
+import {
+  useTableState,
+  SortOrders,
+  DEFAULT_SORT,
+} from "../../../webutil/tableState";
 import CredTimeline from "./CredTimeline";
+import {IdentityTypes} from "../../../core/identity/identityType";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -88,7 +97,29 @@ const useStyles = makeStyles((theme) => ({
   element: {flex: 1, margin: "20px"},
   arrowInput: {width: "40%", display: "inline-block"},
   pageHeader: {color: theme.palette.text.primary},
+  credCircle: {
+    borderColor: theme.palette.blueish,
+  },
+  grainCircle: {
+    borderColor: theme.palette.orange,
+  },
+  participantCircle: {
+    borderColor: theme.palette.pink,
+  },
+  grainPerCredCircle: {
+    borderColor: theme.palette.green,
+  },
 }));
+
+const CRED_SORT = deepFreeze({
+  name: Symbol("Cred"),
+  fn: (n) => n.cred,
+});
+const GRAIN_SORT = deepFreeze({
+  name: Symbol("Grain"),
+  fn: (n) => n.grainEarned,
+});
+const PAGINATION_OPTIONS = deepFreeze([25, 50, 100]);
 
 type ExplorerHomeProps = {|
   +initialView: CredGrainView | null,
@@ -100,37 +131,38 @@ export const ExplorerHome = ({initialView}: ExplorerHomeProps): ReactNode => {
   const classes = useStyles();
   const [tab, setTab] = useState<number>(1);
   const [checkboxes, setCheckboxes] = useState({
-    participants: false,
-    organizations: false,
-    bots: false,
-    projects: false,
+    [IdentityTypes.USER]: false,
+    [IdentityTypes.ORGANIZATION]: false,
+    [IdentityTypes.BOT]: false,
+    [IdentityTypes.PROJECT]: false,
   });
-  const {participants, organizations, bots, projects} = checkboxes;
 
-  const handleChange = (event) => {
-    setCheckboxes({...checkboxes, [event.target.name]: event.target.checked});
-  };
+  const allParticipants = useMemo(
+    () => Array.from(initialView.participants()),
+    [initialView.participants()]
+  );
 
   const summaryInfo = [
     {title: "Cred This Week", value: 610},
     {title: "Grain Harvested", value: "6,765g"},
-    {title: "Active Participants", value: 13},
+    {title: "Active Participants", value: allParticipants.length},
     {title: "Grain per Cred", value: "22g"},
   ];
 
-  const createData = (username, cred, grain, chart) => ({
-    username,
-    cred,
-    grain,
-    chart,
+  // sort by cred amount, highest to lowest
+  const nodes = useTableState(allParticipants, {
+    initialRowsPerPage: PAGINATION_OPTIONS[0],
+    initialSort: {
+      sortName: CRED_SORT.name,
+      sortOrder: SortOrders.DESC,
+      sortFn: CRED_SORT.fn,
+    },
   });
 
-  // sort by cred amount, highest to lowest
-  const nodes = sortBy(initialView.participants(), (n) => -n.cred);
   // create an array of 0s for the cred summary graph at the top of the page
   let credTimelineSummary = initialView.intervals().map(() => 0);
 
-  const rows = nodes.map((node) => {
+  const rows = nodes.currentPage.map((node) => {
     const {credPerInterval} = node;
 
     // add this node's cred to the summary graph
@@ -138,27 +170,65 @@ export const ExplorerHome = ({initialView}: ExplorerHomeProps): ReactNode => {
       (total, i) => credPerInterval[i] + total
     );
 
-    return createData(
-      node.identity.name,
-      node.cred,
-      node.grainEarned,
-      credPerInterval
-    );
+    return {
+      username: node.identity.name,
+      cred: node.cred,
+      grain: node.grainEarned,
+      chart: credPerInterval,
+    };
   });
+
+  const filterIdentities = (event: SyntheticInputEvent<HTMLInputElement>) => {
+    // fuzzy match letters "in order, but not necessarily sequentially"
+    const filterString = event.target.value
+      .trim()
+      .toLowerCase()
+      .split("")
+      .join("+.*");
+    const regex = new RegExp(filterString);
+
+    nodes.createOrUpdateFilterFn("filterIdentities", (participant) =>
+      regex.test(participant.identity.name.toLowerCase())
+    );
+  };
+
+  const handleCheckboxFilter = (event) => {
+    const newCheckboxes = {
+      ...checkboxes,
+      [event.target.name]: event.target.checked,
+    };
+    setCheckboxes(newCheckboxes);
+
+    const includedTypes = Object.keys(newCheckboxes).filter(
+      (type) => newCheckboxes[type] === true
+    );
+
+    if (includedTypes.length === 0) {
+      nodes.createOrUpdateFilterFn("identityType", () => true);
+    } else {
+      nodes.createOrUpdateFilterFn("identityType", (participant) =>
+        includedTypes.includes(participant.identity.subtype)
+      );
+    }
+  };
+
+  const handleChangePage = (event, newIndex) => {
+    nodes.setPageIndex(newIndex);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    nodes.setRowsPerPage(Number(event.target.value));
+  };
 
   const makeCircle = (
     value: string | number,
     title: string,
-    borderColor: string
+    className: string
   ) => (
     <div
-      className={`${classes.centerRow} ${classes.circleWrapper}`}
-      style={{color: borderColor}}
+      className={`${classes.centerRow} ${classes.circleWrapper} ${className}`}
     >
-      <div
-        className={`${classes.centerRow} ${classes.circle}`}
-        style={{borderColor}}
-      >
+      <div className={`${classes.centerRow} ${classes.circle} ${className}`}>
         {value}
       </div>
       <div>{title}</div>
@@ -199,10 +269,26 @@ export const ExplorerHome = ({initialView}: ExplorerHomeProps): ReactNode => {
         </Tabs>
       </div>
       <div className={classes.centerRow}>
-        {makeCircle(summaryInfo[0].value, summaryInfo[0].title, "#6174CC")}
-        {makeCircle(summaryInfo[1].value, summaryInfo[1].title, "#FFAA3D")}
-        {makeCircle(summaryInfo[2].value, summaryInfo[2].title, "#FDBBD1")}
-        {makeCircle(summaryInfo[3].value, summaryInfo[3].title, "#4BD76D")}
+        {makeCircle(
+          summaryInfo[0].value,
+          summaryInfo[0].title,
+          classes.credCircle
+        )}
+        {makeCircle(
+          summaryInfo[1].value,
+          summaryInfo[1].title,
+          classes.grainCircle
+        )}
+        {makeCircle(
+          summaryInfo[2].value,
+          summaryInfo[2].title,
+          classes.participantCircle
+        )}
+        {makeCircle(
+          summaryInfo[3].value,
+          summaryInfo[3].title,
+          classes.grainPerCredCircle
+        )}
       </div>
       <div className={classes.row}>
         <div className={classes.tableWrapper} style={{flexDirection: "column"}}>
@@ -215,7 +301,11 @@ export const ExplorerHome = ({initialView}: ExplorerHomeProps): ReactNode => {
             }}
           >
             <span style={{fontSize: "24px"}}>Last Week&apos;s Activity</span>
-            <TextField label="Filter Names" variant="outlined" />
+            <TextField
+              label="Filter Names"
+              variant="outlined"
+              onChange={filterIdentities}
+            />
           </div>
           <TableContainer component={Paper}>
             <Table aria-label="simple table">
@@ -225,10 +315,34 @@ export const ExplorerHome = ({initialView}: ExplorerHomeProps): ReactNode => {
                     <b>Participant</b>
                   </TableCell>
                   <TableCell>
-                    <b>Cred</b>
+                    <TableSortLabel
+                      active={nodes.sortName === CRED_SORT.name}
+                      direction={
+                        nodes.sortName === CRED_SORT.name
+                          ? nodes.sortOrder
+                          : DEFAULT_SORT
+                      }
+                      onClick={() =>
+                        nodes.setSortFn(CRED_SORT.name, CRED_SORT.fn)
+                      }
+                    >
+                      <b>{CRED_SORT.name.description}</b>
+                    </TableSortLabel>
                   </TableCell>
                   <TableCell>
-                    <b>Grain</b>
+                    <TableSortLabel
+                      active={nodes.sortName === GRAIN_SORT.name}
+                      direction={
+                        nodes.sortName === GRAIN_SORT.name
+                          ? nodes.sortOrder
+                          : DEFAULT_SORT
+                      }
+                      onClick={() =>
+                        nodes.setSortFn(GRAIN_SORT.name, GRAIN_SORT.fn)
+                      }
+                    >
+                      <b>{GRAIN_SORT.name.description}</b>
+                    </TableSortLabel>
                   </TableCell>
                   <TableCell>
                     <b>Contributions Chart (ALL TIME)</b>
@@ -269,6 +383,27 @@ export const ExplorerHome = ({initialView}: ExplorerHomeProps): ReactNode => {
                   <TableCell align="right" />
                 </TableRow>
               </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    rowsPerPageOptions={[
+                      ...PAGINATION_OPTIONS,
+                      {label: "All", value: Number.MAX_SAFE_INTEGER},
+                    ]}
+                    colSpan={4}
+                    count={nodes.length}
+                    rowsPerPage={nodes.rowsPerPage}
+                    page={nodes.pageIndex}
+                    SelectProps={{
+                      inputProps: {"aria-label": "rows per page"},
+                      native: true,
+                    }}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                    // ActionsComponent={TablePaginationActions}
+                  />
+                </TableRow>
+              </TableFooter>
             </Table>
           </TableContainer>
           <FormGroup row className={classes.rightRow}>
@@ -278,25 +413,29 @@ export const ExplorerHome = ({initialView}: ExplorerHomeProps): ReactNode => {
             <FormControlLabel
               control={
                 <Checkbox
-                  checked={participants}
-                  onChange={handleChange}
-                  name="participants"
+                  checked={checkboxes[IdentityTypes.USER]}
+                  onChange={handleCheckboxFilter}
+                  name={IdentityTypes.USER}
                 />
               }
               label="Participants"
             />
             <FormControlLabel
               control={
-                <Checkbox checked={bots} onChange={handleChange} name="bots" />
+                <Checkbox
+                  checked={checkboxes[IdentityTypes.BOT]}
+                  onChange={handleCheckboxFilter}
+                  name={IdentityTypes.BOT}
+                />
               }
               label="Bots"
             />
             <FormControlLabel
               control={
                 <Checkbox
-                  checked={projects}
-                  onChange={handleChange}
-                  name="projects"
+                  checked={checkboxes[IdentityTypes.PROJECT]}
+                  onChange={handleCheckboxFilter}
+                  name={IdentityTypes.PROJECT}
                 />
               }
               label="Projects"
@@ -304,9 +443,9 @@ export const ExplorerHome = ({initialView}: ExplorerHomeProps): ReactNode => {
             <FormControlLabel
               control={
                 <Checkbox
-                  checked={organizations}
-                  onChange={handleChange}
-                  name="organizations"
+                  checked={checkboxes[IdentityTypes.ORGANIZATION]}
+                  onChange={handleCheckboxFilter}
+                  name={IdentityTypes.ORGANIZATION}
                 />
               }
               label="Organizations"
