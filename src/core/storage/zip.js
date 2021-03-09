d1b012fd3d687a1171e6fd47b7d9682e36f1778d
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2021-03-09 01:14:37 -0600

    Implement ZipStorage
    ZipStorage composes with other DataStorage implementations. It compresses
    `value`s before passing them into the underlying `baseStorage` class.
    
    The longer reaching purpose of this class is to allow SourceCred
    instances to operate further under platform file size limits. If instances
    continue to expand and bump up against these limits, solutions such as a
    ShardedStorage composable class can be implemented as well.
    
    test plan: unit tests have been included
    
    TODO: test with SourceCred instance locally.

---
+++ src/core/storage/zip.js
@@ -0,0 +1,36 @@
+// @flow
+
+import {WritableDataStorage, DataStorage} from "./index";
+import {inflate, deflate} from "pako";
+/**
+ * The ZipStorage class composes with other WritableDataStorage implementations.
+ * It compresses `value`s before passing them into the underlying `baseStorage`
+ * implementation, and decompresses them upon receit  from `baseStorage`.
+ */
+export class ZipStorage implements DataStorage {
+  +_baseStorage: DataStorage;
+
+  constructor(baseStorage: DataStorage) {
+    this._baseStorage = baseStorage;
+  }
+
+  async get(key: string): Promise<Uint8Array> {
+    const data = await this._baseStorage.get(key);
+    return inflate(data, {to: "Uint8Array"});
+  }
+}
+
+export class WritableZipStorage
+  extends ZipStorage
+  implements WritableDataStorage {
+  +_baseStorage: WritableDataStorage;
+
+  constructor(baseStorage: WritableDataStorage) {
+    super(baseStorage);
+    this._baseStorage = baseStorage;
+  }
+
+  async set(key: string, value: Uint8Array) {
+    this._baseStorage.set(key, deflate(value));
+  }
+}
