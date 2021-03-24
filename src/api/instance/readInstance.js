4233df98054be1fd62997793eff8cd9a7204c7b6
Thena Seer <seerthena@gmail.com>
2021-03-23 18:52:38 -0700

    Create ReadInstance and refactor NetworkStorage to support URLs

---
+++ src/api/instance/readInstance.js
@@ -0,0 +1,62 @@
+// @flow
+
+import {ReadOnlyInstance} from "./instance";
+import {type CredrankInput} from "../main/credrank";
+import {type WeightedGraph} from "../../core/weightedGraph";
+import {type GraphInput} from "../main/graph";
+import {type GrainInput} from "../main/grain";
+import {join as pathJoin} from "path";
+import {loadJson} from "../../util/storage";
+import {
+  CredGraph,
+  parser as credGraphParser,
+} from "../../core/credrank/credGraph";
+import {NetworkStorage} from "../../core/storage/networkStorage";
+import {OriginStorage} from "../../core/storage/originStorage";
+import {ZipStorage} from "../../core/storage/zip";
+import {DataStorage} from "../../core/storage";
+
+export const getNetworkReadInstance = (base: string): ReadInstance =>
+  new ReadInstance(new NetworkStorage(base));
+export const getOriginReadInstance = (base: string): ReadInstance =>
+  new ReadInstance(new OriginStorage(base));
+
+const CREDGRAPH_PATH: $ReadOnlyArray<string> = [
+  "output",
+  "credGraph.json.gzip",
+];
+
+/**
+This is an Instance implementation that reads and writes using relative paths
+on the given base URL. The base URL given should end with a trailing slash.
+ */
+export class ReadInstance implements ReadOnlyInstance {
+  _storage: DataStorage;
+  _zipStorage: ZipStorage;
+
+  constructor(storage: DataStorage) {
+    this._storage = storage;
+    this._zipStorage = new ZipStorage(this._storage);
+  }
+
+  async readGraphInput(): Promise<GraphInput> {
+    throw "not yet implemented";
+  }
+
+  async readCredrankInput(): Promise<CredrankInput> {
+    throw "not yet implemented";
+  }
+
+  async readGrainInput(): Promise<GrainInput> {
+    throw "not yet implemented";
+  }
+
+  async readWeightedGraphForPlugin(): Promise<WeightedGraph> {
+    throw "not yet implemented";
+  }
+
+  async readCredGraph(): Promise<CredGraph> {
+    const credGraphPath = pathJoin(...CREDGRAPH_PATH);
+    return await loadJson(this._zipStorage, credGraphPath, credGraphParser);
+  }
+}
---
+++ src/core/storage/originStorage.test.js
@@ -0,0 +1,88 @@
+// @flow
+
+import {OriginStorage} from "./originStorage";
+
+jest.mock("cross-fetch", () => ({
+  // needed to utilize fetch as a default export.
+  __esModule: true,
+  default: (path) => {
+    switch (path) {
+      case "base/validPath":
+        return Promise.resolve({
+          arrayBuffer: () => new Uint8Array([1, 2]).buffer,
+          ok: true,
+          status: 200,
+          statusText: "OK",
+        });
+      case "/base/validPath":
+        return Promise.resolve({
+          arrayBuffer: () => new Uint8Array([1, 2]).buffer,
+          ok: true,
+          status: 200,
+          statusText: "OK",
+        });
+      case "base/serverError":
+        return Promise.resolve({
+          arrayBuffer: () => new ArrayBuffer(0),
+          ok: false,
+          status: 500,
+          statusText: "INTERNAL ERROR",
+        });
+      default:
+        return Promise.resolve({
+          arrayBuffer: () => new ArrayBuffer(0),
+          ok: false,
+          status: 404,
+          statusText: "NOT FOUND",
+        });
+    }
+  },
+}));
+
+describe("core/storage/originStorage", () => {
+  describe("OriginStorage", () => {
+    const value = new Uint8Array([1, 2]);
+    it("works when base path is empty", async () => {
+      expect.hasAssertions();
+      const storage = new OriginStorage("");
+      const result = await storage.get("base/validPath");
+      await expect(result).toEqual(value);
+    });
+    it("works when base is a relative path", async () => {
+      expect.hasAssertions();
+      const storage = new OriginStorage("base");
+      const result = await storage.get("validPath");
+      await expect(result).toEqual(value);
+    });
+    it("works when base is an absolute path", async () => {
+      expect.hasAssertions();
+      const storage = new OriginStorage("/base");
+      const result = await storage.get("validPath");
+      await expect(result).toEqual(value);
+    });
+    it("throws on upward traversal when the base is relative", async () => {
+      expect.hasAssertions();
+      const storage = new OriginStorage("base");
+      const thunk = async () => storage.get("../validPath");
+      await expect(thunk()).rejects.toThrow("Path traversal is not allowed");
+    });
+    it("throws on upward traversal when the base is absolute", async () => {
+      expect.hasAssertions();
+      const storage = new OriginStorage("/base/test");
+      const thunk = async () => storage.get("../validPath");
+      await expect(thunk()).rejects.toThrow("Path traversal is not allowed");
+    });
+    it("throws an error if the http response is not ok", async () => {
+      expect.hasAssertions();
+      const storage = new OriginStorage("base");
+      const thunk = async () => storage.get("invalidPath");
+      await expect(thunk()).rejects.toThrow(
+        "Error fetching invalidPath: 404 NOT FOUND"
+      );
+      const dunk = async () => storage.get("serverError");
+      await expect(dunk()).rejects.toThrow(
+        "Error fetching serverError: 500 INTERNAL ERROR"
+      );
+    });
+  });
+});
