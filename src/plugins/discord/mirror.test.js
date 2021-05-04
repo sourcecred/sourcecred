e508103324fcbef4264e647888a79d3630a091f9
hozz <medo3578@gmail.com>
2021-05-04 02:04:05 +0200

    - updated snapshots - added tests to check that nsfw channels are included or excluded

--- src/plugins/discord/mirror.test.js
+++ src/plugins/discord/mirror.test.js
@@ -8,10 +8,10 @@ import {Mirror} from "./mirror";
 describe("plugins/discord/mirror", () => {
   describe("smoke test", () => {
     const guildId = "678348980639498428";
-    const includeNsfwChannels = true;
     // const channelId = "678394406507905129";
 
     it("should print", async () => {
+      const includeNsfwChannels = true;
       // Given
       const repo = new SqliteMirrorRepository(
         new Database(":memory:"),
@@ -23,6 +23,24 @@ describe("plugins/discord/mirror", () => {
       const mirror = new Mirror(repo, api, guildId, includeNsfwChannels);
       await mirror.addMembers();
       await mirror.addTextChannels();
+      expect(repo.channels().some((channel) => channel.name.includes('nsfw'))).toBe(true);
+      // await mirror.addMessages(channelId, 10);
+    });
+
+    it("should print", async () => {
+      const includeNsfwChannels = false;
+      // Given
+      const repo = new SqliteMirrorRepository(
+        new Database(":memory:"),
+        guildId
+      );
+      const api = snapshotFetcher();
+
+      // When
+      const mirror = new Mirror(repo, api, guildId, includeNsfwChannels);
+      await mirror.addMembers();
+      await mirror.addTextChannels();
+      expect(repo.channels().every((channel) => !channel.name.includes('nsfw'))).toBe(true);
       // await mirror.addMessages(channelId, 10);
     });
   });
