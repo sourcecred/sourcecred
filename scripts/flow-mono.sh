c4a4d1a2b170788aef3c69972a7947f029583ed1
hz <medo3578@gmail.com>
2021-05-19 15:45:13 +0200

    flow mono is unnecesary when package is deployed (#3059)
    * flow mono is unnecesary when package is deployed
    
    * Remove yarn invocation from the flow-mono calls
    
    Calling yarn within this script seems to cause yarn to recursively run
    the command for each package, in addition to root.
    
    Because scripts run within yarn already have the project's node_modules
    in the bin path, flow-mono executes successfully without a recursive
    `yarn` call.
    
    test plan:
    
    1. yarn
    
    2. observe that the script does not get invoked more than once in
    the cli logging.
    
    Co-authored-by: Kevin Siegler <17910833+topocount@users.noreply.github.com>

--- scripts/flow-mono.sh
+++ scripts/flow-mono.sh
@@ -4,7 +4,7 @@ toplevel="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
 for package in $(ls ${toplevel}/packages); do
   cd $toplevel
   if [ -f "$toplevel/packages/$package/.flowconfig" ]; then
-    yarn flow-mono create-symlinks $toplevel/packages/$package/.flowconfig
-    yarn flow-mono create-symlinks $toplevel/packages/$package/.flowconfig-ci
+    flow-mono create-symlinks $toplevel/packages/$package/.flowconfig
+    flow-mono create-symlinks $toplevel/packages/$package/.flowconfig-ci
   fi;
 done
