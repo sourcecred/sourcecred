2b5c604c5ac0fd4527e772629889e562934c6ec8
Hammad Jutt <jutt@ualberta.ca>
2021-05-24 10:42:56 -0600

    Add web3 login button in AppBar
    Created LoginButton component to integrate logic from Web3Context and added it to the AppBar
    
    Related: #2975
    
    Test Plan: Run app, click Login in the app bar, sign in with MetaMask / WalletConnect and ensure your address is shown.

---
+++ packages/sourcecred/src/ui/components/LoginButton.js
@@ -0,0 +1,34 @@
+// @flow
+import * as React from "react";
+import {Button, CircularProgress} from "@material-ui/core";
+import {useWeb3} from "../utils/Web3Context";
+import {
+  parseAddress,
+  truncateEthAddress,
+} from "../../plugins/ethereum/ethAddress";
+
+export const LoginButton = (): React.Node => {
+  const {
+    address,
+    connectWeb3,
+    disconnect,
+    isConnected,
+    isConnecting,
+  } = useWeb3();
+
+  const handleLoginClick = React.useCallback(async () => {
+    await connectWeb3();
+  }, [connectWeb3]);
+
+  if (isConnecting) {
+    return <CircularProgress color={"secondary"} />;
+  }
+
+  return (
+    <Button onClick={isConnected ? disconnect : handleLoginClick}>
+      {isConnected && address
+        ? `Disconnect ${truncateEthAddress(parseAddress(address))}`
+        : "Connect"}
+    </Button>
+  );
+};
