852294fadb8bab542850e7dbf8d978edf00496b1
Thena Seer <seerthena@gmail.com>
2021-10-08 11:41:57 -0700

    API: create extended library for node (#3214)

---
+++ packages/sourcecred/src/api/lib/node.js
@@ -0,0 +1,29 @@
+// @flow
+
+import deepFreeze from "deep-freeze";
+import cloneDeep from "lodash.clonedeep";
+import base from "./base";
+// `cloneDeep` and `any` help get around
+// assignment constraints in both Flow and JS
+const api: any = {...cloneDeep(base)};
+
+// Extended exports for calling SourceCred code programmatically
+// in a node environment. Both the structure and the contents of
+// this API are experimental and subject to change.
+
+import {LocalInstance} from "../../api/instance/localInstance";
+api.instance.LocalInstance = LocalInstance;
+
+import {GithubPlugin} from "../../plugins/github/plugin";
+api.plugins.github.GithubPlugin = GithubPlugin;
+
+import {DiscordPlugin} from "../../plugins/discord/plugin";
+api.plugins.discord.DiscordPlugin = DiscordPlugin;
+
+import {DiscoursePlugin} from "../../plugins/discourse/plugin";
+api.plugins.discourse.DiscoursePlugin = DiscoursePlugin;
+
+import {InitiativesPlugin} from "../../plugins/initiatives/plugin";
+api.plugins.initiatives.InitiativesPlugin = InitiativesPlugin;
+
+export default (deepFreeze(api): typeof api);
