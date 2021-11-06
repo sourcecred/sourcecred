fc37ea146c1ae15bc59472e3138c14811daccd6b
Thena Seer <11550396+blueridger@users.noreply.github.com>
2021-11-05 18:23:55 -0700

    Working multi-select chips

--- packages/sourcecred/src/ui/components/LedgerViewer/LedgerToolbar.js
+++ packages/sourcecred/src/ui/components/LedgerViewer/LedgerToolbar.js
@@ -4,64 +4,85 @@ import React, {type Node as ReactNode, useState, useEffect} from "react";
 import {type Action, type LedgerEvent} from "../../../core/ledger/ledger";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import Chip from '@material-ui/core/Chip';
+import InputLabel from "@material-ui/core/InputLabel";
+// Maybe use react-select instead?
+// https://medium.com/geekculture/creating-multi-select-dropdown-with-checkbox-in-react-792ff2464ef3
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Chip from "@material-ui/core/Chip";
 import {type TableState} from "../../../webutil/tableState";
 
 // alternative to hardcoding?
-const ACTION_TYPES = ["CREATE_IDENTITY", "RENAME_IDENTITY", "CHANGE_IDENTITY_TYPE", "ADD_ALIAS", "MERGE_IDENTITIES", "TOGGLE_ACTIVATION", "DISTRIBUTE_GRAIN", "TRANSFER_GRAIN", "SET_PAYOUT_ADDRESS"];
+// You could instantiate a Set (sets don't have duplicates),
+// loop through the Ledger events and add their action types to the Set
+// and then make an array out of the set.
+const ACTION_TYPES = [
+  "CREATE_IDENTITY",
+  "RENAME_IDENTITY",
+  "CHANGE_IDENTITY_TYPE",
+  "ADD_ALIAS",
+  "MERGE_IDENTITIES",
+  "TOGGLE_ACTIVATION",
+  "DISTRIBUTE_GRAIN",
+  "TRANSFER_GRAIN",
+  "SET_PAYOUT_ADDRESS",
+];
 
 type LedgerToolbarProps = {|
   +classes: Object,
-  +ts: TableState<LedgerEvent>
+  +ts: TableState<LedgerEvent>,
 |};
 
 const LedgerToolbar = (props: LedgerToolbarProps): ReactNode => {
-  const [filter, setFilter] = useState<string>("");
+  const [filter, setFilter] = useState<Array<string>>([]);
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-    props.ts.createOrUpdateFilterFn(
-      "filterEventName",
-      (ledgerEvent) => ledgerEvent.action.type
-        .includes(event.target.value)
-    )
+    filter.push(event.target.value);
+    setFilter(filter);
+    props.ts.createOrUpdateFilterFn("filterEventName", (ledgerEvent) =>
+      filter.includes(ledgerEvent.action.type)
+    );
   };
 
-  const handleDeleteFilter = (event) => {
-    setFilter("");
-    props.ts.createOrUpdateFilterFn(
-      "filterEventName",
-      null
-    )
+  const handleDeleteFilter = (type) => {
+    filter.splice(filter.indexOf(type), 1);
+    setFilter(filter);
+    if (filter.length) {
+      props.ts.createOrUpdateFilterFn("filterEventName", (ledgerEvent) =>
+        filter.includes(ledgerEvent.action.type)
+      );
+    } else {
+      props.ts.createOrUpdateFilterFn("filterEventName", null);
+    }
   };
 
   // filter : seperate component?
-  // filter Chips - array of selected filters?? 
+  // filter Chips - array of selected filters??
   return (
     <Toolbar className={props.classes.toolbar}>
       <Typography variant="h6" id="tableTitle" component="div">
         Ledger Event History
       </Typography>
       <InputLabel>Select Ledger Action</InputLabel>
-      <Select onChange={handleFilterChange}>
-        {filterOptions()}
-      </Select>
-      {filter && <Chip variant="outlined" label={filter} onDelete={handleDeleteFilter}/>}
+      <Select onChange={handleFilterChange}>{filterOptions()}</Select>
+      {filter.map((type) => (
+        <Chip
+          variant="outlined"
+          key={type}
+          label={type}
+          onDelete={() => handleDeleteFilter(type)}
+        />
+      ))}
     </Toolbar>
   );
-}
+};
 
 const filterOptions = () => {
   let filterOptions = [];
   ACTION_TYPES.forEach((action) => {
-    filterOptions.push(
-      <MenuItem value={action}>{action}</MenuItem>
-    );
+    filterOptions.push(<MenuItem value={action}>{action}</MenuItem>);
   });
   return filterOptions;
-}
+};
 
 export default LedgerToolbar;
