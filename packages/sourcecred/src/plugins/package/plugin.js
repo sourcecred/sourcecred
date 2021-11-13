41e15690bfa0a6ab9ae44ac47a8df03cdbf2bc40
Zane Starr <zcstarr@gmail.com>
2021-11-13 12:59:38 -0800

    feat: [wip] adds support for importing custom user defined plugins
    This is to help users define custom plugins, without needing to
    use external plugins. It allows the developer to specify any package
    to use as a plugin, as long as it implements the `Plugin` interface.
    
    The motivation here is to lower the barrier to entry for extending
    sourcecred.
    
    This is still a wip so lacks a few tests.

---
+++ packages/sourcecred/src/plugins/package/plugin.js
@@ -0,0 +1,75 @@
+// @flow
+
+import type {Plugin, PluginDirectoryContext} from "../../api/plugin";
+import type {PluginId} from "../../api/pluginId";
+import path from "path";
+import type {PluginDeclaration} from "../../analysis/pluginDeclaration";
+import {type TaskReporter} from "../../util/taskReporter";
+import type {ReferenceDetector} from "../../core/references";
+import {
+  type WeightedGraph,
+} from "../../core/weightedGraph";
+import type {IdentityProposal} from "../../core/ledger/identityProposal";
+
+
+export class PackagePlugin implements Plugin {
+  id: PluginId;
+  plugin: Plugin;
+
+  constructor(options: {|
+    +pluginId: PluginId,
+  |}) {
+    this.id = options.pluginId;
+  }
+
+  async loadPlugin(): Promise<void> {
+    if(this.plugin !== undefined ) return;
+
+    const pluginModule = this.id.startsWith("./") ? path.resolve(process.cwd(), this.id): this.id; 
+    try {
+      // Must import a plugin object from 
+      this.plugin = (await import(pluginModule).default: Plugin);
+    }catch(e){
+      throw new Error("Could not load dynamically imported plugin")
+    }
+  }
+
+  async declaration(): Promise<PluginDeclaration> {
+    await this.loadPlugin();
+    return this.plugin.declaration();
+  }
+
+  async load(
+    ctx: PluginDirectoryContext,
+    reporter: TaskReporter
+  ): Promise<void> {
+    await this.loadPlugin();
+    return this.plugin.load(ctx,reporter);
+  }
+
+  async graph(
+    ctx: PluginDirectoryContext,
+    rd: ReferenceDetector,
+    reporter: TaskReporter
+  ): Promise<WeightedGraph> {
+    await this.loadPlugin();
+    return this.plugin.graph(ctx,rd, reporter);
+  }
+
+  async referenceDetector(
+    ctx: PluginDirectoryContext,
+    reporter: TaskReporter
+  ): Promise<ReferenceDetector> {
+    await this.loadPlugin();
+    return this.plugin.referenceDetector(ctx, reporter);
+  }
+
+  async identities(
+    ctx: PluginDirectoryContext,
+    reporter: TaskReporter
+  ): Promise<$ReadOnlyArray<IdentityProposal>> {
+    this.loadPlugin();
+    return this.plugin.identities(ctx, reporter);
+  }
+
+}
