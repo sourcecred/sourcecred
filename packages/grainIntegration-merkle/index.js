e651a0a75f2f1f1359ac1a3efd011fc3788b52d6
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2021-11-19 09:35:25 -0600

    Begin Setting up Deployment Prompts

--- packages/grainIntegration-merkle/index.js
+++ packages/grainIntegration-merkle/index.js
@@ -1,33 +1,90 @@
 // @flow
 
 const ethers = require("ethers");
+const promptRequire =
+  /* eslint-disable camelcase */
+  typeof __non_webpack_require__ !== "undefined"
+    ? /* eslint-disable no-undef */
+      __non_webpack_require__
+    : require;
+const prompt = promptRequire("prompt-sync")({ sigint: true });
+const { MerkleTree } = require("merkletreejs");
+const { soliditySha3 } = require("web3-utils");
+const keccak256 = require("keccak256");
+
 const redeemContract = require("./artifacts/contracts/MerkleRedeem.sol/MerkleRedeem.json");
 
 /*::
+type Integration = {|
+    contractAddress: string
+|}
 type Currency = {|
   +type: "EVM",
   +chainId: string,
-  +tokenAddress: string
+  +tokenAddress: string,
 |}
 
 type Config = {|
   +accountingEnabled: boolean,
   +processDistributions: boolean,
-  +currency: Currency
+  +currency: Currency,
+  +integration: Integration
 |}
 */
 
 const merkleIntegration /*: any*/ = async (
-  _unused_payoutDistributions,
+  payoutDistributions,
   config /*: Config */
 ) => {
+  const { currency, integration: integrationConfig } = config;
   const provider = new ethers.providers.JsonRpcProvider();
   const signer = provider.getSigner();
-  const contract = await deployMerkleContract(config.currency, signer);
+  console.log({ config, integrationConfig });
+  let address = integrationConfig
+    ? integrationConfig.contractAddress
+    : undefined;
+  let configReturn = {};
+  if (!address) {
+    address = await deployMerkleContract(currency, signer);
+    configReturn = {
+      contractAddress: address
+    };
+  }
+  console.log(payoutDistributions, address);
+  const tree = createMerkleTree(payoutDistributions);
+  const merkleContract = new ethers.Contract(
+    address,
+    redeemContract.abi,
+    signer
+  );
+  try {
+    const defaultDelay = await merkleContract.minDelay();
+    await merkleContract.seedDistribution(
+      0,
+      tree.getHexRoot(),
+      Math.ceil(defaultDelay + Date.now() / 1000)
+    );
+  } catch (e) {
+    throw new Error(`merkle integration error: ${e}`);
+  }
   return {
-    transferredGrain: []
+    transferredGrain: [],
+    configUpdate: {} //configReturn
   };
 };
+
+function createMerkleTree(elements) {
+  const hashedElements = elements.map(([address, amount]) => {
+    console.log({ address, amount });
+    const hash = soliditySha3(address, amount);
+    return hash;
+  });
+  console.log({ hashedElements });
+  return new MerkleTree(hashedElements, keccak256, {
+    hashLeaves: false,
+    sortPairs: true
+  });
+}
 
 async function deployMerkleContract(
   config /*: Currency*/,
@@ -38,11 +95,31 @@ async function deployMerkleContract(
     redeemContract.bytecode,
     signer
   );
-
-  const contract = await factory.deploy(config.tokenAddress, config.chainId);
+  confirmDeploy();
+  const delay = getDelayTime();
+  const contract = await factory.deploy(config.tokenAddress, delay);
 
   console.log("Contract Deployed at: ", contract.address);
-  return contract;
+  return contract.address;
+}
+
+function getDelayTime() {
+  const response = prompt(
+    "how long should the pause period be before funds can be claimed, in days? This can be modified later. (default: 0 days): ",
+    "0"
+  );
+  const possibleInt = Number.parseInt(response);
+  if (Number.isNaN(possibleInt)) return getDelayTime();
+  return possibleInt * 86400;
+}
+
+function confirmDeploy() {
+  const response = prompt(
+    "No distribution contract configured: do you want to deploy one now? [y/N] "
+  );
+  if (response.toLowerCase() === "y") return;
+
+  throw new Error("User exited");
 }
 
 module.exports = { merkleIntegration };
