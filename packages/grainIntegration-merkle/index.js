6e3a1b276376b652ed93ec2d8e99e5f58c64d2d5
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2021-11-23 08:30:52 -0600

    Clean up dialog flow and add blockchain authentication
    The smart contract roles now have a flow to get simple use cases up and
    running quickly.

--- packages/grainIntegration-merkle/index.js
+++ packages/grainIntegration-merkle/index.js
@@ -3,8 +3,10 @@
 const ethers = require("ethers");
 const promptRequire =
   /* eslint-disable camelcase */
+  // $FlowIgnore[cannot-resolve-name]
   typeof __non_webpack_require__ !== "undefined"
     ? /* eslint-disable no-undef */
+      // $FlowIgnore[cannot-resolve-name]
       __non_webpack_require__
     : require;
 const prompt = promptRequire("prompt-sync")({ sigint: true });
@@ -37,9 +39,9 @@ const merkleIntegration /*: any*/ = async (
   config /*: Config */
 ) => {
   const { currency, integration: integrationConfig } = config;
-  const provider = new ethers.providers.JsonRpcProvider();
-  const signer = provider.getSigner();
-  console.log({ config, integrationConfig });
+  //const provider = new ethers.providers.JsonRpcProvider();
+  //const signer = provider.getSigner();
+  const signer = getWalletAndProvider();
   let address = integrationConfig
     ? integrationConfig.contractAddress
     : undefined;
@@ -50,7 +52,6 @@ const merkleIntegration /*: any*/ = async (
       contractAddress: address
     };
   }
-  console.log(payoutDistributions, address);
   const tree = createMerkleTree(payoutDistributions);
   const merkleContract = new ethers.Contract(
     address,
@@ -58,9 +59,11 @@ const merkleIntegration /*: any*/ = async (
     signer
   );
   try {
+    const nextId = await getNextDistributionId(merkleContract);
+    await checkPermissions(merkleContract, signer);
     const defaultDelay = await merkleContract.minDelay();
     await merkleContract.seedDistribution(
-      0,
+      nextId,
       tree.getHexRoot(),
       Math.ceil(defaultDelay + Date.now() / 1000)
     );
@@ -69,17 +72,15 @@ const merkleIntegration /*: any*/ = async (
   }
   return {
     transferredGrain: [],
-    configUpdate: {} //configReturn
+    configUpdate: configReturn
   };
 };
 
 function createMerkleTree(elements) {
   const hashedElements = elements.map(([address, amount]) => {
-    console.log({ address, amount });
     const hash = soliditySha3(address, amount);
     return hash;
   });
-  console.log({ hashedElements });
   return new MerkleTree(hashedElements, keccak256, {
     hashLeaves: false,
     sortPairs: true
@@ -109,7 +110,10 @@ function getDelayTime() {
     "0"
   );
   const possibleInt = Number.parseInt(response);
-  if (Number.isNaN(possibleInt)) return getDelayTime();
+  if (Number.isNaN(possibleInt)) {
+    console.error(`Not a number; ${response}; Please try again...`);
+    return getDelayTime();
+  }
   return possibleInt * 86400;
 }
 
@@ -120,6 +124,54 @@ function confirmDeploy() {
   if (response.toLowerCase() === "y") return;
 
   throw new Error("User exited");
+}
+
+async function checkPermissions(contract, signer) {
+  // These _could_ be hardcoded
+  const AdminRole = await contract.DEFAULT_ADMIN_ROLE();
+  const SeederRole = await contract.SEEDER_ROLE();
+  const signerAddress = await signer.getAddress();
+  const signerIsAdmin = await contract.hasRole(AdminRole, signerAddress);
+  const signerIsSeeder = await contract.hasRole(SeederRole, signerAddress);
+  if (signerIsAdmin && !signerIsSeeder) {
+    console.warn(
+      "Your active account does not have permission to seed on chain grain distributions.Either grant the current account permissions or utilize an account that has the permissions"
+    );
+    await assignSeederToAccount(contract, signerAddress);
+  } else if (!signerIsAdmin && !signerIsSeeder) {
+    console.error(
+      `Account ${signerAddress} cannot distribute grain. exiting...`
+    );
+  }
+}
+
+async function assignSeederToAccount(
+  contract /*: any*/,
+  signerAddress /*: string*/
+) {
+  const SeederRole = await contract.SEEDER_ROLE();
+  const seederAddress = prompt(
+    `Enter address to grant seeder role (default: ${signerAddress}): `,
+    signerAddress
+  );
+  await contract.grantRole(SeederRole, seederAddress);
+}
+
+async function getNextDistributionId(contract /*: any*/) {
+  const publishFilter = contract.filters.DistributionPublished();
+  const publishEvents = await contract.queryFilter(publishFilter);
+  const lastPublishedId = publishEvents.map(e => e.args._id)[
+    publishEvents.length - 1
+  ];
+  return lastPublishedId ? lastPublishedId.add(1) : 0;
+}
+
+function getWalletAndProvider() {
+  const { ETH_MNEMONIC, ACCOUNT_PATH, ETH_NETWORK } = process.env;
+  if (!ETH_MNEMONIC) throw new Error("Can't auth: No mnemonic set");
+  const provider = ethers.getDefaultProvider(ETH_NETWORK);
+  const wallet = ethers.Wallet.fromMnemonic(ETH_MNEMONIC, ACCOUNT_PATH);
+  return wallet.connect(provider);
 }
 
 module.exports = { merkleIntegration };
