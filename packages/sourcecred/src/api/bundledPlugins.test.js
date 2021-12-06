90b5087fb36207ac78422f7fed19e6959f9a5545
Zane Starr <zcstarr@gmail.com>
2021-12-06 09:51:09 -0800

    feat: add initial tests for plugins

--- packages/sourcecred/src/api/bundledPlugins.test.js
+++ packages/sourcecred/src/api/bundledPlugins.test.js
@@ -1,55 +1,49 @@
 // @flow
 
-import { getPlugin } from "./bundledPlugins";
-import { fromString } from "./pluginId";
-import fs from "fs-extra";
-import rm from "rimraf";
-import {promisify} from "util";
-const rmdir = promisify(rm);
-
+import {getPlugin} from "./bundledPlugins";
+import {fromString, fromStringRaw} from "./pluginId";
 
 describe("api/bundledPlugins", () => {
   it("returns a local bundled plugin", async () => {
-      const pluginId = fromString("sourcecred/initiatives");
-      const plugin = getPlugin(pluginId);
-      if(!plugin) {
-        throw new Error("Could not find valid plugin")
-      } 
-      const declaration = await plugin.declaration();
-      expect(declaration.name).toBe("Initiatives");
+    const pluginId = fromString("sourcecred/initiatives");
+    const plugin = getPlugin(pluginId);
+    if (!plugin) {
+      throw new Error("Could not find valid plugin");
+    }
+    const declaration = await plugin.declaration();
+    expect(declaration.name).toBe("Initiatives");
   });
 
   it("returns an external plugin", async () => {
-      const pluginId = fromString("external/fakePlugin");
-      const plugin = getPlugin(pluginId);
-      if(!plugin) {
-        throw new Error("Could not find valid plugin")
-      } 
-      const declaration = await plugin.declaration();
-      expect(declaration.name).toBe("fakeplugin");
+    const pluginId = fromString("external/fakePlugin");
+    const plugin = getPlugin(pluginId);
+    if (!plugin) {
+      throw new Error("Could not find valid plugin");
+    }
+    const declaration = await plugin.declaration();
+    expect(declaration.name).toBe("fakeplugin");
   });
 
-  it("returns a package plugin", async ()=>{
-    // create temp directory
-    // copy over fixtures directory
-    // transpile fixtures directory to js
-    // refer to that path 
-    // clean up temp directory
-      const tmpDir = await fs.mkdtemp("tmp-sourcecred-test");
-      console.log(tmpDir)
-      await fs.copy(`${__dirname}/../plugins/package/fixtures`, `${tmpDir}`) 
+  it("returns a package plugin", async () => {
+    const pluginId = fromStringRaw(
+      `${__dirname}/../../dist/fixtures/packagePlugin/api.js`
+    );
+    const plugin = getPlugin(pluginId);
+    if (!plugin) {
+      throw new Error("Could not find valid plugin");
+    }
+    const declaration = await plugin.declaration();
+    expect(declaration.name).toBe("test_custom_identity");
+    //await rmdir(tmpDir)
+  });
 
-      const pluginId = fromString(`${__dirname}/../plugins/package/fixtures`);
-      const plugin = getPlugin(pluginId);
-      if(!plugin) {
-        throw new Error("Could not find valid plugin")
-      } 
-      const declaration = await plugin.declaration();
-      expect(declaration.name).toBe("fakeplugin");
-
-      await rmdir(tmpDir)
-
-  })
-
+  it("returns a package plugin from an ambient module", async () => {
+    const pluginId = fromStringRaw(`zcstarr-sourcecred-test-package-plugin`);
+    const plugin = getPlugin(pluginId);
+    if (!plugin) {
+      throw new Error("Could not find valid plugin");
+    }
+    const declaration = await plugin.declaration();
+    expect(declaration.name).toBe("test_custom_identity");
+  });
 });
-
