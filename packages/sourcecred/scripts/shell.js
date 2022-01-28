7152d8b9b3503f9540a03e30413e573930242bb8
Thena Seer <11550396+blueridger@users.noreply.github.com>
2022-01-27 20:51:48 -0800

    CredEquate: expose core code in library

--- packages/sourcecred/scripts/shell.js
+++ packages/sourcecred/scripts/shell.js
@@ -1,10 +1,13 @@
 // @no-flow
-var repl = require("repl");
-var context = repl.start("$ ").context;
+const repl = require("repl");
+const context = repl.start("$ ").context;
 
 try {
   context.sc = require("../dist/server/api.js").sourcecred;
   context.scClient = require("../dist/client/api.js");
 } catch (e) {
-  throw "Run `yarn build` before opening the shell.";
+  console.log(
+    "\nAn error occurred. Try running `yarn build` before opening the shell.\n"
+  );
+  throw e;
 }
