f4f7f55b1ae2c2927aa74ef64331f9e8874bdb28
Thena Seer <11550396+blueridger@users.noreply.github.com>
2022-02-23 16:02:13 -0800

    fixing tests

--- packages/sourcecred/src/plugins/discord/config.test.js
+++ packages/sourcecred/src/plugins/discord/config.test.js
@@ -31,7 +31,7 @@ describe("plugins/discord/config", () => {
         },
         includeNsfwChannels: true,
         simplifyGraph: true,
-        beginningDate: 123,
+        beginningDate: "1/1/2021",
       },
     ];
     const expected = [
@@ -64,7 +64,7 @@ describe("plugins/discord/config", () => {
         },
         includeNsfwChannels: true,
         simplifyGraph: true,
-        beginningTimestampMs: 123,
+        beginningTimestampMs: 1609488000000,
       },
     ];
     const parsed: DiscordConfigs = parser.parseOrThrow(raw);
